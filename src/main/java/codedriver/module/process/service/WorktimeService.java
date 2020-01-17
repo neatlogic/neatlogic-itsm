0da1f926012e788e216158434e44848edf2b35c8
林邦泉 <linbq@techsure.com.cn>
2020-01-17 18:29:33 +0800

    --story=1002178 --user=林邦泉 超时策略 https://www.tapd.cn/54247054/s/1043542

--- src/main/java/codedriver/module/process/service/WorktimeService.java
+++ src/main/java/codedriver/module/process/service/WorktimeService.java
@@ -1,6 +1,12 @@
 package codedriver.module.process.service;
 
+import java.util.List;
+
+import codedriver.module.process.dto.WorktimeRangeVo;
+
 public interface WorktimeService {
 
 	Long calculateTimeoutPoint(long startTime, long timeLimit, String worktimeUuid);
+	
+	Long calculateCostTime(List<WorktimeRangeVo> worktimeRangeList);
 }
