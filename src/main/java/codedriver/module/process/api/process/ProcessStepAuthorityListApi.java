91fb80cc35aeb5aaf06151c536acdddbfd6eae28
林邦泉 <linbq@techsure.com.cn>
2020-01-19 16:29:47 +0800

    --story=1002145 --user=林邦泉 流程管理&编辑页面 https://www.tapd.cn/54247054/s/1043768

--- src/main/java/codedriver/module/process/api/process/ProcessStepAuthorityListApi.java
+++ src/main/java/codedriver/module/process/api/process/ProcessStepAuthorityListApi.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
+import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
 import codedriver.module.process.constvalue.ProcessStepAuthority;
 
@@ -25,7 +28,10 @@ public class ProcessStepAuthorityListApi extends ApiComponentBase {
 	public String getConfig() {
 		return null;
 	}
-
+	
+	@Output({
+		@Param(name="Return", explode=ValueTextVo[].class, desc = "流程步骤权限列表")
+	})
 	@Description(desc = "流程步骤权限列表接口")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
--- src/main/java/codedriver/module/process/service/WorktimeServiceImpl.java
+++ src/main/java/codedriver/module/process/service/WorktimeServiceImpl.java
@@ -3,13 +3,10 @@ package codedriver.module.process.service;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import codedriver.framework.process.dao.mapper.WorktimeMapper;
 import codedriver.framework.process.exception.WorktimeNotFoundException;
@@ -22,7 +19,7 @@ public class WorktimeServiceImpl implements WorktimeService {
 	private WorktimeMapper worktimeMapper;
 
 	@Override
-	public Long calculateTimeoutPoint(long activeTime, long timeLimit, String worktimeUuid) {
+	public long calculateTimeoutPoint(long activeTime, long timeLimit, String worktimeUuid) {
 		if (worktimeMapper.checkWorktimeIsExists(worktimeUuid) == 0) {
 			throw new WorktimeNotFoundException(worktimeUuid);
 		}
@@ -57,62 +54,56 @@ public class WorktimeServiceImpl implements WorktimeService {
 	}
 
 	@Override
-	public Long calculateCostTime(List<WorktimeRangeVo> worktimeRangeList) {
-		if (worktimeRangeList != null && !worktimeRangeList.isEmpty()) {
-			// 先按开始时间从小到大排序
-			Collections.sort(worktimeRangeList, new Comparator<WorktimeRangeVo>() {
-				@Override
-				public int compare(WorktimeRangeVo o1, WorktimeRangeVo o2) {
-					return o1.getStartTime().compareTo(o2.getStartTime());
-				}
-			});
-			// 保存删除重复时间后的列表
-			List<Map<String, Object>> deduplicationList = new ArrayList<>();
-			String worktimeUuid = null;
-			long startTime = -1L;
-			long endTime = -1L;
-			WorktimeRangeVo worktimeRangeVo = null;
-			Map<String, Object> map = null;
-			for (int i = 0; i < worktimeRangeList.size(); i++) {
-				worktimeRangeVo = worktimeRangeList.get(i);
-				worktimeUuid = worktimeRangeVo.getWorktimeUuid();
-				if (startTime == -1L) {
-					startTime = worktimeRangeVo.getStartTime();
-				}
-				if (endTime == -1L) {
-					endTime = worktimeRangeVo.getEndTime();
-				} else if (endTime >= worktimeRangeVo.getEndTime()) {// 如果上一段时间的结束时间大于等于这段的结束时间，说明上一段包含这段时间范围，抛弃这段
-
-				} else if (endTime >= worktimeRangeVo.getStartTime()) {// 如果上一段时间的结束时间大于等于下一段的开始时间，就拼成一段时间
-					endTime = worktimeRangeVo.getEndTime();
-				} else {// 保存上一段
-					map = new HashMap<>();
-					map.put("startTime", startTime);
-					map.put("endTime", endTime);
-					map.put("worktimeUuid", worktimeUuid);
-					deduplicationList.add(map);
-					// 开始新一段
-					startTime = worktimeRangeVo.getStartTime();
-					endTime = worktimeRangeVo.getEndTime();
-
-				}
-				// 最后一段
-				if (i == worktimeRangeList.size() - 1) {
-					map = new HashMap<>();
-					map.put("startTime", startTime);
-					map.put("endTime", endTime);
-					deduplicationList.add(map);
-				}
-			}
-			long sum = 0L;
-			for (Map<String, Object> timeMap : deduplicationList) {
-				startTime = Long.parseLong(timeMap.get("startTime").toString());
-				endTime = Long.parseLong(timeMap.get("endTime").toString());
-				sum += worktimeMapper.calculateCostTime(worktimeUuid, startTime, endTime);
-			}
-			return sum;
+	public long calculateCostTime(List<WorktimeRangeVo> worktimeRangeList) {
+		if (worktimeRangeList == null || worktimeRangeList.isEmpty()) {
+			return 0L;
 		}
-		return 0L;
+		
+		// 先按开始时间从小到大排序
+		Collections.sort(worktimeRangeList, new Comparator<WorktimeRangeVo>() {
+			@Override
+			public int compare(WorktimeRangeVo o1, WorktimeRangeVo o2) {
+				return o1.getStartTime().compareTo(o2.getStartTime());
+			}
+		});
+		// 保存删除重复时间后的列表
+		List<WorktimeRangeVo> deduplicationList = new ArrayList<>();
+		long startTime = -1L;
+		long endTime = -1L;
+		WorktimeRangeVo worktimeRange = null;
+		String worktimeUuid = null;
+		for (WorktimeRangeVo worktimeRangeVo: worktimeRangeList) {
+			if (startTime == -1L) {
+				worktimeUuid = worktimeRangeVo.getWorktimeUuid();
+				startTime = worktimeRangeVo.getStartTime();
+			}
+			if (endTime == -1L) {
+				endTime = worktimeRangeVo.getEndTime();
+			} else if (endTime >= worktimeRangeVo.getEndTime()) {// 如果上一段时间的结束时间大于等于这段的结束时间，说明上一段包含这段时间范围，抛弃这段
+
+			} else if (endTime >= worktimeRangeVo.getStartTime()) {// 如果上一段时间的结束时间大于等于下一段的开始时间，就拼成一段时间
+				endTime = worktimeRangeVo.getEndTime();
+			} else {// 保存上一段
+				worktimeRange = new WorktimeRangeVo();
+				worktimeRange.setStartTime(startTime);
+				worktimeRange.setEndTime(endTime);
+				deduplicationList.add(worktimeRange);
+				// 开始新一段
+				startTime = worktimeRangeVo.getStartTime();
+				endTime = worktimeRangeVo.getEndTime();
+			}
+		}
+		// 最后一段
+		worktimeRange = new WorktimeRangeVo();
+		worktimeRange.setStartTime(startTime);
+		worktimeRange.setEndTime(endTime);
+		deduplicationList.add(worktimeRange);
+		
+		long sum = 0L;
+		for (WorktimeRangeVo worktimeRangeVo : deduplicationList) {
+			sum += worktimeMapper.calculateCostTime(worktimeUuid, worktimeRangeVo.getStartTime(), worktimeRangeVo.getEndTime());
+		}
+		return sum;
 	}
 
 }
