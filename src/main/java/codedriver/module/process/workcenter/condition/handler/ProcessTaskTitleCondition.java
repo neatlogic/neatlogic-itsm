58f36432395a618949a631763f589aca6c76a100
吕佐康 <lvzk@techsure.com.cn>
2020-02-11 21:29:56 +0800

    update

---
+++ src/main/java/codedriver/module/process/workcenter/condition/handler/ProcessTaskTitleCondition.java
@@ -0,0 +1,43 @@
+package codedriver.module.process.workcenter.condition.handler;
+
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.module.process.workcenter.condition.core.IWorkcenterCondition;
+import codedriver.module.process.workcenter.dto.WorkcenterConditionVo;
+
+@Component
+public class ProcessTaskTitleCondition implements IWorkcenterCondition{
+
+	@Override
+	public String getName() {
+		return "title";
+	}
+
+	@Override
+	public String getDisplayName() {
+		return "标题";
+	}
+
+	@Override
+	public String getType() {
+		return WorkcenterConditionVo.Type.TEXT.toString();
+	}
+
+	@Override
+	public JSONObject getConfig() {
+		return null;
+	}
+
+	@Override
+	public Integer getSort() {
+		return 1;
+	}
+
+	@Override
+	public String[] getExpressionList() {
+		return new String[] { WorkcenterConditionVo.ExpressionType.LIKE.getValue() };
+	}
+
+}
---
+++ src/main/java/codedriver/module/process/workcenter/dto/WorkcenterConditionVo.java
@@ -0,0 +1,130 @@
+package codedriver.module.process.workcenter.dto;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class WorkcenterConditionVo {
+	
+	private String name;
+	private String displayName;
+	private String type;
+	private JSONObject config;
+	private Integer sort;
+	private String[] expressionList;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public JSONObject getConfig() {
+		return config;
+	}
+
+
+	public void setConfig(JSONObject config) {
+		this.config = config;
+	}
+
+
+	public Integer getSort() {
+		return sort;
+	}
+
+
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
+
+
+	public String[] getExpressionList() {
+		return expressionList;
+	}
+
+
+	public void setExpressionList(String[] expressionList) {
+		this.expressionList = expressionList;
+	}
+
+	public enum Type {
+		TEXT("text"), SELECT("select"), MSELECT("mselect"), INPUTSELECT("inputselect"), TIMESCOPE("timescope"), TEXTAREA("textarea");
+		private String name;
+
+		private Type(String _name) {
+			this.name = _name;
+		}
+
+		@Override
+		public String toString() {
+			return this.name;
+		}
+	}
+
+	public enum ExpressionType {
+		EQ("eq", "等于", "="), LIKE("like", "模糊查询", "like"), CONTAIN("contain", "模糊查询", "contain"),LT("lt", "小于", "&lt;"), GT("gt", "大于", "&gt;");
+		private String name;
+		private String text;
+		private String expression;
+
+		private ExpressionType(String _name, String _text, String _expression) {
+			this.name = _name;
+			this.text = _text;
+			this.expression = _expression;
+		}
+
+		public String getValue() {
+			return name;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public String getExpression() {
+			return expression;
+		}
+
+		public static String getValue(String name) {
+			for (ExpressionType s : ExpressionType.values()) {
+				if (s.getValue().equals(name)) {
+					return s.getExpression();
+				}
+			}
+			return "";
+		}
+
+		public static String getText(String name) {
+			for (ExpressionType s : ExpressionType.values()) {
+				if (s.getValue().equals(name)) {
+					return s.getText();
+				}
+			}
+			return "";
+		}
+	}
+}
