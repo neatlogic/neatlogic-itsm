5dec98cb72a086665dd35f2efeacc81ae39c6ec3
lixs <lixs@techsure.com.cn>
2020-02-18 17:24:04 +0800

    --task=1002484 --user=李晓升 权限类调整和用户角色... https://www.tapd.cn/54247054/s/1047300

--- src/main/java/codedriver/module/process/auth/label/FORM_MODIFY.java
+++ src/main/java/codedriver/module/process/auth/label/FORM_MODIFY.java
@@ -1,6 +1,7 @@
 package codedriver.module.process.auth.label;
 
 import codedriver.framework.auth.core.AuthBase;
+import codedriver.framework.auth.core.AuthGroupEnum;
 
 public class FORM_MODIFY extends AuthBase {
 
@@ -12,6 +13,11 @@ public class FORM_MODIFY extends AuthBase {
 	@Override
 	public String getAuthIntroduction() {
 		return "对表单进行添加、修改和删除";
+	}
+
+	@Override
+	public String getAuthGroup() {
+		return AuthGroupEnum.PROCESS.getValue();
 	}
 
 }
