80c45995558625bea2ce6b6b0a5266f20a17f69c
吕佐康 <lvzk@techsure.com.cn>
2020-02-26 14:32:13 +0800

    update

--- src/main/java/codedriver/module/process/api/process/ProcessGetConditionApi.java
+++ src/main/java/codedriver/module/process/api/process/ProcessGetConditionApi.java
@@ -73,7 +73,7 @@ public class ProcessGetConditionApi extends ApiComponentBase {
 			commonObj.put("handlerName", condition.getDisplayName());
 			String handlerType = condition.getHandler();
 			if(handlerType.equals("select")) {
-				commonObj.put("isMultiple",condition.getConfig().getString("isMultiple"));
+				commonObj.put("isMultiple",condition.getConfig().getBoolean("isMultiple"));
 			}else if(handlerType.equals("radio")) {
 				commonObj.put("isMultiple",false);
 			}else if(handlerType.equals("checkbox")) {
