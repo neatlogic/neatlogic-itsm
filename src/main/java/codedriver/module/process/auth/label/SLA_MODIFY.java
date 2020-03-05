af0a5d4d3814f69deed83dcb818b92055b0d11a7
吕佐康 <lvzk@techsure.com.cn>
2020-03-05 17:38:00 +0800

    update  auth.label

---
+++ src/main/java/codedriver/module/process/auth/label/SLA_MODIFY.java
@@ -0,0 +1,23 @@
+package codedriver.module.process.auth.label;
+
+import codedriver.framework.auth.core.AuthBase;
+import codedriver.framework.common.constvalue.ModuleEnum;
+
+public class SLA_MODIFY extends AuthBase {
+
+	@Override
+	public String getAuthDisplayName() {
+		return "SLA管理权限";
+	}
+
+	@Override
+	public String getAuthIntroduction() {
+		return "对SLA进行管理";
+	}
+
+	@Override
+	public String getAuthGroup() {
+		return ModuleEnum.PROCESS.getValue();
+	}
+
+}
