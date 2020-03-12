745f76b65b8bb4408e6933157063b822894c5241
林邦泉 <linbq@techsure.com.cn>
2020-03-12 18:26:02 +0800

    --story=1002161 --user=林邦泉 查看页面相关接口 https://www.tapd.cn/54247054/s/1051835

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskStepActionListApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskStepActionListApi.java
@@ -1,27 +1,31 @@
 package codedriver.module.process.api.processtask;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
-import codedriver.module.process.dto.ProcessTaskStepUserAuthorityVo;
+import codedriver.module.process.constvalue.ProcessTaskStepAction;
 @Service
-public class ProcessTaskStepUserAuthorityGetApi extends ApiComponentBase {
+public class ProcessTaskStepActionListApi extends ApiComponentBase {
 
 	@Override
 	public String getToken() {
-		return "processtask/step/userauthority/get";
+		return "processtask/step/action/List";
 	}
 
 	@Override
 	public String getName() {
-		return "工单步骤当前用户权限获取接口";
+		return "工单步骤当前用户操作权限列表获取接口";
 	}
 
 	@Override
@@ -34,13 +38,20 @@ public class ProcessTaskStepUserAuthorityGetApi extends ApiComponentBase {
 		@Param(name = "processTaskStepId", type = ApiParamType.LONG, isRequired = true, desc = "工单步骤id")
 	})
 	@Output({
-		@Param(explode = ProcessTaskStepUserAuthorityVo.class, desc = "权限数据")
+		@Param(name = "Return", explode = ValueTextVo[].class, desc = "当前用户操作权限列表")
 	})
-	@Description(desc = "工单步骤当前用户权限获取接口")
+	@Description(desc = "工单步骤当前用户操作权限列表获取接口")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<ValueTextVo> resultList = new ArrayList<>();
+		for(ProcessTaskStepAction processTaskStepAction : ProcessTaskStepAction.values()) {
+			//TODO linbq 判断当前用户是否有该权限
+			ValueTextVo valueText = new ValueTextVo();
+			valueText.setValue(processTaskStepAction.getValue());
+			valueText.setText(processTaskStepAction.getText());
+			resultList.add(valueText);
+		}
+		return resultList;
 	}
 
 }
