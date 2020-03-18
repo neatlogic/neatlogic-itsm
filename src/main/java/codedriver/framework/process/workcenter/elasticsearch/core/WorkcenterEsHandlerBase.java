4aa5baf89bdfb14bcebe61c23299ac02265eedc8
吕佐康 <lvzk@techsure.com.cn>
2020-03-18 15:37:38 +0800

    update

--- src/main/java/codedriver/framework/process/workcenter/elasticsearch/core/WorkcenterEsHandlerBase.java
+++ src/main/java/codedriver/framework/process/workcenter/elasticsearch/core/WorkcenterEsHandlerBase.java
@@ -3,8 +3,9 @@ package codedriver.framework.process.workcenter.elasticsearch.core;
 import com.techsure.multiattrsearch.MultiAttrsObjectPool;
 
 import codedriver.framework.elasticsearch.core.ElasticSearchPoolManager;
+import codedriver.framework.elasticsearch.core.IElasticSearchHandler;
 
-public abstract class WorkcenterEsHandlerBase implements IWorkcenterESHandler {
+public abstract class WorkcenterEsHandlerBase implements IElasticSearchHandler {
 	public final static String POOL_NAME = "processtask";
 	protected MultiAttrsObjectPool objectPool;
 	
@@ -13,5 +14,9 @@ public abstract class WorkcenterEsHandlerBase implements IWorkcenterESHandler {
 			return ElasticSearchPoolManager.getObjectPool(POOL_NAME);
 		}
 		return objectPool;
+	}
+	
+	public String getPoolName() {
+		return POOL_NAME;
 	}
 }
