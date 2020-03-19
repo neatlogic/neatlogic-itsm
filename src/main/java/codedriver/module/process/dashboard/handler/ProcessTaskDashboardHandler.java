d73f16d787e6697f2dca5b5d79947c32c7a0344d
chenqiwei <chenqw@techsure.com.cn>
2020-03-19 15:17:39 +0800

    adjust

--- src/main/java/codedriver/module/process/dashboard/handler/ProcessTaskDashboardHandler.java
+++ src/main/java/codedriver/module/process/dashboard/handler/ProcessTaskDashboardHandler.java
@@ -1,10 +1,5 @@
 package codedriver.module.process.dashboard.handler;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.ResultContext;
 import org.apache.ibatis.session.ResultHandler;
 import org.springframework.stereotype.Component;
@@ -14,9 +9,7 @@ import com.alibaba.fastjson.JSONObject;
 import codedriver.framework.dashboard.core.DashboardHandlerBase;
 import codedriver.framework.dashboard.dto.ChartDataVo;
 import codedriver.framework.dashboard.dto.DashboardWidgetVo;
-import codedriver.framework.process.workcenter.WorkcenterHandler;
 import codedriver.module.process.dto.ProcessTaskVo;
-import codedriver.module.process.workcenter.dto.WorkcenterVo;
 
 @Component
 public class ProcessTaskDashboardHandler extends DashboardHandlerBase {
@@ -33,43 +26,43 @@ public class ProcessTaskDashboardHandler extends DashboardHandlerBase {
 	 * "sort":"createTime", "limit":10 }
 	 */
 
-	protected ChartDataVo myGetData2(DashboardWidgetVo widgetVo) {
-		JSONObject resultObj = WorkcenterHandler.doSearch(new WorkcenterVo());
-
-		JSONObject chartConfigObj = widgetVo.getChartConfigObj();
-		if (chartConfigObj != null) {
-			final String groupField = chartConfigObj.getString("groupField");
-			final String subGroupField = chartConfigObj.getString("subGroupField");
-			final String aggregate = chartConfigObj.getString("aggregate");
-			final String valueField = chartConfigObj.getString("valueField");
-
-			ChartDataVo chartDataVo = new ChartDataVo();
-			chartDataVo.setGroupField(groupField);
-			chartDataVo.setSubGroupField(subGroupField);
-			chartDataVo.setValueField("value");
-
-			Map<String, Long> resultMap = null;
-			if (aggregate.equals("sum")) {
-				resultMap = resultObj.getJSONArray("tbodyList").stream().collect(Collectors.groupingBy(map -> ((JSONObject) map).getString(groupField) + (StringUtils.isNotBlank(subGroupField) ? "#" + ((JSONObject) map).getString(subGroupField) : ""), Collectors.summingLong(map -> ((JSONObject) map).getLong(valueField))));
-			} else if (aggregate.equals("count")) {
-				resultMap = resultObj.getJSONArray("tbodyList").stream().collect(Collectors.groupingBy(map -> ((JSONObject) map).getString(groupField) + (StringUtils.isNotBlank(subGroupField) ? "#" + ((JSONObject) map).getString(subGroupField) : ""), Collectors.counting()));
-			}
-
-			Iterator<String> it = resultMap.keySet().iterator();
-			while (it.hasNext()) {
-				String key = it.next();
-				for (int i = 0; i < 10; i++) {
-					JSONObject jsonObj = new JSONObject();
-					jsonObj.put("year", Integer.toString(1000 + i));
-					jsonObj.put("value", Math.random() * 100);
-					chartDataVo.addData(jsonObj);
-				}
-			}
-
-		}
-
-		return null;
-	}
+//	protected ChartDataVo myGetData2(DashboardWidgetVo widgetVo) {
+//		JSONObject resultObj = WorkcenterHandler.doSearch(new WorkcenterVo());
+//
+//		JSONObject chartConfigObj = widgetVo.getChartConfigObj();
+//		if (chartConfigObj != null) {
+//			final String groupField = chartConfigObj.getString("groupField");
+//			final String subGroupField = chartConfigObj.getString("subGroupField");
+//			final String aggregate = chartConfigObj.getString("aggregate");
+//			final String valueField = chartConfigObj.getString("valueField");
+//
+//			ChartDataVo chartDataVo = new ChartDataVo();
+//			chartDataVo.setGroupField(groupField);
+//			chartDataVo.setSubGroupField(subGroupField);
+//			chartDataVo.setValueField("value");
+//
+//			Map<String, Long> resultMap = null;
+//			if (aggregate.equals("sum")) {
+//				resultMap = resultObj.getJSONArray("tbodyList").stream().collect(Collectors.groupingBy(map -> ((JSONObject) map).getString(groupField) + (StringUtils.isNotBlank(subGroupField) ? "#" + ((JSONObject) map).getString(subGroupField) : ""), Collectors.summingLong(map -> ((JSONObject) map).getLong(valueField))));
+//			} else if (aggregate.equals("count")) {
+//				resultMap = resultObj.getJSONArray("tbodyList").stream().collect(Collectors.groupingBy(map -> ((JSONObject) map).getString(groupField) + (StringUtils.isNotBlank(subGroupField) ? "#" + ((JSONObject) map).getString(subGroupField) : ""), Collectors.counting()));
+//			}
+//
+//			Iterator<String> it = resultMap.keySet().iterator();
+//			while (it.hasNext()) {
+//				String key = it.next();
+//				for (int i = 0; i < 10; i++) {
+//					JSONObject jsonObj = new JSONObject();
+//					jsonObj.put("year", Integer.toString(1000 + i));
+//					jsonObj.put("value", Math.random() * 100);
+//					chartDataVo.addData(jsonObj);
+//				}
+//			}
+//
+//		}
+//
+//		return null;
+//	}
 	
 	@Override
 	protected ChartDataVo myGetData(DashboardWidgetVo widgetVo) {
