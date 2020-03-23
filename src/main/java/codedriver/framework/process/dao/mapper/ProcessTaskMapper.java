bfe3d3e5f945ba3a0c53fad8e29f6bc726b2e4b3
林邦泉 <linbq@techsure.com.cn>
2020-03-23 19:17:59 +0800

    --story=1002161 --user=林邦泉 查看页面相关接口 https://www.tapd.cn/54247054/s/1053690

--- src/main/java/codedriver/framework/process/dao/mapper/ProcessTaskMapper.java
+++ src/main/java/codedriver/framework/process/dao/mapper/ProcessTaskMapper.java
@@ -123,7 +123,7 @@ public interface ProcessTaskMapper {
 
 	public List<ProcessTaskStepWorkerVo> getProcessTaskStepWorkerList(@Param("userId") String userId, @Param("teamUuidList") List<String> teamUuidList, @Param("roleNameList") List<String> roleNameList);
 
-	public List<Map<String, Object>> getProcessTaskTaskInfoList(@Param("keyword") String keyword, @Param("processTaskStepIdList") List<Long> processTaskStepIdList);
+	public List<Map<String, Object>> getProcessTaskActiveStepListByStepIdList(@Param("keyword") String keyword, @Param("processTaskStepIdList") List<Long> processTaskStepIdList);
 
 	public ProcessTaskFormAttributeDataVo getProcessTaskFormAttributeDataByProcessTaskIdAndAttributeUuid(ProcessTaskFormAttributeDataVo processTaskFormAttributeDataVo);
 
