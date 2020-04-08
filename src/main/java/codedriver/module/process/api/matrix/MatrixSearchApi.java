c15b8653f18c6a51bad2421ab7a2d702f7470c22
lixs <lixs@techsure.com.cn>
2020-04-08 15:47:18 +0800

    参数信息修改

--- src/main/java/codedriver/module/process/api/matrix/MatrixSearchApi.java
+++ src/main/java/codedriver/module/process/api/matrix/MatrixSearchApi.java
@@ -1,6 +1,7 @@
 package codedriver.module.process.api.matrix;
 
 import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.common.dto.BasePageVo;
 import codedriver.framework.process.dto.ProcessMatrixVo;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
@@ -45,10 +46,7 @@ public class MatrixSearchApi extends ApiComponentBase {
              @Param( name = "needPage", desc = "是否分页", type = ApiParamType.BOOLEAN),
              @Param( name = "pageSize", desc = "页面展示数", type = ApiParamType.INTEGER)})
     @Output({ @Param( name = "tbodyList", desc = "矩阵数据源列表", explode = ProcessMatrixVo[].class),
-            @Param( name = "pageCount", desc = "页码数", type = ApiParamType.INTEGER),
-            @Param( name = "rowNum", desc = "统计个数", type = ApiParamType.INTEGER),
-            @Param( name = "pageSize", desc = "页最大条数", type = ApiParamType.INTEGER),
-            @Param( name = "currentPage", desc = "当前页码", type = ApiParamType.INTEGER)})
+            @Param( explode = BasePageVo.class)})
     @Description(desc = "数据源矩阵检索")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
