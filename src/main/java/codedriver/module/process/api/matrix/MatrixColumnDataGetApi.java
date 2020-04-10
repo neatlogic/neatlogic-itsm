1fdd2d57a9064967215925c91d825b46eac3b96d
lixs <lixs@techsure.com.cn>
2020-04-10 18:59:21 +0800

    属性查询调整

--- src/main/java/codedriver/module/process/api/matrix/MatrixColumnDataGetApi.java
+++ src/main/java/codedriver/module/process/api/matrix/MatrixColumnDataGetApi.java
@@ -3,6 +3,7 @@ package codedriver.module.process.api.matrix;
 import codedriver.framework.apiparam.core.ApiParamType;
 import codedriver.framework.process.constvalue.ProcessMatrixType;
 import codedriver.framework.process.dao.mapper.MatrixMapper;
+import codedriver.framework.process.dto.ProcessMatrixColumnVo;
 import codedriver.framework.process.dto.ProcessMatrixDataVo;
 import codedriver.framework.process.dto.ProcessMatrixVo;
 import codedriver.framework.restful.annotation.Description;
@@ -12,6 +13,7 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
 import codedriver.module.process.service.MatrixDataService;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,14 +52,25 @@ public class MatrixColumnDataGetApi extends ApiComponentBase {
 
     @Input({ @Param( name = "matrixUuid", desc = "矩阵Uuid", type = ApiParamType.STRING, isRequired = true),
              @Param( name = "targetColumn", desc = "目标属性ID", type = ApiParamType.STRING, isRequired = true),
-             @Param( name = "sourceColumn", desc = "源属性ID", type = ApiParamType.STRING),
-             @Param( name = "sourceColumnValue", desc = "源属性值", type = ApiParamType.STRING)})
+             @Param( name = "sourceColumnList", desc = "源属性集合", type = ApiParamType.JSONARRAY)})
     @Description(desc = "矩阵属性数据获取接口")
     @Output({ @Param( name = "columnDataList", type = ApiParamType.JSONARRAY, desc = "属性数据集合")})
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
         JSONObject returnObj = new JSONObject();
-        ProcessMatrixDataVo dataVo = JSON.toJavaObject(jsonObj, ProcessMatrixDataVo.class);
+        ProcessMatrixDataVo dataVo = new ProcessMatrixDataVo();
+        dataVo.setMatrixUuid(jsonObj.getString("matrixUuid"));
+        dataVo.setTargetColumn(jsonObj.getString("targetColumn"));
+        List<ProcessMatrixColumnVo> sourceColumnVoList = new ArrayList<>();
+        if (jsonObj.containsKey("sourceColumnList")){
+            JSONArray sourceArray = jsonObj.getJSONArray("sourceColumnList");
+            for (int i = 0; i < sourceArray.size(); i++){
+                JSONObject sourceObj = sourceArray.getJSONObject(i);
+                ProcessMatrixColumnVo sourceColumn = JSON.toJavaObject(sourceObj, ProcessMatrixColumnVo.class);
+                sourceColumnVoList.add(sourceColumn);
+            }
+        }
+        dataVo.setSourceColumnList(sourceColumnVoList);
         ProcessMatrixVo matrixVo = matrixMapper.getMatrixByUuid(jsonObj.getString("matrixUuid"));
         Set<String> columnDataSet = new HashSet<>();
         if (matrixVo.getType().equals(ProcessMatrixType.CUSTOM.getValue())){
