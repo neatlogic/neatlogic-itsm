badd51f6c87759babb8a5ce3fd68aa6655ec8565
林邦泉 <linbq@techsure.com.cn>
2020-04-17 18:39:13 +0800

    --story=1002853 --user=林邦泉 矩阵接口联调 https://www.tapd.cn/54247054/s/1058334

--- src/main/java/codedriver/module/process/api/matrix/MatrixAttributeSaveApi.java
+++ src/main/java/codedriver/module/process/api/matrix/MatrixAttributeSaveApi.java
@@ -46,8 +46,14 @@ public class MatrixAttributeSaveApi extends ApiComponentBase {
         return null;
     }
 
-    @Input({ @Param( name = "matrixUuid", desc = "矩阵uuid", type = ApiParamType.STRING, isRequired = true),
-             @Param( name = "attributeArray", desc = "属性数据", type = ApiParamType.JSONARRAY, isRequired = true)})
+    @Input({ 
+    	@Param( name = "matrixUuid", desc = "矩阵uuid", type = ApiParamType.STRING, isRequired = true),
+        @Param( name = "matrixAttributeList", desc = "属性数据列表", type = ApiParamType.JSONARRAY, isRequired = true),
+        @Param( name = "matrixAttributeList[x].uuid", desc = "属性uuid", type = ApiParamType.STRING, isRequired = true),
+        @Param( name = "matrixAttributeList[x].name", desc = "属性名", type = ApiParamType.STRING, isRequired = true),
+        @Param( name = "matrixAttributeList[x].type", desc = "属性类型", type = ApiParamType.STRING, isRequired = true),
+        @Param( name = "matrixAttributeList[x].config", desc = "属性配置", type = ApiParamType.JSONOBJECT, isRequired = true),
+    })
     @Description( desc = "矩阵属性保存接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
