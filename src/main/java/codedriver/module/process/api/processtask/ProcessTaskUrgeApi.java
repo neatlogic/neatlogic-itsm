7df87a792b7109036d666c799871e78c4f293076
林邦泉 <linbq@techsure.com.cn>
2020-04-18 15:17:24 +0800

    --story=1002161 --user=林邦泉 查看页面相关接口 https://www.tapd.cn/54247054/s/1058413

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskUrgeApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskUrgeApi.java
@@ -1,10 +1,20 @@
 package codedriver.module.process.api.processtask;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.process.constvalue.ProcessTaskStepAction;
+import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
+import codedriver.framework.process.dto.ProcessTaskStepVo;
+import codedriver.framework.process.dto.ProcessTaskStepWorkerVo;
+import codedriver.framework.process.exception.processtask.ProcessTaskNoPermissionException;
+import codedriver.framework.process.stephandler.core.ProcessStepHandlerFactory;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.Param;
@@ -12,6 +22,9 @@ import codedriver.framework.restful.core.ApiComponentBase;
 @Service
 public class ProcessTaskUrgeApi extends ApiComponentBase {
 
+	@Autowired
+	private ProcessTaskMapper processTaskMapper;
+	
 	@Override
 	public String getToken() {
 		return "processtask/urge";
@@ -28,12 +41,21 @@ public class ProcessTaskUrgeApi extends ApiComponentBase {
 	}
 
 	@Input({
-		@Param(name = "processTaskId", type = ApiParamType.LONG, isRequired = true, desc = "工单Id"),
-		@Param(name = "processTaskStepId", type = ApiParamType.LONG, isRequired = true, desc = "当前步骤Id")
+		@Param(name = "processTaskId", type = ApiParamType.LONG, isRequired = true, desc = "工单Id")
 	})
 	@Description(desc = "工单完成接口")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
+		Long processTaskId = jsonObj.getLong("processTaskId");
+		List<ProcessTaskStepVo> processTaskStepList = ProcessStepHandlerFactory.getHandler().getUrgeableStepList(processTaskId);
+		if(CollectionUtils.isNotEmpty(processTaskStepList)) {
+			for(ProcessTaskStepVo processTaskStepVo : processTaskStepList) {
+				List<ProcessTaskStepWorkerVo> processTaskStepWorkerList = processTaskMapper.getProcessTaskStepWorkerByProcessTaskStepId(processTaskStepVo.getId());
+				System.out.println("催办成功..");
+			}
+		}else {
+			throw new ProcessTaskNoPermissionException(ProcessTaskStepAction.URGE.getText());
+		}
 		return null;
 	}
 
