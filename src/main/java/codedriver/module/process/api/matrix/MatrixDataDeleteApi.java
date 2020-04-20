f5f70aa30975a294ab25673a39e0f7d196fafbd8
林邦泉 <linbq@techsure.com.cn>
2020-04-20 18:32:52 +0800

    --story=1002853 --user=林邦泉 矩阵接口联调 https://www.tapd.cn/54247054/s/1058786

--- src/main/java/codedriver/module/process/api/matrix/MatrixDataDeleteApi.java
+++ src/main/java/codedriver/module/process/api/matrix/MatrixDataDeleteApi.java
@@ -8,7 +8,12 @@ import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
+
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -42,7 +47,7 @@ public class MatrixDataDeleteApi extends ApiComponentBase {
     }
 
     @Input({ @Param( name = "matrixUuid", desc = "矩阵uuid", type = ApiParamType.STRING, isRequired = true),
-             @Param( name = "uuid", desc = "矩阵数据uuid", type = ApiParamType.STRING, isRequired = true)})
+             @Param( name = "uuidList", desc = "矩阵数据uuid列表", type = ApiParamType.JSONARRAY, isRequired = true)})
     @Description(desc = "矩阵数据删除接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
@@ -50,7 +55,10 @@ public class MatrixDataDeleteApi extends ApiComponentBase {
 		if(matrixMapper.checkMatrixIsExists(matrixUuid) == 0) {
 			throw new MatrixNotFoundException(matrixUuid);
 		}
-        dataMapper.deleteDynamicTableDataByUuid(matrixUuid, jsonObj.getString("uuid"));
+		List<String> uuidList = JSON.parseArray(jsonObj.getString("uuidList"), String.class);
+		for(String uuid : uuidList) {
+	        dataMapper.deleteDynamicTableDataByUuid(matrixUuid, uuid);
+		}
         return null;
     }
 }
