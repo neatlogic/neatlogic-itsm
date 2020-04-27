7c944bebc4b6f96b9125d22c9ff7ace6fba807e0
林邦泉 <linbq@techsure.com.cn>
2020-04-27 15:20:20 +0800

    --story=1002853 --user=林邦泉 矩阵接口联调 https://www.tapd.cn/54247054/s/1060906

--- src/main/java/codedriver/module/process/api/matrix/MatrixAttributeTypeListApi.java
+++ src/main/java/codedriver/module/process/api/matrix/MatrixAttributeTypeListApi.java
@@ -9,6 +9,9 @@ import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.process.constvalue.ProcessMatrixAttributeType;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.ApiComponentBase;
 @Service
 public class MatrixAttributeTypeListApi extends ApiComponentBase {
@@ -27,7 +30,8 @@ public class MatrixAttributeTypeListApi extends ApiComponentBase {
 	public String getConfig() {
 		return null;
 	}
-
+	@Output({ @Param( name = "Return", desc = "矩阵属性类型列表", explode = ValueTextVo[].class)})
+    @Description(desc = "矩阵属性类型列表接口")
 	@Override
 	public Object myDoService(JSONObject jsonObj) throws Exception {
 		 List<ValueTextVo> typeList = new ArrayList<>();
