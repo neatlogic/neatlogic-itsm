b43038a15ae2a79ceab5f520a6aa84b6a18f5697
林邦泉 <linbq@techsure.com.cn>
2020-04-28 11:26:17 +0800

    --story=1002853 --user=林邦泉 矩阵接口联调 https://www.tapd.cn/54247054/s/1061147

--- src/main/java/codedriver/module/process/api/matrix/MatrixDataSearchApi.java
+++ src/main/java/codedriver/module/process/api/matrix/MatrixDataSearchApi.java
@@ -25,7 +25,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * @program: codedriver
@@ -77,8 +76,9 @@ public class MatrixDataSearchApi extends ApiComponentBase {
     	}
     	List<ProcessMatrixAttributeVo> attributeVoList = attributeMapper.getMatrixAttributeByMatrixUuid(dataVo.getMatrixUuid());
         if (CollectionUtils.isNotEmpty(attributeVoList)){
-        	List<Map<String, String>> tbodyList = dataService.searchDynamicTableData(dataVo);
-        	returnObj.put("tbodyList", dataService.matrixValueHandle(attributeVoList, tbodyList));
+//        	List<Map<String, String>> tbodyList = dataService.searchDynamicTableData(dataVo);
+//        	returnObj.put("tbodyList", dataService.matrixValueHandle(attributeVoList, tbodyList));
+        	returnObj.put("tbodyList", dataService.searchDynamicTableData(dataVo));
             List<ProcessMatrixColumnVo> processMatrixColumnList = dataVo.getSourceColumnList();
             if(CollectionUtils.isNotEmpty(processMatrixColumnList)) {
             	JSONArray headList = new JSONArray();
