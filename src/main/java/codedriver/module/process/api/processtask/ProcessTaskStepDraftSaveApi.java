cd0cf9d8cc309e540d95975b79b2affd71d51846
林邦泉 <linbq@techsure.com.cn>
2020-05-13 18:56:18 +0800

    --story=1002161 --user=林邦泉 查看页面相关接口 https://www.tapd.cn/54247054/s/1064365

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskStepDraftSaveApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskStepDraftSaveApi.java
@@ -68,6 +68,7 @@ public class ProcessTaskStepDraftSaveApi extends ApiComponentBase {
 		@Param(name = "processTaskId", type = ApiParamType.LONG, isRequired = true, desc = "工单id"),
 		@Param(name = "processTaskStepId", type = ApiParamType.LONG, isRequired = true, desc = "步骤id"),
 		@Param(name="formAttributeDataList", type = ApiParamType.JSONARRAY, isRequired = true, desc = "表单属性数据列表"),
+		@Param(name="hidecomponentList", type = ApiParamType.JSONARRAY, desc = "联动隐藏表单属性列表"),//TODO linbq isRequired = true
 		@Param(name = "content", type = ApiParamType.STRING, xss = true, desc = "描述"),
 		@Param(name="fileUuidList", type=ApiParamType.JSONARRAY, desc = "附件uuid列表")
 	})
@@ -100,22 +101,35 @@ public class ProcessTaskStepDraftSaveApi extends ApiComponentBase {
 
 		ProcessTaskFormVo processTaskFormVo = processTaskMapper.getProcessTaskFormByProcessTaskId(processTaskId);
 		if(processTaskFormVo != null && StringUtils.isNotBlank(processTaskFormVo.getFormContent())) {
-			//表单属性显示控制
-			Map<String, String> formAttributeActionMap = new HashMap<>();
-			List<ProcessTaskStepFormAttributeVo> processTaskStepFormAttributeList = processTaskMapper.getProcessTaskStepFormAttributeByProcessTaskStepId(processTaskStepId);
-			if(processTaskStepFormAttributeList.size() > 0) {
-				for(ProcessTaskStepFormAttributeVo processTaskStepFormAttributeVo : processTaskStepFormAttributeList) {
-					formAttributeActionMap.put(processTaskStepFormAttributeVo.getAttributeUuid(), processTaskStepFormAttributeVo.getAction());
-				}
+			//组件联动导致隐藏的属性uuid列表
+			processTaskMapper.deleteProcessTaskStepDynamicHideFormAttributeByProcessTaskStepId(processTaskStepId);
+			List<String> hidecomponentList = JSON.parseArray(jsonObj.getString("hidecomponentList"), String.class);
+			for(String attributeUuid : hidecomponentList) {
+				ProcessTaskStepFormAttributeVo processTaskStepFormAttributeVo = new ProcessTaskStepFormAttributeVo();
+				processTaskStepFormAttributeVo.setProcessTaskId(processTaskId);
+				processTaskStepFormAttributeVo.setProcessTaskStepId(processTaskStepId);
+				processTaskStepFormAttributeVo.setAttributeUuid(attributeUuid);
+				processTaskMapper.insertProcessTaskStepDynamicHideFormAttribute(processTaskStepFormAttributeVo);
 			}
 			//暂存时对表单属性值的修改不写入processtask_formattribute_data表，先保存在暂存活动中，等回复或流转操作时，再写入processtask_formattribute_data表
 			JSONArray formAttributeDataList = jsonObj.getJSONArray("formAttributeDataList");
 			if(CollectionUtils.isNotEmpty(formAttributeDataList)) {
+				//表单属性显示控制
+				Map<String, String> formAttributeActionMap = new HashMap<>();
+				List<ProcessTaskStepFormAttributeVo> processTaskStepFormAttributeList = processTaskMapper.getProcessTaskStepFormAttributeByProcessTaskStepId(processTaskStepId);
+				if(processTaskStepFormAttributeList.size() > 0) {
+					for(ProcessTaskStepFormAttributeVo processTaskStepFormAttributeVo : processTaskStepFormAttributeList) {
+						formAttributeActionMap.put(processTaskStepFormAttributeVo.getAttributeUuid(), processTaskStepFormAttributeVo.getAction());
+					}
+				}
 				List<ProcessTaskFormAttributeDataVo> processTaskFormAttributeDataList = new ArrayList<>(formAttributeDataList.size());
 				for(int i = 0; i < formAttributeDataList.size(); i++) {
 					JSONObject formAttributeDataObj = formAttributeDataList.getJSONObject(i);
 					String attributeUuid = formAttributeDataObj.getString("attributeUuid");
 					if(formAttributeActionMap.get(attributeUuid) != null) {//对于只读或隐藏的属性，当前用户不能修改，不更新数据库中的值，不进行修改前后对比
+						continue;
+					}
+					if(hidecomponentList.contains(attributeUuid)) {
 						continue;
 					}
 					ProcessTaskFormAttributeDataVo attributeData = new ProcessTaskFormAttributeDataVo();
