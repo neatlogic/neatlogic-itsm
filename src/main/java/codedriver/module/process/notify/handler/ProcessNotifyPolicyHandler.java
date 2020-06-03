6b4866e7a3542d132d985198f067d35ad14a828c
林邦泉 <linbq@techsure.com.cn>
2020-06-03 15:26:35 +0800

    --story=1002990 --user=林邦泉 通知配置采用通知策略，以及通知模板优化 https://www.tapd.cn/54247054/s/1068534

--- src/main/java/codedriver/module/process/notify/handler/ProcessNotifyPolicyHandler.java
+++ src/main/java/codedriver/module/process/notify/handler/ProcessNotifyPolicyHandler.java
@@ -38,40 +38,6 @@ public class ProcessNotifyPolicyHandler extends NotifyPolicyHandlerBase {
 	}
 
 	@Override
-	public List<NotifyPolicyParamTypeVo> myParamTypeList() {
-		List<NotifyPolicyParamTypeVo> resultList = new ArrayList<>();
-		String conditionModel = ProcessConditionModel.CUSTOM.getValue();
-		//固定字段条件
-		Map<String, IProcessTaskCondition> conditionMap = ProcessTaskConditionFactory.getConditionComponentMap();
-		for (Map.Entry<String, IProcessTaskCondition> entry : conditionMap.entrySet()) {
-			IProcessTaskCondition condition = entry.getValue();
-			if(ProcessField.getValue(condition.getName())== null) {
-				continue;
-			}
-			NotifyPolicyParamTypeVo notifyPolicyParamTypeVo = new NotifyPolicyParamTypeVo();
-			notifyPolicyParamTypeVo.setHandler(condition.getName());
-			notifyPolicyParamTypeVo.setHandlerName(condition.getDisplayName());
-			notifyPolicyParamTypeVo.setHandlerType(condition.getHandler(conditionModel));
-			if(condition.getConfig() != null) {
-				notifyPolicyParamTypeVo.setConfig(condition.getConfig().toJSONString());
-				notifyPolicyParamTypeVo.setIsMultiple(condition.getConfig().getBoolean("isMultiple"));
-			}
-			notifyPolicyParamTypeVo.setType(condition.getType());
-			notifyPolicyParamTypeVo.setDefaultExpression(condition.getDefaultExpression().getExpression());
-			List<ProcessExpressionVo> expressionList = new ArrayList<>();
-			for(ProcessExpression expression:condition.getExpressionList()) {
-				ProcessExpressionVo processExpressionVo = new ProcessExpressionVo();
-				processExpressionVo.setExpression(expression.getExpression());
-				processExpressionVo.setExpressionName(expression.getExpressionName());
-				expressionList.add(processExpressionVo);
-				notifyPolicyParamTypeVo.setExpressionList(expressionList);			
-			}
-			resultList.add(notifyPolicyParamTypeVo);
-		}
-		return resultList;
-	}
-
-	@Override
 	protected List<NotifyPolicyParamVo> mySystemParamList() {
 		List<NotifyPolicyParamVo> notifyPolicyParamList = new ArrayList<>();
 		//固定字段条件
--- src/main/java/codedriver/module/process/notify/handler/TestNotifyPolicyHandler.java
+++ src/main/java/codedriver/module/process/notify/handler/TestNotifyPolicyHandler.java
@@ -37,39 +37,39 @@ public class TestNotifyPolicyHandler extends NotifyPolicyHandlerBase {
 		return returnList;
 	}
 
-	@Override
-	public List<NotifyPolicyParamTypeVo> myParamTypeList() {
-		List<NotifyPolicyParamTypeVo> resultList = new ArrayList<>();
-		String conditionModel = ProcessConditionModel.CUSTOM.getValue();
-		//固定字段条件
-		Map<String, IProcessTaskCondition> conditionMap = ProcessTaskConditionFactory.getConditionComponentMap();
-		for (Map.Entry<String, IProcessTaskCondition> entry : conditionMap.entrySet()) {
-			IProcessTaskCondition condition = entry.getValue();
-			if(ProcessField.getValue(condition.getName())== null) {
-				continue;
-			}
-			NotifyPolicyParamTypeVo notifyPolicyParamTypeVo = new NotifyPolicyParamTypeVo();
-			notifyPolicyParamTypeVo.setHandler(condition.getName());
-			notifyPolicyParamTypeVo.setHandlerName(condition.getDisplayName());
-			notifyPolicyParamTypeVo.setHandlerType(condition.getHandler(conditionModel));
-			if(condition.getConfig() != null) {
-				notifyPolicyParamTypeVo.setConfig(condition.getConfig().toJSONString());
-				notifyPolicyParamTypeVo.setIsMultiple(condition.getConfig().getBoolean("isMultiple"));
-			}
-			notifyPolicyParamTypeVo.setType(condition.getType());
-			notifyPolicyParamTypeVo.setDefaultExpression(condition.getDefaultExpression().getExpression());
-			List<ProcessExpressionVo> expressionList = new ArrayList<>();
-			for(ProcessExpression expression:condition.getExpressionList()) {
-				ProcessExpressionVo processExpressionVo = new ProcessExpressionVo();
-				processExpressionVo.setExpression(expression.getExpression());
-				processExpressionVo.setExpressionName(expression.getExpressionName());
-				expressionList.add(processExpressionVo);
-				notifyPolicyParamTypeVo.setExpressionList(expressionList);			
-			}
-			resultList.add(notifyPolicyParamTypeVo);
-		}
-		return resultList;
-	}
+//	@Override
+//	public List<NotifyPolicyParamTypeVo> myParamTypeList() {
+//		List<NotifyPolicyParamTypeVo> resultList = new ArrayList<>();
+//		String conditionModel = ProcessConditionModel.CUSTOM.getValue();
+//		//固定字段条件
+//		Map<String, IProcessTaskCondition> conditionMap = ProcessTaskConditionFactory.getConditionComponentMap();
+//		for (Map.Entry<String, IProcessTaskCondition> entry : conditionMap.entrySet()) {
+//			IProcessTaskCondition condition = entry.getValue();
+//			if(ProcessField.getValue(condition.getName())== null) {
+//				continue;
+//			}
+//			NotifyPolicyParamTypeVo notifyPolicyParamTypeVo = new NotifyPolicyParamTypeVo();
+//			notifyPolicyParamTypeVo.setHandler(condition.getName());
+//			notifyPolicyParamTypeVo.setHandlerName(condition.getDisplayName());
+//			notifyPolicyParamTypeVo.setHandlerType(condition.getHandler(conditionModel));
+//			if(condition.getConfig() != null) {
+//				notifyPolicyParamTypeVo.setConfig(condition.getConfig().toJSONString());
+//				notifyPolicyParamTypeVo.setIsMultiple(condition.getConfig().getBoolean("isMultiple"));
+//			}
+//			notifyPolicyParamTypeVo.setType(condition.getType());
+//			notifyPolicyParamTypeVo.setDefaultExpression(condition.getDefaultExpression().getExpression());
+//			List<ProcessExpressionVo> expressionList = new ArrayList<>();
+//			for(ProcessExpression expression:condition.getExpressionList()) {
+//				ProcessExpressionVo processExpressionVo = new ProcessExpressionVo();
+//				processExpressionVo.setExpression(expression.getExpression());
+//				processExpressionVo.setExpressionName(expression.getExpressionName());
+//				expressionList.add(processExpressionVo);
+//				notifyPolicyParamTypeVo.setExpressionList(expressionList);			
+//			}
+//			resultList.add(notifyPolicyParamTypeVo);
+//		}
+//		return resultList;
+//	}
 
 	@Override
 	protected List<NotifyPolicyParamVo> mySystemParamList() {
