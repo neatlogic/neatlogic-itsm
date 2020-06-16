b214b98b38adafd5415600b699e49c5154eb138b
林邦泉 <linbq@techsure.com.cn>
2020-06-16 10:43:57 +0800

    --story=1003036 --user=林邦泉 增加分组中“公司、中心、部门、组”等分类标识 https://www.tapd.cn/54247054/s/1071344

--- src/main/java/codedriver/module/process/workerdispatcher/handler/LeaderDispatcher.java
+++ src/main/java/codedriver/module/process/workerdispatcher/handler/LeaderDispatcher.java
@@ -3,6 +3,7 @@ package codedriver.module.process.workerdispatcher.handler;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,13 @@ import org.springframework.stereotype.Service;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import codedriver.framework.common.constvalue.GroupSearch;
+import codedriver.framework.common.constvalue.TeamLevel;
+import codedriver.framework.common.constvalue.TeamUserTitle;
+import codedriver.framework.common.dto.ValueTextVo;
+import codedriver.framework.dao.mapper.TeamMapper;
 import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.dto.TeamVo;
 import codedriver.framework.process.dto.ProcessTaskStepVo;
 import codedriver.framework.process.workerdispatcher.core.WorkerDispatcherBase;
 
@@ -18,6 +25,8 @@ import codedriver.framework.process.workerdispatcher.core.WorkerDispatcherBase;
 public class LeaderDispatcher extends WorkerDispatcherBase {
 	@Autowired
 	private UserMapper userMapper;
+	@Autowired
+	private TeamMapper teamMapper;
 
 	@Override
 	public String getHandler() {
@@ -31,15 +40,26 @@ public class LeaderDispatcher extends WorkerDispatcherBase {
 
 	@Override
 	protected List<String> myGetWorker(ProcessTaskStepVo processTaskStepVo, JSONObject configObj) {
-		if (configObj != null && configObj.containsKey("value")) {
-			String teamUuid = configObj.getString("value");
-			if (StringUtils.isNotBlank(teamUuid)) {
-				List<String> teamIdList = new ArrayList<>();
-				teamIdList.add(teamUuid);
-				return userMapper.getLeaderUserUuidByTeamIds(teamIdList);
+//		if (configObj != null && configObj.containsKey("value")) {
+//			String teamUuid = configObj.getString("value");
+//			if (StringUtils.isNotBlank(teamUuid)) {
+//				List<String> teamIdList = new ArrayList<>();
+//				teamIdList.add(teamUuid);
+//				return userMapper.getLeaderUserUuidByTeamIds(teamIdList);
+//			}
+//		}
+		List<String> resultList = new ArrayList<>();
+		if(MapUtils.isNotEmpty(configObj)) {
+			String teamFilter = configObj.getString("teamFilter");
+			if(StringUtils.isNotBlank(teamFilter) && teamFilter.startsWith(GroupSearch.TEAM.getValuePlugin())) {
+				String[] split = teamFilter.split("#");
+				TeamVo teamVo = teamMapper.getTeamByUuid(split[1]);
+				if(teamVo != null) {
+					
+				}
 			}
 		}
-		return null;
+		return resultList;
 	}
 
 	@Override
@@ -50,13 +70,39 @@ public class LeaderDispatcher extends WorkerDispatcherBase {
 	@Override
 	public JSONArray getConfig() {
 		JSONArray resultArray = new JSONArray();
-		JSONObject configObj = new JSONObject();
-		configObj.put("plugin", "teamFilter");
-		configObj.put("pluginName", "处理组");
-		JSONObject pluginConfigObj = new JSONObject();
-		pluginConfigObj.put("isMultiple", false);
-		configObj.put("config",pluginConfigObj);
-		resultArray.add(configObj);
+		JSONObject teamFilterConfigObj = new JSONObject();
+		teamFilterConfigObj.put("plugin", "teamFilter");
+		teamFilterConfigObj.put("pluginName", "处理组");
+		JSONObject teamFilterPluginConfigObj = new JSONObject();
+		teamFilterPluginConfigObj.put("isMultiple", false);
+		teamFilterConfigObj.put("config",teamFilterPluginConfigObj);
+		resultArray.add(teamFilterConfigObj);
+		
+//		JSONObject teamLevelFilterConfigObj = new JSONObject();
+//		teamLevelFilterConfigObj.put("plugin", "teamLevelFilter");
+//		teamLevelFilterConfigObj.put("pluginName", "领导级别");
+//		JSONObject teamLevelFilterPluginConfigObj = new JSONObject();
+//		teamLevelFilterPluginConfigObj.put("isMultiple", false);
+//		List<ValueTextVo> teamLevelFilterDataList = new ArrayList<>();
+//		for(TeamLevel level : TeamLevel.values()) {
+//			teamLevelFilterDataList.add(new ValueTextVo(level.getValue(), level.getText()));
+//		}
+//		teamLevelFilterPluginConfigObj.put("dataList", teamLevelFilterDataList);
+//		teamLevelFilterConfigObj.put("config",teamLevelFilterPluginConfigObj);
+//		resultArray.add(teamLevelFilterConfigObj);
+			
+		JSONObject teamUserTitleFilterConfigObj = new JSONObject();
+		teamUserTitleFilterConfigObj.put("plugin", "teamUserTitleFilter");
+		teamUserTitleFilterConfigObj.put("pluginName", "头衔");
+		JSONObject teamUserTitleFilterPluginConfigObj = new JSONObject();
+		teamUserTitleFilterPluginConfigObj.put("isMultiple", false);
+		List<ValueTextVo> teamUserTitleFilterDataList = new ArrayList<>();
+		for(TeamUserTitle title : TeamUserTitle.values()) {
+			teamUserTitleFilterDataList.add(new ValueTextVo(title.getValue(), title.getText()));
+		}
+		teamUserTitleFilterPluginConfigObj.put("dataList", teamUserTitleFilterDataList);
+		teamUserTitleFilterConfigObj.put("config",teamUserTitleFilterPluginConfigObj);
+		resultArray.add(teamUserTitleFilterConfigObj);
 		return resultArray;
 	}
 
