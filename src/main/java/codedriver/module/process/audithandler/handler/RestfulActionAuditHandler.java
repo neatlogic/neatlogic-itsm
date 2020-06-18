866b97617aad6f38072dcfba9b5c8e053967af3e
林邦泉 <linbq@techsure.com.cn>
2020-06-18 17:40:35 +0800

    --story=1003045 --user=林邦泉 restful事件-普通节点增加“动作设置” https://www.tapd.cn/54247054/s/1072025

---
+++ src/main/java/codedriver/module/process/audithandler/handler/RestfulActionAuditHandler.java
@@ -0,0 +1,50 @@
+package codedriver.module.process.audithandler.handler;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+
+import codedriver.framework.integration.dao.mapper.IntegrationMapper;
+import codedriver.framework.integration.dto.IntegrationVo;
+import codedriver.framework.process.audithandler.core.IProcessTaskStepAuditDetailHandler;
+import codedriver.framework.process.constvalue.ProcessTaskAuditDetailType;
+import codedriver.framework.process.dto.ActionVo;
+import codedriver.framework.process.dto.ProcessTaskStepAuditDetailVo;
+import codedriver.framework.process.notify.core.NotifyTriggerType;
+
+@Service
+public class RestfulActionAuditHandler implements IProcessTaskStepAuditDetailHandler {
+
+	@Autowired
+	private IntegrationMapper integrationMapper;
+	
+	@Override
+	public String getType() {
+		return ProcessTaskAuditDetailType.RESTFULACTION.getValue();
+	}
+
+	@Override
+	public void handle(ProcessTaskStepAuditDetailVo processTaskStepAuditDetailVo) {
+		if(StringUtils.isNotBlank(processTaskStepAuditDetailVo.getNewContent())) {
+			ActionVo actionVo = JSON.parseObject(processTaskStepAuditDetailVo.getNewContent(), ActionVo.class);
+			IntegrationVo integrationVo = integrationMapper.getIntegrationByUuid(actionVo.getIntegrationUuid());
+			if(integrationVo != null) {
+				actionVo.setIntegrationName(integrationVo.getName());
+			}
+			String triggerText = NotifyTriggerType.getText(actionVo.getTrigger());
+			if(StringUtils.isNotBlank(triggerText)) {
+				actionVo.setTriggerText(triggerText);
+			}
+			if(actionVo.isSucceed()) {
+				actionVo.setStatusText("已成功");
+			}else {
+				actionVo.setStatusText("已失败");
+			}
+			processTaskStepAuditDetailVo.setNewContent(JSON.toJSONString(actionVo));
+		}
+		
+	}
+
+}
