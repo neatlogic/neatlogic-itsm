eb682f634a9e918c0158c90db5382eb9fb683650
林邦泉 <linbq@techsure.com.cn>
2020-06-22 16:54:46 +0800

    --bug=1004766 --user=林邦泉 工单处理-工单流转时，必填的表单组件被隐藏，工单流转时却出现必填校验 https://www.tapd.cn/54247054/s/1072770

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskCommentApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskCommentApi.java
@@ -109,17 +109,7 @@ public class ProcessTaskCommentApi extends ApiComponentBase {
 		}
 		handler.verifyActionAuthoriy(processTaskId, processTaskStepId, ProcessTaskStepAction.COMMENT);
 		
-		//删除暂存活动
-//		ProcessTaskStepAuditVo auditVo = new ProcessTaskStepAuditVo();
-//		auditVo.setProcessTaskId(processTaskId);
-//		auditVo.setProcessTaskStepId(processTaskStepId);
-//		auditVo.setAction(ProcessTaskStepAction.SAVE.getValue());
-//		auditVo.setUserUuid(UserContext.get().getUserUuid(true));
-//		List<ProcessTaskStepAuditVo> processTaskStepAuditList = processTaskMapper.getProcessTaskStepAuditList(auditVo);
-//		for(ProcessTaskStepAuditVo processTaskStepAudit : processTaskStepAuditList) {
-//			processTaskMapper.deleteProcessTaskStepAuditById(processTaskStepAudit.getId());
-//		}
-
+		//删除暂存
 		ProcessTaskStepDataVo processTaskStepDataVo = new ProcessTaskStepDataVo();
 		processTaskStepDataVo.setProcessTaskId(processTaskId);
 		processTaskStepDataVo.setProcessTaskStepId(processTaskStepId);
@@ -127,22 +117,12 @@ public class ProcessTaskCommentApi extends ApiComponentBase {
 		processTaskStepDataVo.setType("stepDraftSave");
 		processTaskStepDataMapper.deleteProcessTaskStepData(processTaskStepDataVo);
 		//组件联动导致隐藏的属性uuid列表
-//		processTaskMapper.deleteProcessTaskStepDynamicHideFormAttributeByProcessTaskStepId(processTaskStepId);
+
 		List<String> hidecomponentList = JSON.parseArray(jsonObj.getString("hidecomponentList"), String.class);
 		if(hidecomponentList == null) {
 			hidecomponentList = new ArrayList<>();
 		}
-//		if(CollectionUtils.isNotEmpty(hidecomponentList)) {
-//			for(String attributeUuid : hidecomponentList) {
-//				ProcessTaskStepFormAttributeVo processTaskStepFormAttributeVo = new ProcessTaskStepFormAttributeVo();
-//				processTaskStepFormAttributeVo.setProcessTaskId(processTaskId);
-//				processTaskStepFormAttributeVo.setProcessTaskStepId(processTaskStepId);
-//				processTaskStepFormAttributeVo.setAttributeUuid(attributeUuid);
-//				processTaskMapper.insertProcessTaskStepDynamicHideFormAttribute(processTaskStepFormAttributeVo);
-//			}				
-//		}else {
-//			hidecomponentList = new ArrayList<>();
-//		}
+
 		//表单属性显示控制
 		Map<String, String> formAttributeActionMap = new HashMap<>();
 		List<ProcessTaskStepFormAttributeVo> processTaskStepFormAttributeList = processTaskMapper.getProcessTaskStepFormAttributeByProcessTaskStepId(processTaskStepId);
