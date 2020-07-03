d9582ced125bd50103a79366bbec5ade007bdbd7
林邦泉 <linbq@techsure.com.cn>
2020-07-03 19:08:41 +0800

    --story=1003096 --user=林邦泉 优化流程管理-节点设置-分配处理人中的表单值、分派器 https://www.tapd.cn/54247054/s/1075701

--- src/main/java/codedriver/module/process/workerdispatcher/handler/WorkloadDispatcher.java
+++ src/main/java/codedriver/module/process/workerdispatcher/handler/WorkloadDispatcher.java
@@ -1,29 +1,73 @@
 package codedriver.module.process.workerdispatcher.handler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import codedriver.framework.common.constvalue.GroupSearch;
+import codedriver.framework.dao.mapper.TeamMapper;
+import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.dto.TeamUserVo;
+import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
 import codedriver.framework.process.dto.ProcessTaskStepVo;
+import codedriver.framework.process.dto.ProcessTaskStepWorkerVo;
 import codedriver.framework.process.workerdispatcher.core.WorkerDispatcherBase;
 
 @Component
 public class WorkloadDispatcher extends WorkerDispatcherBase {
+	
+	@Autowired
+	private TeamMapper teamMapper;
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Autowired
+	private ProcessTaskMapper processTaskMapper;
 
 	@Override
 	public String getName() {
 		return "根据工作量分配处理人";
-	}
-
-	
+	}	
 
 	@Override
 	protected List<String> myGetWorker(ProcessTaskStepVo processTaskStepVo, JSONObject configObj) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> resultList = new ArrayList<>();
+		if(MapUtils.isNotEmpty(configObj)) {
+			String team = configObj.getString("team");
+			if(StringUtils.isNotBlank(team) && team.startsWith(GroupSearch.TEAM.getValuePlugin())) {
+				String[] split = team.split("#");
+				List<TeamUserVo> teamUserList = teamMapper.getTeamUserListByTeamUuid(split[1]);
+				if(CollectionUtils.isNotEmpty(teamUserList)) {
+					String targerUserUuid = null;
+					int minWorkload = Integer.MAX_VALUE;
+					for(TeamUserVo teamUser : teamUserList) {
+						List<String> roleUuidList = userMapper.getRoleUuidListByUserUuid(teamUser.getUserUuid());
+						List<String> teamUuidList = userMapper.getTeamUuidListByUserUuid(teamUser.getUserUuid());
+						/** 查出每个组员的工单量 **/
+						List<ProcessTaskStepWorkerVo> processTaskStepWorkerList = processTaskMapper.getProcessTaskStepWorkerListByUserUuidTeamUuidListRoleUuidList(teamUser.getUserUuid(), teamUuidList, roleUuidList);
+						/** 找出工单量较小的组员 **/
+						if(processTaskStepWorkerList.size() < minWorkload) {
+							minWorkload = processTaskStepWorkerList.size();
+							targerUserUuid = teamUser.getUserUuid();
+						}						
+					}
+					if(StringUtils.isNotBlank(targerUserUuid)) {
+						resultList.add(targerUserUuid);
+					}
+				}
+			}
+		}
+		return resultList;
 	}
 
 	@Override
@@ -33,7 +77,19 @@ public class WorkloadDispatcher extends WorkerDispatcherBase {
 
 	@Override
 	public JSONArray getConfig() {
-		return null;
+		JSONArray resultArray = new JSONArray();
+		/** 选择处理组 **/
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("type", "userselect");
+		jsonObj.put("name", "team");
+		jsonObj.put("label", "处理组");
+		jsonObj.put("validateList", Arrays.asList("required"));
+		jsonObj.put("multiple", false);
+		jsonObj.put("value", "");
+		jsonObj.put("defaultValue", "");
+		jsonObj.put("groupList", Arrays.asList("team"));
+		resultArray.add(jsonObj);
+		return resultArray;
 	}
 
 }
--- src/main/java/codedriver/module/process/workerpolicy/handler/FormWorkerPolicyHandler.java
+++ src/main/java/codedriver/module/process/workerpolicy/handler/FormWorkerPolicyHandler.java
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Service;
 import com.alibaba.fastjson.JSON;
 
 import codedriver.framework.common.constvalue.GroupSearch;
+import codedriver.framework.dao.mapper.RoleMapper;
+import codedriver.framework.dao.mapper.TeamMapper;
 import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.process.constvalue.ProcessFormHandler;
 import codedriver.framework.process.constvalue.WorkerPolicy;
 import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
 import codedriver.framework.process.dto.ProcessTaskFormAttributeDataVo;
@@ -39,47 +42,64 @@ public class FormWorkerPolicyHandler implements IWorkerPolicyHandler {
 	
 	@Autowired
 	private UserMapper userMapper;
+	
+	@Autowired
+	private TeamMapper teamMapper;
+	
+	@Autowired
+	private RoleMapper roleMapper;
 
 	@Override
 	public List<ProcessTaskStepWorkerVo> execute(ProcessTaskStepWorkerPolicyVo workerPolicyVo, ProcessTaskStepVo currentProcessTaskStepVo) {
 		List<ProcessTaskStepWorkerVo> processTaskStepWorkerList = new ArrayList<>();
-		if (MapUtils.isEmpty(workerPolicyVo.getConfigObj())) {
-			return processTaskStepWorkerList;
-		}
-		String attributeUuid = workerPolicyVo.getConfigObj().getString("attributeUuid");
-		if(StringUtils.isBlank(attributeUuid)) {
-			return processTaskStepWorkerList;
-		}
-		List<ProcessTaskFormAttributeDataVo> processTaskFormAttributeDataList = processTaskMapper.getProcessTaskStepFormAttributeDataByProcessTaskId(currentProcessTaskStepVo.getProcessTaskId());
-		if(CollectionUtils.isEmpty(processTaskFormAttributeDataList)) {
-			return processTaskStepWorkerList;
-		}
-		for(ProcessTaskFormAttributeDataVo processTaskFormAttributeDataVo : processTaskFormAttributeDataList) {
-			if(!attributeUuid.equals(processTaskFormAttributeDataVo.getAttributeUuid())) {
-				continue;
-			}
-			String data = processTaskFormAttributeDataVo.getData();
-			if(StringUtils.isBlank(data)) {
-				return processTaskStepWorkerList;
-			}
-			if(data.startsWith("[") && data.endsWith("]")) {
-				List<String> dataList = JSON.parseArray(data, String.class);
-				if(CollectionUtils.isEmpty(dataList)) {
-					return processTaskStepWorkerList;
-				}
-				for(String userUuid : dataList) {
-					if(userMapper.getUserBaseInfoByUuid(userUuid) != null) {
-						processTaskStepWorkerList.add(new ProcessTaskStepWorkerVo(currentProcessTaskStepVo.getProcessTaskId(), currentProcessTaskStepVo.getId(), GroupSearch.USER.getValue(), userUuid));
+		if (MapUtils.isNotEmpty(workerPolicyVo.getConfigObj())) {
+			/** 选择的表单属性uuid **/
+			String attributeUuid = workerPolicyVo.getConfigObj().getString("attributeUuid");
+			if(StringUtils.isNotBlank(attributeUuid)) {
+				ProcessTaskFormAttributeDataVo processTaskFormAttributeDataVo = new ProcessTaskFormAttributeDataVo();
+				processTaskFormAttributeDataVo.setProcessTaskId(currentProcessTaskStepVo.getProcessTaskId());
+				processTaskFormAttributeDataVo.setAttributeUuid(attributeUuid);
+				ProcessTaskFormAttributeDataVo processTaskFormAttributeData = processTaskMapper.getProcessTaskFormAttributeDataByProcessTaskIdAndAttributeUuid(processTaskFormAttributeDataVo);
+				if(processTaskFormAttributeData != null) {
+					/** 只有表单属性类型为用户选择器才生效**/
+					if(ProcessFormHandler.FORMUSERSELECT.getHandler().equals(processTaskFormAttributeData.getType())) {
+						String data = processTaskFormAttributeData.getData();
+						if(StringUtils.isNotBlank(data)) {
+							List<String> dataList = new ArrayList<>();
+							if(data.startsWith("[") && data.endsWith("]")) {
+								dataList = JSON.parseArray(data, String.class);
+							}else {
+								dataList.add(data);
+							}
+							if(CollectionUtils.isNotEmpty(dataList)) {
+								for(String value : dataList) {
+									/** 校验属性值是否合法，只有是当前存在的用户、组、角色才合法**/
+									if(value.contains("#")) {
+										String[] split = value.split("#");
+										if(GroupSearch.USER.getValue().equals(split[0])) {
+											if(userMapper.checkUserIsExists(split[1]) == 0) {
+												continue;
+											}
+										}else if(GroupSearch.TEAM.getValue().equals(split[0])) {
+											if(teamMapper.checkTeamIsExists(split[1]) == 0) {
+												continue;
+											}
+										}else if(GroupSearch.ROLE.getValue().equals(split[0])) {
+											if(roleMapper.checkRoleIsExists(split[1]) == 0) {
+												continue;
+											}
+										}else {
+											continue;
+										}
+										processTaskStepWorkerList.add(new ProcessTaskStepWorkerVo(currentProcessTaskStepVo.getProcessTaskId(), currentProcessTaskStepVo.getId(), split[0], split[1]));
+									}
+								}
+							}
+						}
 					}
 				}
-			}else {
-				if(userMapper.getUserBaseInfoByUuid(data) != null) {
-					processTaskStepWorkerList.add(new ProcessTaskStepWorkerVo(currentProcessTaskStepVo.getProcessTaskId(), currentProcessTaskStepVo.getId(), GroupSearch.USER.getValue(), data));				
-				}
-			}
-			return processTaskStepWorkerList;
+			}			
 		}
-		//List<ProcessTaskAssignUserVo> assignUserList = processTaskMapper.getProcessAssignUserByToStepId(currentProcessTaskStepVo.getId());
 		return processTaskStepWorkerList;
 	}
 }
