eaa54ea609befffb7a1065e72b3a67c6e683293b
林邦泉 <linbq@techsure.com.cn>
2020-08-31 17:58:28 +0800

    --story=1003155 --user=林邦泉 增加变更节点，变更上报，变更处理，变更模板 https://www.tapd.cn/54247054/s/1085242

--- src/main/java/codedriver/module/process/service/WorkcenterService.java
+++ src/main/java/codedriver/module/process/service/WorkcenterService.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -218,6 +219,7 @@ public class WorkcenterService {
 	 * @param MultiAttrsObject el
 	 * @return
 	 */
+	@Transactional
 	public Object getStepAction(MultiAttrsObject el) {
 		JSONArray actionArray = new JSONArray();
 		JSONObject commonJson = (JSONObject) el.getJSON(ProcessFieldType.COMMON.getValue());
