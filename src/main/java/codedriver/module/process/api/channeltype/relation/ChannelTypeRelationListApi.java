ada7d8babf68a61acaf0c587bfae88f8bd577e2b
林邦泉 <linbq@techsure.com.cn>
2020-09-07 17:54:33 +0800

    --story=1003281 --user=林邦泉 事件转报逻辑 https://www.tapd.cn/54247054/s/1086278

---
+++ src/main/java/codedriver/module/process/api/channeltype/relation/ChannelTypeRelationListApi.java
@@ -0,0 +1,78 @@
+package codedriver.module.process.api.channeltype.relation;
+
+import codedriver.framework.reminder.core.OperationTypeEnum;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BasePageVo;
+import codedriver.framework.common.util.PageUtil;
+import codedriver.framework.process.dao.mapper.ChannelMapper;
+import codedriver.framework.process.dto.ChannelTypeRelationVo;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class ChannelTypeRelationListApi extends PrivateApiComponentBase {
+
+	@Autowired
+	private ChannelMapper channelMapper;
+
+	@Override
+	public String getToken() {
+		return "process/channeltype/relation/list";
+	}
+
+	@Override
+	public String getName() {
+		return "查询服务类型关系列表";
+	}
+
+	@Override
+	public String getConfig() {
+		return null;
+	}
+
+	@Input({
+		@Param(name = "keyword", type = ApiParamType.STRING, isRequired = true, desc = "关系名称，关键字搜索"),
+        @Param(name = "isActive", type = ApiParamType.ENUM, desc = "是否激活", rule = "0,1"),
+        @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true"),
+        @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页条目"),
+        @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页")
+	})
+	@Output({
+		@Param(name = "tbodyList", explode = ChannelTypeRelationVo[].class, desc = "服务类型关系列表"),
+		@Param(explode = BasePageVo.class)
+	})
+	@Description(desc = "查询服务类型关系列表")
+	@Override
+	public Object myDoService(JSONObject jsonObj) throws Exception {
+	    JSONObject resultObj = new JSONObject();
+	    resultObj.put("tbodyList", new ArrayList<>());
+ 	    BasePageVo basePageVo = JSON.toJavaObject(jsonObj, BasePageVo.class);
+ 	    int pageCount = 0;
+ 	    if(basePageVo.getNeedPage()) {
+ 	        int rowNum = channelMapper.getChannelTypeRelationCount(basePageVo);
+ 	        pageCount = PageUtil.getPageCount(rowNum, basePageVo.getPageSize());
+ 	        resultObj.put("currentPage", basePageVo.getCurrentPage());
+ 	        resultObj.put("pageSize", basePageVo.getPageSize());
+ 	        resultObj.put("pageCount", pageCount);
+ 	        resultObj.put("rowNum", rowNum);
+ 	    }
+ 	   if(!basePageVo.getNeedPage() || basePageVo.getCurrentPage() <= pageCount) {
+ 	        List<ChannelTypeRelationVo> channelTypeRelationList = channelMapper.getChannelTypeRelationList(basePageVo);
+ 	        //TODO linbq引用数量
+ 	       resultObj.put("tbodyList", channelTypeRelationList);
+	    }
+		return resultObj;
+	}
+
+}
