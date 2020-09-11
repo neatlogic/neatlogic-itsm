323785b8083d9ef63f4cb7cdc89e602cf80e7675
林邦泉 <linbq@techsure.com.cn>
2020-09-11 18:01:01 +0800

    --story=1003281 --user=林邦泉 事件转报逻辑 https://www.tapd.cn/54247054/s/1087690

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskRelationListApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskRelationListApi.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +18,12 @@ import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.dto.BasePageVo;
 import codedriver.framework.common.util.PageUtil;
 import codedriver.framework.process.constvalue.ProcessTaskOperationType;
+import codedriver.framework.process.dao.mapper.ChannelMapper;
 import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
+import codedriver.framework.process.dto.ChannelTypeRelationVo;
+import codedriver.framework.process.dto.ChannelTypeVo;
+import codedriver.framework.process.dto.ChannelVo;
 import codedriver.framework.process.dto.ProcessTaskRelationVo;
-import codedriver.framework.process.dto.ProcessTaskStepAuditDetailVo;
-import codedriver.framework.process.dto.ProcessTaskStepAuditVo;
 import codedriver.framework.process.dto.ProcessTaskVo;
 import codedriver.framework.process.stephandler.core.ProcessStepUtilHandlerFactory;
 import codedriver.framework.reminder.core.OperationTypeEnum;
@@ -38,6 +41,9 @@ public class ProcessTaskRelationListApi extends PrivateApiComponentBase {
 
     @Autowired
     private ProcessTaskMapper processTaskMapper;
+    
+    @Autowired
+    private ChannelMapper channelMapper;
     
     @Autowired
     private ProcessTaskService processTaskService;
@@ -70,33 +76,63 @@ public class ProcessTaskRelationListApi extends PrivateApiComponentBase {
     @Description(desc = "查询关联工单列表")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-//        ProcessTaskRelationVo processTaskRelationVo = JSON.toJavaObject(jsonObj, ProcessTaskRelationVo.class);
-//        ProcessTaskVo processTaskVo = processTaskService.checkProcessTaskParamsIsLegal(processTaskRelationVo.getProcessTaskId());
-//        ProcessStepUtilHandlerFactory.getHandler().verifyOperationAuthoriy(processTaskVo, ProcessTaskOperationType.POCESSTASKVIEW, true);
-//        JSONObject resultObj = new JSONObject();
-//        resultObj.put("processTaskRelationList", new ArrayList<>());
-//        int pageCount = 0;
-//        if(processTaskRelationVo.getNeedPage()) {
-//            int rowNum = processTaskMapper.getProcessTaskRelationCount(processTaskRelationVo.getProcessTaskId());
-//            pageCount = PageUtil.getPageCount(rowNum, processTaskRelationVo.getPageSize());
-//            resultObj.put("currentPage", processTaskRelationVo.getCurrentPage());
-//            resultObj.put("pageSize", processTaskRelationVo.getPageSize());
-//            resultObj.put("pageCount", pageCount);
-//            resultObj.put("rowNum", rowNum);           
-//        }
-//        if(!processTaskRelationVo.getNeedPage() || processTaskRelationVo.getCurrentPage() <= pageCount) {
-//            Map<Long, ProcessTaskRelationVo> processTaskRelationMap = new HashMap<>();
-//            Set<Long> processTaskSet = new HashSet<>();
+        ProcessTaskRelationVo processTaskRelationVo = JSON.toJavaObject(jsonObj, ProcessTaskRelationVo.class);
+        ProcessTaskVo processTaskVo = processTaskService.checkProcessTaskParamsIsLegal(processTaskRelationVo.getProcessTaskId());
+        ProcessStepUtilHandlerFactory.getHandler().verifyOperationAuthoriy(processTaskVo, ProcessTaskOperationType.POCESSTASKVIEW, true);
+        JSONObject resultObj = new JSONObject();
+        resultObj.put("processTaskRelationList", new ArrayList<>());
+        int pageCount = 0;
+        if(processTaskRelationVo.getNeedPage()) {
+            int rowNum = processTaskMapper.getProcessTaskRelationCountByProcessTaskId(processTaskRelationVo.getProcessTaskId());
+            pageCount = PageUtil.getPageCount(rowNum, processTaskRelationVo.getPageSize());
+            resultObj.put("currentPage", processTaskRelationVo.getCurrentPage());
+            resultObj.put("pageSize", processTaskRelationVo.getPageSize());
+            resultObj.put("pageCount", pageCount);
+            resultObj.put("rowNum", rowNum);           
+        }
+        if(!processTaskRelationVo.getNeedPage() || processTaskRelationVo.getCurrentPage() <= pageCount) {
+            Set<Long> processTaskIdSet = new HashSet<>();
 //            Set<Long> channelTypeRelationIdSet = new HashSet<>();
-//            List<ProcessTaskRelationVo> processTaskRelationList = processTaskMapper.getProcessTaskRelationList(processTaskRelationVo);            
-//            for(ProcessTaskRelationVo processTaskRelation : processTaskRelationList) {
-//                processTaskRelationMap.put(processTaskRelation.getProcessTaskId(), processTaskRelation);
-//                processTaskSet.add(processTaskRelation.getProcessTaskId());
+            List<ProcessTaskRelationVo> processTaskRelationList = processTaskMapper.getProcessTaskRelationList(processTaskRelationVo);            
+            for(ProcessTaskRelationVo processTaskRelation : processTaskRelationList) {
+                processTaskIdSet.add(processTaskRelation.getProcessTaskId());
+                ChannelTypeRelationVo channelTypeRelationVo = channelMapper.getChannelTypeRelationById(processTaskRelation.getChannelTypeRelationId());
+                if(channelTypeRelationVo != null) {
+                    processTaskRelation.setChannelTypeRelationName(channelTypeRelationVo.getName());
+                }
 //                channelTypeRelationIdSet.add(processTaskRelation.getChannelTypeRelationId());
+            }
+//            Map<Long, String> channelTypeRelationNameMap = new HashMap<>();
+//            List<ChannelTypeRelationVo> channelTypeRelationList = channelMapper.getChannelTypeRelationListByIdSet(channelTypeRelationIdSet);
+//            for(ChannelTypeRelationVo channelTypeRelationVo : channelTypeRelationList) {
+//                channelTypeRelationNameMap.put(channelTypeRelationVo.getId(), channelTypeRelationVo.getName());
 //            }
-//        }
-//        return resultObj;
-        return null;
+            Map<Long, ProcessTaskVo> processTaskMap = new HashMap<>();
+            List<ProcessTaskVo> processTaskList = processTaskMapper.getProcessTaskListByKeywordAndIdList(null, new ArrayList<>(processTaskIdSet), null, null);
+            for(ProcessTaskVo processTask : processTaskList) {
+                ChannelVo channelVo = channelMapper.getChannelByUuid(processTask.getChannelUuid());
+                if(channelVo != null && StringUtils.isNotBlank(channelVo.getChannelTypeUuid())) {
+                    ChannelTypeVo channelTypeVo = channelMapper.getChannelTypeByUuid(channelVo.getChannelTypeUuid());
+                    if(channelTypeVo == null) {
+                        channelTypeVo = new ChannelTypeVo();
+                        channelTypeVo.setUuid(channelVo.getChannelTypeUuid());
+                    }
+                    processTask.setChannelType(channelTypeVo);
+                }
+                processTaskMap.put(processTask.getId(), processTask);
+            }
+            for(ProcessTaskRelationVo processTaskRelation : processTaskRelationList) {
+//                processTaskRelation.setChannelTypeRelationName(channelTypeRelationNameMap.get(processTaskRelation.getChannelTypeRelationId()));
+                ProcessTaskVo processTask = processTaskMap.get(processTaskRelation.getProcessTaskId());
+                if(processTask != null) {
+                    processTaskRelation.setTilte(processTask.getTitle());
+                    processTaskRelation.setStatusVo(processTask.getStatusVo());
+                    processTaskRelation.setChannelTypeVo(processTask.getChannelType());
+                }
+            }
+            resultObj.put("processTaskRelationList", processTaskRelationList);
+        }
+        return resultObj;
     }
 
 }
