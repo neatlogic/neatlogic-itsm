c2bbade05642d82f7d4d0e5511e2f4a0e618b428
林邦泉 <linbq@techsure.com.cn>
2020-09-17 09:42:03 +0800

    --story=1003281 --user=林邦泉 事件转报逻辑 https://www.tapd.cn/54247054/s/1089896

---
+++ src/main/java/codedriver/module/process/audithandler/handler/ProcessTaskAuditHandler.java
@@ -0,0 +1,49 @@
+package codedriver.module.process.audithandler.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.process.audithandler.core.ProcessTaskStepAuditDetailHandlerBase;
+import codedriver.framework.process.constvalue.ProcessTaskAuditDetailType;
+import codedriver.framework.process.dao.mapper.ChannelMapper;
+import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
+import codedriver.framework.process.dto.ChannelTypeVo;
+import codedriver.framework.process.dto.ChannelVo;
+import codedriver.framework.process.dto.ProcessTaskStepAuditDetailVo;
+import codedriver.framework.process.dto.ProcessTaskVo;
+@Service
+public class ProcessTaskAuditHandler extends ProcessTaskStepAuditDetailHandlerBase {
+	
+    @Autowired
+    private ProcessTaskMapper processTaskMapper;
+    @Autowired
+    private ChannelMapper channelMapper;
+    
+	@Override
+	public String getType() {
+		return ProcessTaskAuditDetailType.PROCESSTASKLIST.getValue();
+	}
+
+	@Override
+	protected int myHandle(ProcessTaskStepAuditDetailVo processTaskStepAuditDetailVo) {
+	    JSONObject resultObj = new JSONObject();
+	    Long fromProcessTaskId = Long.valueOf(processTaskStepAuditDetailVo.getNewContent());
+	    resultObj.put("processTaskId", fromProcessTaskId);
+	    ProcessTaskVo processTaskVo = processTaskMapper.getProcessTaskById(fromProcessTaskId);
+	    if(processTaskVo != null) {
+	        resultObj.put("title", processTaskVo.getTitle());
+	        ChannelVo channelVo = channelMapper.getChannelByUuid(processTaskVo.getChannelUuid());
+	        if(channelVo != null) {
+	            ChannelTypeVo channelTypeVo = channelMapper.getChannelTypeByUuid(channelVo.getChannelTypeUuid());
+	            if(channelTypeVo != null) {
+	                resultObj.put("prefix", channelTypeVo.getPrefix());
+	            }
+	        }
+	    }
+	    processTaskStepAuditDetailVo.setNewContent(resultObj.toJSONString());
+		return 1;
+	}
+
+}
