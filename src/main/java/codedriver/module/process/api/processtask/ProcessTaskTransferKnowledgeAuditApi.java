cb4843940f25bb7386aac0a35acc5d57f790c39e
林邦泉 <linbq@techsure.com.cn>
2020-11-05 15:36:59 +0800

    --story=1003584 --user=林邦泉 知识库-工单分享到知识-后端 https://www.tapd.cn/54247054/s/1102920

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskTransferKnowledgeAuditApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskTransferKnowledgeAuditApi.java
@@ -1,22 +1,34 @@
 package codedriver.module.process.api.processtask;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.fastjson.JSONObject;
 
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.process.constvalue.ProcessTaskAuditType;
+import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
+import codedriver.framework.process.dto.ProcessTaskStepVo;
+import codedriver.framework.process.stephandler.core.ProcessStepUtilHandlerFactory;
 import codedriver.framework.reminder.core.OperationTypeEnum;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.process.service.ProcessTaskService;
 
 @Service
 @OperationType(type = OperationTypeEnum.CREATE)
 @Transactional
 public class ProcessTaskTransferKnowledgeAuditApi extends PrivateApiComponentBase {
+
+    @Autowired
+    private ProcessTaskMapper processTaskMapper;
+    
+    @Autowired
+    private ProcessTaskService processTaskService;
 
     @Override
     public String getToken() {
@@ -39,7 +51,14 @@ public class ProcessTaskTransferKnowledgeAuditApi extends PrivateApiComponentBas
     @Description(desc = "记录工单转知识活动")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        // TODO Auto-generated method stub
+        Long processTaskId = jsonObj.getLong("processTaskId");
+        processTaskService.checkProcessTaskParamsIsLegal(processTaskId);
+        processTaskMapper.getProcessTaskLockById(processTaskId);
+        //生成活动  
+        ProcessTaskStepVo processTaskStepVo = new ProcessTaskStepVo();
+        processTaskStepVo.setProcessTaskId(processTaskId);
+        processTaskStepVo.setParamObj(jsonObj);
+        ProcessStepUtilHandlerFactory.getHandler().activityAudit(processTaskStepVo, ProcessTaskAuditType.TRANSFERKNOWLEDGE);
         return null;
     }
 
