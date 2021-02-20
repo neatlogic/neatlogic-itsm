b351b1eeafbb54e06447a167073011b8ed4578d5
吕佐康 <lvzk@techsure.com.cn>
2021-02-20 18:19:36 +0800

    --story=1004044 --user=lvzk 接口扩展校验逻辑机制 https://www.tapd.cn/54247054/s/1135850

--- src/main/java/codedriver/module/process/api/catalog/CatalogSaveApi.java
+++ src/main/java/codedriver/module/process/api/catalog/CatalogSaveApi.java
@@ -17,10 +17,10 @@ import codedriver.module.process.service.CatalogService;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.collections4.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
@@ -29,10 +29,10 @@ import java.util.List;
 @AuthAction(action = CATALOG_MODIFY.class)
 public class CatalogSaveApi extends PrivateApiComponentBase {
 
-	@Autowired
+	@Resource
 	private CatalogMapper catalogMapper;
 
-	@Autowired
+	@Resource
 	private CatalogService catalogService;
 
 	@Override
