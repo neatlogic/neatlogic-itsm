289c3294d2b37fd9bc87f21a2391cf22ffc6d363
林邦泉 <linbq@techsure.com.cn>
2021-03-18 10:53:56 +0800

    --story=1003130 --user=林邦泉 树结构代码优化 https://www.tapd.cn/54247054/s/1143892

--- src/main/java/codedriver/module/process/api/catalog/CatalogDeteleApi.java
+++ src/main/java/codedriver/module/process/api/catalog/CatalogDeteleApi.java
@@ -77,7 +77,7 @@ public class CatalogDeteleApi extends PrivateApiComponentBase {
 		if(CollectionUtils.isNotEmpty(channelList)) {
 			throw new CatalogIllegalParameterException("服务目录：'" + uuid + "'还存在子服务");
 		}
-		LRCodeManager.deleteTreeNode("catalog", "uuid", "parent_uuid", uuid);
+		LRCodeManager.deleteTreeNodeBefore("catalog", "uuid", "parent_uuid", uuid);
 		catalogMapper.deleteCatalogByUuid(uuid);
 		catalogMapper.deleteCatalogAuthorityByCatalogUuid(uuid);
 		return null;
