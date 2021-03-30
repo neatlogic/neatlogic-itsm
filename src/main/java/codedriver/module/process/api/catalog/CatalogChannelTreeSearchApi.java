cc4405bc06fabe80c8e5b495002fa42a31fa0540
林邦泉 <linbq@techsure.com.cn>
2021-03-30 17:36:35 +0800

    --story=1004281 --user=林邦泉 服务的删除逻辑调整 https://www.tapd.cn/54247054/s/1147122

--- src/main/java/codedriver/module/process/api/catalog/CatalogChannelTreeSearchApi.java
+++ src/main/java/codedriver/module/process/api/catalog/CatalogChannelTreeSearchApi.java
@@ -2,6 +2,7 @@ package codedriver.module.process.api.catalog;
 
 import java.util.*;
 
+import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
@@ -24,71 +25,75 @@ import codedriver.module.process.service.CatalogService;
 @Transactional
 public class CatalogChannelTreeSearchApi extends PrivateApiComponentBase {
 
-	@Autowired
-	private CatalogService catalogService;
-	
-	@Autowired
-	private CatalogMapper catalogMapper;
-	
-	@Autowired
-	private ChannelMapper channelMapper;
-	
-	@Override
-	public String getToken() {
-		return "process/catalog/channel/tree/search";
-	}
+    @Autowired
+    private CatalogService catalogService;
 
-	@Override
-	public String getName() {
-		return "服务目录及通道树查询接口";
-	}
+    @Autowired
+    private CatalogMapper catalogMapper;
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Autowired
+    private ChannelMapper channelMapper;
 
-	@Input({})
-	@Output({
-		@Param(name="Return",explode=CatalogVo[].class,desc="服务目录及通道树")
-	})
-	@Description(desc = "服务目录及通道树查询接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {				
-		
-		Map<String, CatalogVo> uuidKeyMap = new HashMap<>();
-		if(catalogMapper.checkLeftRightCodeIsWrong() > 0) {
-			catalogMapper.getCatalogCountOnLock();
-			catalogService.rebuildLeftRightCode();
-		}
-		CatalogVo rootCatalog = catalogService.buildRootCatalog();
-		List<CatalogVo> catalogList = catalogMapper.getCatalogListForTree(rootCatalog.getLft(), rootCatalog.getRht());
-		if(CollectionUtils.isNotEmpty(catalogList)) {
-			//将虚拟的root节点加入到catalogList中
-			catalogList.add(rootCatalog);
-			for(CatalogVo catalogVo : catalogList) {
-				uuidKeyMap.put(catalogVo.getUuid(), catalogVo);			
-			}
-			for(CatalogVo catalogVo : catalogList) {
-				String parentUuid = catalogVo.getParentUuid();
-				CatalogVo parent = uuidKeyMap.get(parentUuid);
-				if(parent != null) {
-					catalogVo.setParent(parent);
-				}				
-			}
-		}
+    @Autowired
+    private ProcessTaskMapper processTaskMapper;
 
-		List<ChannelVo> channelList = channelMapper.getChannelListForTree(null);
-		if(CollectionUtils.isNotEmpty(channelList)) {
-			for(ChannelVo channelVo : channelList) {
-				String parentUuid = channelVo.getParentUuid();
-				CatalogVo parent = uuidKeyMap.get(parentUuid);
-				if(parent != null) {
-					channelVo.setParent(parent);
-				}
-			}
-		}
-		
-		return rootCatalog.getChildren();
-	}
+    @Override
+    public String getToken() {
+        return "process/catalog/channel/tree/search";
+    }
+
+    @Override
+    public String getName() {
+        return "服务目录及通道树查询接口";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({})
+    @Output({
+            @Param(name = "Return", explode = CatalogVo[].class, desc = "服务目录及通道树")
+    })
+    @Description(desc = "服务目录及通道树查询接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+
+        Map<String, CatalogVo> uuidKeyMap = new HashMap<>();
+        CatalogVo rootCatalog = catalogService.buildRootCatalog();
+        List<CatalogVo> catalogList = catalogMapper.getCatalogListForTree(rootCatalog.getLft(), rootCatalog.getRht());
+        //将虚拟的root节点加入到catalogList中
+        catalogList.add(rootCatalog);
+        for (CatalogVo catalogVo : catalogList) {
+            uuidKeyMap.put(catalogVo.getUuid(), catalogVo);
+        }
+        for (CatalogVo catalogVo : catalogList) {
+            String parentUuid = catalogVo.getParentUuid();
+            CatalogVo parent = uuidKeyMap.get(parentUuid);
+            if (parent != null) {
+                catalogVo.setParent(parent);
+            }
+        }
+
+        Map<String, Integer> channelReferencedCountMap = new HashMap<>();
+        List<ChannelVo> channelReferencedCountList = processTaskMapper.getChannelReferencedCountList();
+        for (ChannelVo channelVo : channelReferencedCountList) {
+            channelReferencedCountMap.put(channelVo.getUuid(), channelVo.getChildrenCount());
+        }
+        List<ChannelVo> channelList = channelMapper.getChannelListForTree(null);
+        for (ChannelVo channelVo : channelList) {
+            String parentUuid = channelVo.getParentUuid();
+            CatalogVo parent = uuidKeyMap.get(parentUuid);
+            if (parent != null) {
+                channelVo.setParent(parent);
+                Integer count = channelReferencedCountMap.get(channelVo.getUuid());
+                if (count != null) {
+                    channelVo.setChildrenCount(count);
+                }
+            }
+        }
+
+        return rootCatalog.getChildren();
+    }
 }
