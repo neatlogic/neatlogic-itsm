dd281c5241afcdc6c8652b679324cf9fa2937a20
林邦泉 <linbq@techsure.com.cn>
2021-03-31 10:06:04 +0800

    --bug=1007757 --user=林邦泉 服务目录管理-移动服务到两个目录之间的位置时，提示服务通道不存在 https://www.tapd.cn/54247054/s/1147246

--- src/main/java/codedriver/module/process/api/channel/ChannelMoveApi.java
+++ src/main/java/codedriver/module/process/api/channel/ChannelMoveApi.java
@@ -10,7 +10,6 @@ import codedriver.framework.lrcode.exception.MoveTargetNodeIllegalException;
 import codedriver.framework.process.exception.channel.ChannelNameRepeatException;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.annotation.OperationType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,152 +37,153 @@ import javax.annotation.Resource;
 @AuthAction(action = CATALOG_MODIFY.class)
 public class ChannelMoveApi extends PrivateApiComponentBase {
 
-	@Resource
-	private ChannelMapper channelMapper;
-	
-	@Resource
-	private CatalogMapper catalogMapper;
-	
-	@Override
-	public String getToken() {
-		return "process/channel/move";
-	}
+    @Resource
+    private ChannelMapper channelMapper;
 
-	@Override
-	public String getName() {
-		return "服务通道移动位置接口";
-	}
+    @Resource
+    private CatalogMapper catalogMapper;
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
-	@Input({
-		@Param(name = "uuid", type = ApiParamType.STRING, isRequired = true, desc = "被移动的服务通道uuid"),
-		@Param(name = "targetUuid", type = ApiParamType.STRING, isRequired = true, desc = "目标节点uuid"),
-		@Param(name = "moveType", type = ApiParamType.ENUM, rule = "inner,prev,next", isRequired = true, desc = "移动类型")
-	})
-	@Description(desc = "服务通道移动位置接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		String uuid = jsonObj.getString("uuid");
-		//目标节点uuid
-		String targetUuid = jsonObj.getString("targetUuid");
-		if(uuid.equals(targetUuid)) {
-			throw new MoveTargetNodeIllegalException();
-		}
-		ChannelVo moveChannel = channelMapper.getChannelByUuid(uuid);
-		//判断被移动的服务通道是否存在
-		if(moveChannel == null) {
-			throw new ChannelNotFoundException(uuid);
-		}
-		int oldSort = moveChannel.getSort();
-		int newSort;
-		String oldParentUuid = moveChannel.getParentUuid();
-		String newParentUuid;
-		MoveType moveType = MoveType.getMoveType(jsonObj.getString("moveType"));
-		if(MoveType.INNER == moveType) {
-			//通道只能移进目录里面，不能移进通道里面
-			CatalogVo target = catalogMapper.getCatalogByUuid(targetUuid);
-			if(target == null){
-				throw new CatalogNotFoundException(targetUuid);
-			}
-			newParentUuid = targetUuid;
-			int maxSort = channelMapper.getMaxSortByParentUuid(newParentUuid);
-			if(oldParentUuid.equals(newParentUuid)){
-				//相同目录下移动
-				newSort = maxSort;
-				if(newSort == oldSort){
-					return null;
-				}else{
-					//旧目录，被移动目录后面的兄弟节点序号减一
-					channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
-				}
-			}else {
-				//不同目录下移动
-				newSort = maxSort + 1;
-				//旧目录，被移动目录后面的兄弟节点序号减一
-				channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
-				//新目录，目标目录后面的兄弟节点序号加一
-				channelMapper.updateSortIncrement(newParentUuid, newSort, null);
-			}
-		}else {
-			//目标节点可能是目录或通道
-			CatalogVo targetCatalog = catalogMapper.getCatalogByUuid(targetUuid);
-			if(targetCatalog != null) {
-				//不管是移动到目录的前面还是后面，移动后排在最前面
-				newSort = 1;
-				newParentUuid = targetCatalog.getParentUuid();
-				if(oldParentUuid.equals(newParentUuid)){
-					//相同目录下移动
-					if(newSort == oldSort){
-						return null;
-					}else {
-						channelMapper.updateSortIncrement(oldParentUuid, newSort, oldSort - 1);
-					}
-				}else {
-					//不同目录下移动
-					//旧目录，被移动目录后面的兄弟节点序号减一
-					channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
-					//新目录，目标目录后面的兄弟节点序号加一
-					channelMapper.updateSortIncrement(newParentUuid, newSort, null);
-				}
-			}else {
-				ChannelVo targetChannel = channelMapper.getChannelByUuid(targetUuid);
-				if(targetChannel != null){
-					//移动到其他服务的前面或后面
-					newParentUuid = targetChannel.getParentUuid();
-					if(oldParentUuid.equals(newParentUuid)){
-						//相同目录下移动
-						if(oldSort > targetChannel.getSort()){
-							//往前移动
-							if(MoveType.PREV == moveType){
-								newSort = targetChannel.getSort();
-							}else {
-								newSort = targetChannel.getSort() + 1;
-							}
-							if(newSort == oldSort){
-								return null;
-							}
-							channelMapper.updateSortIncrement(oldParentUuid, newSort, oldSort - 1);
-						}else {
-							//往后移动
-							if(MoveType.PREV == moveType){
-								newSort = targetChannel.getSort() - 1;
-							}else {
-								newSort = targetChannel.getSort();
-							}
-							if(newSort == oldSort){
-								return null;
-							}
-							channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, newSort);
-						}
-					}else {
-						//不同目录下移动
-						if(MoveType.PREV == moveType){
-							newSort = targetChannel.getSort();
-						}else {
-							newSort = targetChannel.getSort() + 1;
-						}
-						//旧目录，被移动目录后面的兄弟节点序号减一
-						channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
-						//新目录，目标目录后面的兄弟节点序号加一
-						channelMapper.updateSortIncrement(newParentUuid, newSort, null);
-					}
+    @Override
+    public String getToken() {
+        return "process/channel/move";
+    }
 
-				}else {
-					throw new ChannelNotFoundException(targetUuid);
-				}
-			}
-		}
-		moveChannel.setSort(newSort);
-		moveChannel.setParentUuid(newParentUuid);
-		//判断移动后相同目录下是否有同名服务
-		if(channelMapper.checkChannelNameIsRepeat(moveChannel) > 0) {
-			throw new ChannelNameRepeatException(moveChannel.getName());
-		}
-		channelMapper.updateChannelForMove(moveChannel);
-		return null;
-	}
+    @Override
+    public String getName() {
+        return "服务通道移动位置接口";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "uuid", type = ApiParamType.STRING, isRequired = true, desc = "被移动的服务通道uuid"),
+            @Param(name = "targetUuid", type = ApiParamType.STRING, isRequired = true, desc = "目标节点uuid"),
+            @Param(name = "moveType", type = ApiParamType.ENUM, rule = "inner,prev,next", isRequired = true, desc = "移动类型")
+    })
+    @Description(desc = "服务通道移动位置接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        String uuid = jsonObj.getString("uuid");
+        //目标节点uuid
+        String targetUuid = jsonObj.getString("targetUuid");
+        if (uuid.equals(targetUuid)) {
+            throw new MoveTargetNodeIllegalException();
+        }
+        ChannelVo moveChannel = channelMapper.getChannelByUuid(uuid);
+        //判断被移动的服务通道是否存在
+        if (moveChannel == null) {
+            throw new ChannelNotFoundException(uuid);
+        }
+        int oldSort = moveChannel.getSort();
+        int newSort;
+        String oldParentUuid = moveChannel.getParentUuid();
+        String newParentUuid;
+        MoveType moveType = MoveType.getMoveType(jsonObj.getString("moveType"));
+        if (MoveType.INNER == moveType) {
+            //通道只能移进目录里面，不能移进通道里面
+            CatalogVo target = catalogMapper.getCatalogByUuid(targetUuid);
+            if (target == null) {
+                throw new CatalogNotFoundException(targetUuid);
+            }
+            newParentUuid = targetUuid;
+            int maxSort = channelMapper.getMaxSortByParentUuid(newParentUuid);
+            if (oldParentUuid.equals(newParentUuid)) {
+                //相同目录下移动
+                newSort = maxSort;
+                if (newSort == oldSort) {
+                    return null;
+                } else {
+                    //旧目录，被移动目录后面的兄弟节点序号减一
+                    channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
+                }
+            } else {
+                //不同目录下移动
+                newSort = maxSort + 1;
+                //旧目录，被移动目录后面的兄弟节点序号减一
+                channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
+                //新目录，目标目录后面的兄弟节点序号加一
+                channelMapper.updateSortIncrement(newParentUuid, newSort, null);
+            }
+        } else {
+            //目标节点可能是目录或通道
+            CatalogVo targetCatalog = catalogMapper.getCatalogByUuid(targetUuid);
+            if (targetCatalog != null) {
+                //不管是移动到目录的前面还是后面，移动后排在最前面
+                newSort = 1;
+                newParentUuid = targetCatalog.getParentUuid();
+                if (oldParentUuid.equals(newParentUuid)) {
+                    //相同目录下移动
+                    if (newSort == oldSort) {
+                        return null;
+                    } else {
+                        channelMapper.updateSortIncrement(oldParentUuid, newSort, oldSort - 1);
+                    }
+                } else {
+                    //不同目录下移动
+                    //旧目录，被移动目录后面的兄弟节点序号减一
+                    channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
+                    //新目录，目标目录后面的兄弟节点序号加一
+                    channelMapper.updateSortIncrement(newParentUuid, newSort, null);
+                }
+            } else {
+                ChannelVo targetChannel = channelMapper.getChannelByUuid(targetUuid);
+                if (targetChannel != null) {
+                    //移动到其他服务的前面或后面
+                    newParentUuid = targetChannel.getParentUuid();
+                    if (oldParentUuid.equals(newParentUuid)) {
+                        //相同目录下移动
+                        if (oldSort > targetChannel.getSort()) {
+                            //往前移动
+                            if (MoveType.PREV == moveType) {
+                                newSort = targetChannel.getSort();
+                            } else {
+                                newSort = targetChannel.getSort() + 1;
+                            }
+                            if (newSort == oldSort) {
+                                return null;
+                            }
+                            channelMapper.updateSortIncrement(oldParentUuid, newSort, oldSort - 1);
+                        } else {
+                            //往后移动
+                            if (MoveType.PREV == moveType) {
+                                newSort = targetChannel.getSort() - 1;
+                            } else {
+                                newSort = targetChannel.getSort();
+                            }
+                            if (newSort == oldSort) {
+                                return null;
+                            }
+                            channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, newSort);
+                        }
+                    } else {
+                        //不同目录下移动
+                        if (MoveType.PREV == moveType) {
+                            newSort = targetChannel.getSort();
+                        } else {
+                            newSort = targetChannel.getSort() + 1;
+                        }
+                        //旧目录，被移动目录后面的兄弟节点序号减一
+                        channelMapper.updateSortDecrement(oldParentUuid, oldSort + 1, null);
+                        //新目录，目标目录后面的兄弟节点序号加一
+                        channelMapper.updateSortIncrement(newParentUuid, newSort, null);
+                    }
+
+                } else {
+                    throw new ChannelNotFoundException(targetUuid);
+                }
+            }
+        }
+        moveChannel.setSort(newSort);
+        moveChannel.setParentUuid(newParentUuid);
+        //判断移动后相同目录下是否有同名服务
+        if (channelMapper.checkChannelNameIsRepeat(moveChannel) > 0) {
+            throw new ChannelNameRepeatException(moveChannel.getName());
+        }
+        channelMapper.updateChannelForMove(moveChannel);
+        return null;
+    }
 
 }
