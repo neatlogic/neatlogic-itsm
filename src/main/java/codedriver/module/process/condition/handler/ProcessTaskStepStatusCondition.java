2dab583ad44489da87ab47484e6de4e9cfa626d2
吕佐康 <lvzk@techsure.com.cn>
2021-04-20 17:48:33 +0800

    bug fixed 工单中心步骤状态过滤

--- src/main/java/codedriver/module/process/condition/handler/ProcessTaskStepStatusCondition.java
+++ src/main/java/codedriver/module/process/condition/handler/ProcessTaskStepStatusCondition.java
@@ -1,13 +1,14 @@
 package codedriver.module.process.condition.handler;
 
+import codedriver.framework.common.constvalue.Expression;
 import codedriver.framework.common.constvalue.FormHandlerType;
 import codedriver.framework.common.constvalue.ParamType;
 import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.dto.condition.ConditionVo;
+import codedriver.framework.form.constvalue.FormConditionModel;
 import codedriver.framework.process.condition.core.IProcessTaskCondition;
 import codedriver.framework.process.condition.core.ProcessTaskConditionBase;
 import codedriver.framework.process.constvalue.ConditionConfigType;
-import codedriver.framework.form.constvalue.FormConditionModel;
 import codedriver.framework.process.constvalue.ProcessFieldType;
 import codedriver.framework.process.constvalue.ProcessTaskStatus;
 import codedriver.framework.process.workcenter.dto.JoinTableColumnVo;
@@ -143,5 +144,7 @@ public class ProcessTaskStepStatusCondition extends ProcessTaskConditionBase imp
     @Override
     public void getSqlConditionWhere(List<ConditionVo> conditionList, Integer index, StringBuilder sqlSb) {
         getSimpleSqlConditionWhere(conditionList.get(index), sqlSb, new ProcessTaskStepSqlTable().getShortName(), ProcessTaskStepSqlTable.FieldEnum.STATUS.getValue());
+        sqlSb.append(" and ");
+        sqlSb.append(Expression.getExpressionSql(Expression.EXCLUDE.getExpression(), new ProcessTaskStepSqlTable().getShortName(), ProcessTaskStepSqlTable.FieldEnum.IS_ACTIVE.getValue(),"0','-1"));
     }
 }
