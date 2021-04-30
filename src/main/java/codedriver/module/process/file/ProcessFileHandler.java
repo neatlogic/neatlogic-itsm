68e50d0b50a13d58d58a3dd9ccda386ff5480647
陈其炜 <chenqw@techsure.com.cn>
2021-04-30 15:08:32 +0800

    fileTypeHandler优化
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/process/file/ProcessFileHandler.java
+++ src/main/java/codedriver/module/process/file/ProcessFileHandler.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.module.process.file;
 
 import org.springframework.stereotype.Component;
@@ -10,23 +15,27 @@ import codedriver.framework.file.dto.FileVo;
 @Component
 public class ProcessFileHandler extends FileTypeHandlerBase {
 
-	@Override
-	public boolean valid(String userUuid, JSONObject jsonObj) {
-		return true;
-	}
+    @Override
+    public boolean valid(String userUuid, FileVo fileVo, JSONObject jsonObj) {
+        return true;
+    }
 
-	@Override
-	public String getDisplayName() {
-		return "IT服务附件";
-	}
+    @Override
+    public String getDisplayName() {
+        return "IT服务附件";
+    }
 
-	@Override
-	public void afterUpload(FileVo fileVo, JSONObject jsonObj) {
-	}
+    @Override
+    public void afterUpload(FileVo fileVo, JSONObject jsonObj) {
+    }
 
-	@Override
-	public String getName() {
-		return "ITSM";
-	}
+    @Override
+    public String getName() {
+        return "ITSM";
+    }
 
+    @Override
+    protected void myDeleteFile(Long fileId) {
+
+    }
 }
