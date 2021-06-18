7cf4d9797b768ba679e4a30e18fab3e485172a7f
林邦泉 <linbq@techsure.com.cn>
2021-06-18 10:15:41 +0800

    --story=1004869 --user=林邦泉 提供给下拉框作为数据源的接口，统一增加defaultValue入参，用于已选中项的回显 https://www.tapd.cn/54247054/s/1159861

--- src/main/java/codedriver/module/process/api/channel/ChannelSearchForSelectApi.java
+++ src/main/java/codedriver/module/process/api/channel/ChannelSearchForSelectApi.java
@@ -33,8 +33,7 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.process.service.CatalogService;
 
-//@Service
-@Deprecated
+@Service
 @AuthAction(action = PROCESS_BASE.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class ChannelSearchForSelectApi extends PrivateApiComponentBase {
