d79e80465a94264c76c62379801686402c554cae
赖文韬 <laiwt@techsure.com.cn>
2021-07-01 16:01:30 +0800

    --bug=1008164 --user=赖文韬 工单中心-导出工单列表... https://www.tapd.cn/54247054/s/1161866

--- src/main/java/codedriver/module/process/workcenter/column/handler/ProcessTaskReporterColumn.java
+++ src/main/java/codedriver/module/process/workcenter/column/handler/ProcessTaskReporterColumn.java
@@ -1,6 +1,5 @@
 package codedriver.module.process.workcenter.column.handler;
 
-import codedriver.framework.dao.mapper.UserMapper;
 import codedriver.framework.process.column.core.IProcessTaskColumn;
 import codedriver.framework.process.column.core.ProcessTaskColumnBase;
 import codedriver.framework.process.constvalue.ProcessFieldType;
@@ -11,27 +10,22 @@ import codedriver.framework.process.workcenter.dto.TableSelectColumnVo;
 import codedriver.framework.process.workcenter.table.ProcessTaskSqlTable;
 import codedriver.framework.process.workcenter.table.UserTable;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @Component
-public class ProcessTaskReporterColumn extends ProcessTaskColumnBase implements IProcessTaskColumn{
-	@Autowired
-	UserMapper userMapper;
-	@Override
-	public String getName() {
-		return "reporter";
-	}
+public class ProcessTaskReporterColumn extends ProcessTaskColumnBase implements IProcessTaskColumn {
 
-	@Override
-	public String getDisplayName() {
-		return "代报人";
-	}
+    @Override
+    public String getName() {
+        return "reporter";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "代报人";
+    }
 
 	/*@Override
 	public Object getMyValue(JSONObject json) throws RuntimeException {
@@ -52,26 +46,26 @@ public class ProcessTaskReporterColumn extends ProcessTaskColumnBase implements 
 		return userJson;
 	}*/
 
-	@Override
-	public Boolean allowSort() {
-		return false;
-	}
-	
-	@Override
-	public String getType() {
-		return ProcessFieldType.COMMON.getValue();
-	}
+    @Override
+    public Boolean allowSort() {
+        return false;
+    }
 
-	@Override
-	public String getClassName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getType() {
+        return ProcessFieldType.COMMON.getValue();
+    }
 
-	@Override
-	public Integer getSort() {
-		return 4;
-	}
+    @Override
+    public String getClassName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Integer getSort() {
+        return 4;
+    }
 
 	/*@Override
 	public Object getSimpleValue(Object json) {
@@ -82,45 +76,48 @@ public class ProcessTaskReporterColumn extends ProcessTaskColumnBase implements 
 		return username;
 	}*/
 
-	@Override
-	public String getSimpleValue(ProcessTaskVo processTaskVo) {
-		if(processTaskVo.getReporterVo() != null ||(processTaskVo.getOwnerVo() != null && !processTaskVo.getOwnerVo().getUuid().equals(processTaskVo.getReporterVo().getUuid()))){
-			return processTaskVo.getReporterVo().getName();
-		}
-		return StringUtils.EMPTY;
-	}
+    @Override
+    public String getSimpleValue(ProcessTaskVo processTaskVo) {
+        if (processTaskVo.getReporterVo() != null) {
+            if (!(processTaskVo.getOwnerVo() != null
+                    && Objects.equals(processTaskVo.getOwnerVo().getUuid(), processTaskVo.getReporterVo().getUuid()))) {
+                return processTaskVo.getReporterVo().getName();
+            }
+        }
+        return StringUtils.EMPTY;
+    }
 
-	@Override
-	public Object getValue(ProcessTaskVo processTaskVo) {
-		if(processTaskVo.getReporterVo() != null ||(processTaskVo.getOwnerVo() != null && !processTaskVo.getOwnerVo().getUuid().equals(processTaskVo.getReporterVo().getUuid()))){
-			return processTaskVo.getReporterVo();
-		}
-		return null;
-	}
+    @Override
+    public Object getValue(ProcessTaskVo processTaskVo) {
+        if (processTaskVo.getReporterVo() != null || (processTaskVo.getOwnerVo() != null && !processTaskVo.getOwnerVo().getUuid().equals(processTaskVo.getReporterVo().getUuid()))) {
+            return processTaskVo.getReporterVo();
+        }
+        return null;
+    }
 
-	@Override
-	public List<TableSelectColumnVo> getTableSelectColumn() {
-		return new ArrayList<TableSelectColumnVo>(){
-			{
-				add(new TableSelectColumnVo(new UserTable(),"reporter", Arrays.asList(
-						new SelectColumnVo(UserTable.FieldEnum.UUID.getValue(),"reporterUuid"),
-						new SelectColumnVo(UserTable.FieldEnum.USER_NAME.getValue(),"reporterName"),
-						new SelectColumnVo(UserTable.FieldEnum.USER_INFO.getValue(),"reporterInfo"),
-						new SelectColumnVo(UserTable.FieldEnum.VIP_LEVEL.getValue(),"reporterVipLevel"),
-						new SelectColumnVo(UserTable.FieldEnum.PINYIN.getValue(),"reporterPinYin")
-				)));
-			}
-		};
-	}
+    @Override
+    public List<TableSelectColumnVo> getTableSelectColumn() {
+        return new ArrayList<TableSelectColumnVo>() {
+            {
+                add(new TableSelectColumnVo(new UserTable(), "reporter", Arrays.asList(
+                        new SelectColumnVo(UserTable.FieldEnum.UUID.getValue(), "reporterUuid"),
+                        new SelectColumnVo(UserTable.FieldEnum.USER_NAME.getValue(), "reporterName"),
+                        new SelectColumnVo(UserTable.FieldEnum.USER_INFO.getValue(), "reporterInfo"),
+                        new SelectColumnVo(UserTable.FieldEnum.VIP_LEVEL.getValue(), "reporterVipLevel"),
+                        new SelectColumnVo(UserTable.FieldEnum.PINYIN.getValue(), "reporterPinYin")
+                )));
+            }
+        };
+    }
 
-	@Override
-	public List<JoinTableColumnVo> getMyJoinTableColumnList() {
-		return new ArrayList<JoinTableColumnVo>() {
-			{
-				add(new JoinTableColumnVo(new ProcessTaskSqlTable(), new UserTable(),"reporter", new HashMap<String, String>() {{
-					put(ProcessTaskSqlTable.FieldEnum.REPORTER.getValue(), UserTable.FieldEnum.UUID.getValue());
-				}}));
-			}
-		};
-	}
+    @Override
+    public List<JoinTableColumnVo> getMyJoinTableColumnList() {
+        return new ArrayList<JoinTableColumnVo>() {
+            {
+                add(new JoinTableColumnVo(new ProcessTaskSqlTable(), new UserTable(), "reporter", new HashMap<String, String>() {{
+                    put(ProcessTaskSqlTable.FieldEnum.REPORTER.getValue(), UserTable.FieldEnum.UUID.getValue());
+                }}));
+            }
+        };
+    }
 }
