c275610e4b4b40fe72c2cd44c733d3df32d86236
林邦泉 <linbq@techsure.com.cn>
2021-08-11 19:00:28 +0800

    --story=1005053 --user=林邦泉 苏州银行-批量合并上报流程涉及的定制表单、接口、定制通知插件 https://www.tapd.cn/54247054/s/1170455

--- src/main/java/codedriver/module/process/api/channel/ChannelSearchForSelectApi.java
+++ src/main/java/codedriver/module/process/api/channel/ChannelSearchForSelectApi.java
@@ -1,10 +1,7 @@
 package codedriver.module.process.api.channel;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.process.auth.PROCESS_BASE;
@@ -12,10 +9,8 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
 import com.google.common.base.Objects;
 
 import codedriver.framework.asynchronization.threadlocal.UserContext;
@@ -62,6 +57,7 @@ public class ChannelSearchForSelectApi extends PrivateApiComponentBase {
 	@Input({
 		@Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字，匹配名称"),
 		@Param(name = "parentUuid", type = ApiParamType.STRING, desc = "服务目录uuid"),
+		@Param(name = "uuidList", type = ApiParamType.JSONARRAY, desc = "服务uuid列表"),
 		@Param(name = "defaultValue", type = ApiParamType.JSONARRAY,  desc = "用于回显的参数列表", xss = true),
 		@Param(name = "isFavorite", type = ApiParamType.ENUM, desc = "是否只查询已收藏的数据，1：已收藏，0：全部", rule = "0,1"),
 		@Param(name = "isActive", type = ApiParamType.ENUM, desc = "是否激活", rule = "0,1"),
@@ -84,7 +80,6 @@ public class ChannelSearchForSelectApi extends PrivateApiComponentBase {
 		ChannelVo channelVo = JSONObject.toJavaObject(jsonObj, ChannelVo.class);
 		channelVo.setUserUuid(UserContext.get().getUserUuid(true));
 		Integer isAuthenticate = jsonObj.getInteger("isAuthenticate");
-		List<ValueTextVo> channelList = new ArrayList<>();
 		if(Objects.equal(isAuthenticate, 1)) {
             //查出当前用户已授权的服务
             channelVo.setAuthorizedUuidList(catalogService.getCurrentUserAuthorizedChannelUuidList());
@@ -103,10 +98,26 @@ public class ChannelSearchForSelectApi extends PrivateApiComponentBase {
                     }
                 }
             }else {
-                channelVo.setAuthorizedUuidList(Arrays.asList(defaultValue.toArray()).stream().map(object -> object.toString()).collect(Collectors.toList()));
+                channelVo.setAuthorizedUuidList(defaultValue.toJavaList(String.class));
             }
             channelVo.setNeedPage(false);
         }
+		List<String> uuidList = channelVo.getUuidList();
+        if (CollectionUtils.isNotEmpty(uuidList)) {
+			List<String> authorizationUuidList = channelVo.getAuthorizedUuidList();
+			if(CollectionUtils.isNotEmpty(authorizationUuidList)) {
+				Iterator<String> authUuidIterator = authorizationUuidList.iterator();
+				while(authUuidIterator.hasNext()) {
+					String uuid = authUuidIterator.next();
+					if(!uuidList.contains(uuid)) {
+						authUuidIterator.remove();
+					}
+				}
+			}else {
+				channelVo.setAuthorizedUuidList(uuidList);
+			}
+			channelVo.setNeedPage(false);
+		}
         if(channelVo.getNeedPage()) {
             int rowNum = channelMapper.searchChannelCount(channelVo);
             int pageCount = PageUtil.getPageCount(rowNum,channelVo.getPageSize());
@@ -116,8 +127,8 @@ public class ChannelSearchForSelectApi extends PrivateApiComponentBase {
             resultObj.put("pageSize",channelVo.getPageSize());
             resultObj.put("pageCount", pageCount);
             resultObj.put("rowNum", rowNum);
-        }           
-        channelList = channelMapper.searchChannelListForSelect(channelVo);
+        }
+		List<ValueTextVo> channelList = channelMapper.searchChannelListForSelect(channelVo);
 		resultObj.put("list", channelList);
 		return resultObj;
 	}
