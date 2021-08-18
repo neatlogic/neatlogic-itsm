d70662fff9b3dc580c991ce389992fb311968c8f
林邦泉 <linbq@techsure.com.cn>
2021-08-18 14:16:57 +0800

    --task=1005152 --user=林邦泉 后端-苏州银行-流程步骤节点打标签 https://www.tapd.cn/54247054/s/1172065

--- src/main/java/codedriver/module/process/api/process/ProcessTagGetApi.java
+++ src/main/java/codedriver/module/process/api/process/ProcessTagGetApi.java
@@ -1,7 +1,10 @@
 package codedriver.module.process.api.process;
 
 import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.common.dto.BasePageVo;
 import codedriver.framework.process.auth.PROCESS_BASE;
+import com.alibaba.fastjson.JSONArray;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,56 +24,71 @@ import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @AuthAction(action = PROCESS_BASE.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class ProcessTagGetApi extends PrivateApiComponentBase {
 
-	@Autowired
-	private ProcessMapper processMapper;
+    @Autowired
+    private ProcessMapper processMapper;
 
-	@Override
-	public String getToken() {
-		return "process/tag/get";
-	}
+    @Override
+    public String getToken() {
+        return "process/tag/get";
+    }
 
-	@Override
-	public String getName() {
-		return "获取标签_下拉";
-	}
+    @Override
+    public String getName() {
+        return "获取标签_下拉";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	@Input({
-		@Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字，匹配名称"),
-		@Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true"),
-        @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页条目"),
-        @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页")
-	})
-	@Output({
-	    @Param(name="currentPage",type=ApiParamType.INTEGER,isRequired=true,desc="当前页码"),
-        @Param(name="pageSize",type=ApiParamType.INTEGER,isRequired=true,desc="页大小"),
-        @Param(name="pageCount",type=ApiParamType.INTEGER,isRequired=true,desc="总页数"),
-        @Param(name="rowNum",type=ApiParamType.INTEGER,isRequired=true,desc="总行数"),
-        @Param(name="list",explode=ValueTextVo[].class,desc="流程列表")
-	})
-	@Description(desc = "获取标签_下拉")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-	    JSONObject resultObj = new JSONObject();
-		ProcessTagVo processTagVo = JSON.toJavaObject(jsonObj, ProcessTagVo.class);
-		if (processTagVo.getNeedPage()) {
-            int rowNum = processMapper.getProcessTagCount(processTagVo);
-            resultObj.put("rowNum", rowNum);
-            resultObj.put("pageSize", processTagVo.getPageSize());
-            resultObj.put("currentPage", processTagVo.getCurrentPage());
-            resultObj.put("pageCount", PageUtil.getPageCount(rowNum, processTagVo.getPageSize()));
-        }
-		resultObj.put("list", processMapper.getProcessTagForSelect(processTagVo));
-		return resultObj;
-	}
+    @Input({
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字，匹配名称"),
+            @Param(name = "defaultValue", type = ApiParamType.JSONARRAY, desc = "默认值列表"),
+            @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true"),
+            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页条目"),
+            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页")
+    })
+    @Output({
+            @Param(explode = BasePageVo.class),
+            @Param(name = "list", explode = ValueTextVo[].class, desc = "流程列表")
+    })
+    @Description(desc = "获取标签_下拉")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        JSONObject resultObj = new JSONObject();
+        ProcessTagVo processTagVo = JSON.toJavaObject(jsonObj, ProcessTagVo.class);
+		JSONArray defaultValue = processTagVo.getDefaultValue();
+		if (CollectionUtils.isNotEmpty(defaultValue)) {
+			List<String> tagNameList = defaultValue.toJavaList(String.class);
+			List<ValueTextVo> valueTextList = new ArrayList<>();
+			List<ProcessTagVo> processTagList = processMapper.getProcessTagByNameList(tagNameList);
+			for (ProcessTagVo processTag : processTagList) {
+				valueTextList.add(new ValueTextVo(processTag.getId(), processTag.getName()));
+			}
+			resultObj.put("list", valueTextList);
+		} else {
+			int rowNum = processMapper.getProcessTagCount(processTagVo);
+			processTagVo.setRowNum(rowNum);
+			resultObj.put("rowNum", rowNum);
+			resultObj.put("pageSize", processTagVo.getPageSize());
+			resultObj.put("currentPage", processTagVo.getCurrentPage());
+			resultObj.put("pageCount", processTagVo.getPageCount());
+			if (rowNum > 0) {
+				resultObj.put("list", processMapper.getProcessTagForSelect(processTagVo));
+			} else {
+				resultObj.put("list", new ArrayList<>());
+			}
+		}
+        return resultObj;
+    }
 
 }
