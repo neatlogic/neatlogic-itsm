8e239d04d143fac6b3130aeab10789199dece91c
吕佐康 <lvzk@techsure.com.cn>
2021-08-23 15:33:49 +0800

    update

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskBatchMergeReportActionApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskBatchMergeReportActionApi.java
@@ -10,7 +10,6 @@ import codedriver.framework.notify.core.INotifyHandler;
 import codedriver.framework.notify.core.NotifyHandlerFactory;
 import codedriver.framework.notify.dto.NotifyVo;
 import codedriver.framework.notify.exception.NotifyHandlerNotFoundException;
-import codedriver.module.framework.notify.handler.EmailNotifyHandler;
 import codedriver.framework.process.constvalue.ProcessTaskAuditDetailType;
 import codedriver.framework.process.constvalue.ProcessTaskAuditType;
 import codedriver.framework.process.dao.mapper.ChannelTypeMapper;
@@ -79,9 +78,9 @@ public class ProcessTaskBatchMergeReportActionApi extends PrivateApiComponentBas
     @Description(desc = "工单批量合并上报触发动作")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        INotifyHandler handler = NotifyHandlerFactory.getHandler(EmailNotifyHandler.class.getSimpleName());
+        INotifyHandler handler = NotifyHandlerFactory.getHandler("EmailNotifyHandler");
         if (handler == null) {
-            throw new NotifyHandlerNotFoundException(EmailNotifyHandler.class.getSimpleName());
+            throw new NotifyHandlerNotFoundException("EmailNotifyHandler");
         }
         Long processTaskId = paramObj.getLong("processTaskId");
         ProcessTaskVo processTaskVo = processTaskMapper.getProcessTaskBaseInfoById(processTaskId);
