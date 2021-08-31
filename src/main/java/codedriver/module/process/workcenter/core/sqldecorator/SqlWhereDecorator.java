70e197567cea49d2a0a34806b92a23a65086d82c
吕佐康 <lvzk@techsure.com.cn>
2021-08-31 17:11:00 +0800

    --story=1005238 --user=lvzk 工单中心 - 默认出厂添加”我的草稿“分类 https://www.tapd.cn/54247054/s/1175141

--- src/main/java/codedriver/module/process/workcenter/core/sqldecorator/SqlWhereDecorator.java
+++ src/main/java/codedriver/module/process/workcenter/core/sqldecorator/SqlWhereDecorator.java
@@ -238,6 +238,10 @@ public class SqlWhereDecorator extends SqlDecoratorBase {
         if (!isHasProcessTaskAuth) {
             sqlSb.append(" and pt.is_show = 1 ");
         }
+        //只有”我的草稿“分类才显示工单状态”未提交“的工单
+        if(!Objects.equals("draftProcessTask",workcenterVo.getUuid())){
+            sqlSb.append(" and pt.status != 'draft' ");
+        }
         //其它条件过滤
         buildWhereMap.get(workcenterVo.getSqlFieldType()).build(workcenterVo, sqlSb);
 
