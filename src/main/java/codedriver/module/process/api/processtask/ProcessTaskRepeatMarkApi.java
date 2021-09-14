d8f4d7c06d8378c99b862acdcf378028138922bd
林邦泉 <linbq@techsure.com.cn>
2021-09-14 18:02:46 +0800

    --task=1005296 --user=林邦泉 后端-苏州银行-批量关闭事件单 https://www.tapd.cn/54247054/s/1177958

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskRepeatMarkApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskRepeatMarkApi.java
@@ -10,14 +10,18 @@ import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.constvalue.SystemUser;
 import codedriver.framework.exception.type.ParamNotExistsException;
+import codedriver.framework.exception.type.PermissionDeniedException;
 import codedriver.framework.process.auth.PROCESS_BASE;
+import codedriver.framework.process.constvalue.ProcessTaskOperationType;
 import codedriver.framework.process.constvalue.ProcessTaskStatus;
 import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
 import codedriver.framework.process.dto.ProcessTaskRepeatVo;
 import codedriver.framework.process.dto.ProcessTaskStepVo;
 import codedriver.framework.process.dto.ProcessTaskVo;
+import codedriver.framework.process.exception.processtask.ProcessTaskNoPermissionException;
 import codedriver.framework.process.exception.processtask.ProcessTaskNotFoundException;
 import codedriver.framework.process.notify.constvalue.TaskNotifyTriggerType;
+import codedriver.framework.process.operationauth.core.ProcessAuthManager;
 import codedriver.framework.process.service.ProcessTaskService;
 import codedriver.framework.process.stephandler.core.IProcessStepHandlerUtil;
 import codedriver.framework.process.stephandler.core.ProcessStepHandlerFactory;
@@ -81,6 +85,12 @@ public class ProcessTaskRepeatMarkApi extends PrivateApiComponentBase {
     public Object myDoService(JSONObject paramObj) throws Exception {
         Long processTaskId = paramObj.getLong("processTaskId");
         processTaskService.checkProcessTaskParamsIsLegal(processTaskId);
+        try {
+            new ProcessAuthManager.TaskOperationChecker(processTaskId, ProcessTaskOperationType.PROCESSTASK_ENABLEMARKREPEAT)
+                    .build().checkAndNoPermissionThrowException();
+        } catch (ProcessTaskNoPermissionException e) {
+            throw new PermissionDeniedException();
+        }
         JSONArray repeatProcessTaskIdArray = paramObj.getJSONArray("repeatProcessTaskIdList");
         if (CollectionUtils.isEmpty(repeatProcessTaskIdArray)) {
             throw new ParamNotExistsException("repeatProcessTaskIdList");
