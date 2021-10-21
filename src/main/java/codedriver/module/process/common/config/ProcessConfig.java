dd225a3bfe3fde148dd59de47d4bb1d230582b3c
陈其炜 <chenqw@techsure.com.cn>
2021-10-21 14:07:28 +0800

    优化mongodb连接方式
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/process/common/config/ProcessConfig.java
+++ src/main/java/codedriver/module/process/common/config/ProcessConfig.java
@@ -1,35 +1,38 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.module.process.common.config;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Properties;
-import java.util.concurrent.Executor;
-
-import javax.annotation.PostConstruct;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import codedriver.framework.common.RootConfiguration;
 import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.api.exception.NacosException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import codedriver.framework.common.RootConfiguration;
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+import java.util.concurrent.Executor;
 
 @RootConfiguration
 public class ProcessConfig {
-	private static Logger logger = LoggerFactory.getLogger(ProcessConfig.class);
+	private static final Logger logger = LoggerFactory.getLogger(ProcessConfig.class);
 	@NacosInjected
 	private ConfigService configService;
 	private static final String CONFIG_FILE = "config.properties";
 
 	private static String MOBILE_FORM_UI_TYPE;
 
-	public static final String MOBILE_FORM_UI_TYPE() {
+	public static String MOBILE_FORM_UI_TYPE() {
 		return MOBILE_FORM_UI_TYPE;
 	}
 
@@ -61,7 +64,7 @@ public class ProcessConfig {
 				prop.load(new ByteArrayInputStream(configInfo.getBytes()));
 			} else {
 				// 如果从nacos中读不出配置，则使用本地配置文件配置
-				prop.load(new InputStreamReader(ProcessConfig.class.getClassLoader().getResourceAsStream(CONFIG_FILE), "UTF-8"));
+				prop.load(new InputStreamReader(ProcessConfig.class.getClassLoader().getResourceAsStream(CONFIG_FILE), StandardCharsets.UTF_8));
 			}
 			
 			MOBILE_FORM_UI_TYPE = prop.getProperty("mobile.form.ui.type", "0");
