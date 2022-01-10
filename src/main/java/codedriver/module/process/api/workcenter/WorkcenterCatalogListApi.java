a8336c98a2fb86d3f8bf3baaab562a915bc9969f
longrf <techsure901>
2022-01-10 16:49:59 +0800

    --story=1005805 --user=龙儒锋 苏州银行-工单中心查询条件添加分组功能 https://www.tapd.cn/54247054/s/1202189

---
+++ src/main/java/codedriver/module/process/api/workcenter/WorkcenterCatalogListApi.java
@@ -0,0 +1,53 @@
+package codedriver.module.process.api.workcenter;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.process.auth.PROCESS_BASE;
+import codedriver.framework.process.dao.mapper.workcenter.WorkcenterMapper;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author longrf
+ * @date 2022/1/10 2:34 下午
+ */
+@Service
+@AuthAction(action = PROCESS_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class WorkcenterCatalogListApi extends PrivateApiComponentBase {
+
+    @Resource
+    WorkcenterMapper workcenterMapper;
+
+    @Override
+    public String getName() {
+        return "查询工单中心菜单类型列表";
+    }
+
+    @Override
+    public String getToken() {
+        return "workcenter/catalog/list";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+    })
+    @Output({
+            @Param(name = "name", type = ApiParamType.STRING, isRequired = true, desc = "类型名")
+    })
+    @Description(desc = "查询工单中心菜单类型列表接口")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        return workcenterMapper.getWorkcenterCatalogListByName(paramObj.getString("name"));
+    }
+
+}
---
+++ src/main/java/codedriver/module/process/api/workcenter/WorkcenterCatalogSaveApi.java
@@ -0,0 +1,71 @@
+package codedriver.module.process.api.workcenter;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.process.auth.PROCESS_BASE;
+import codedriver.framework.process.dao.mapper.workcenter.WorkcenterMapper;
+import codedriver.framework.process.exception.workcenter.WorkcenterCatalogIdNotFoundException;
+import codedriver.framework.process.exception.workcenter.WorkcenterCatalogNameRepeatsException;
+import codedriver.framework.process.workcenter.dto.WorkcenterCatalogVo;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author longrf
+ * @date 2022/1/10 2:34 下午
+ */
+@Service
+@AuthAction(action = PROCESS_BASE.class)
+@OperationType(type = OperationTypeEnum.CREATE)
+public class WorkcenterCatalogSaveApi extends PrivateApiComponentBase {
+
+
+    @Resource
+    WorkcenterMapper workcenterMapper;
+
+    @Override
+    public String getName() {
+        return "保存工单中心菜单类型";
+    }
+
+    @Override
+    public String getToken() {
+        return "workcenter/catalog/save";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "id", type = ApiParamType.STRING, desc = "类型id"),
+            @Param(name = "name", type = ApiParamType.STRING, isRequired = true, desc = "类型名")
+    })
+    @Output({
+    })
+    @Description(desc = "保存工单中心菜单类型接口")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        Long id = paramObj.getLong("id");
+        WorkcenterCatalogVo catalogVo = JSON.toJavaObject(paramObj, WorkcenterCatalogVo.class);
+        if (workcenterMapper.checkWorkcenterCatalogNameIsRepeats(catalogVo) > 0) {
+            throw new WorkcenterCatalogNameRepeatsException(catalogVo.getName());
+        }
+        if (id != null) {
+            if (workcenterMapper.checkWorkcenterCatalogIsExists(id) == 0) {
+                throw new WorkcenterCatalogIdNotFoundException(id);
+            }
+            workcenterMapper.updateWorkcenterCatalog(catalogVo);
+        } else {
+            workcenterMapper.insertWorkcenterCatalog(catalogVo);
+        }
+        return null;
+    }
+}
