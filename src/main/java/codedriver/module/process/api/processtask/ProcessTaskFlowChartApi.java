162d903df2b148d1018007e6e59340e5970ae52f
linbq@techsure.com.cn <Besiter789>
2022-05-16 14:43:36 +0800

    --task=1006320 --user=林邦泉 后端-工单处理-流程图查看，展示子任务处理人策略，类型工单中心 https://www.tapd.cn/54247054/s/1228294

--- src/main/java/codedriver/module/process/api/processtask/ProcessTaskFlowChartApi.java
+++ src/main/java/codedriver/module/process/api/processtask/ProcessTaskFlowChartApi.java
@@ -10,7 +10,9 @@ import codedriver.framework.process.constvalue.ProcessTaskStepUserStatus;
 import codedriver.framework.process.constvalue.ProcessUserType;
 import codedriver.framework.process.dao.mapper.ChannelMapper;
 import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
+import codedriver.framework.process.dao.mapper.ProcessTaskStepTaskMapper;
 import codedriver.framework.process.dao.mapper.SelectContentByHashMapper;
+import codedriver.framework.process.dao.mapper.task.TaskMapper;
 import codedriver.framework.process.dto.*;
 import codedriver.framework.process.exception.channel.ChannelNotFoundException;
 import codedriver.framework.process.exception.process.ProcessNotFoundException;
@@ -19,14 +21,17 @@ import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.process.dao.mapper.ProcessMapper;
 import codedriver.module.process.service.ProcessTaskService;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @AuthAction(action = PROCESS_BASE.class)
@@ -47,6 +52,10 @@ public class ProcessTaskFlowChartApi extends PrivateApiComponentBase {
 
     @Resource
     private ProcessTaskService processTaskService;
+    @Resource
+    private TaskMapper taskMapper;
+    @Resource
+    private ProcessTaskStepTaskMapper processTaskStepTaskMapper;
 
     @Override
     public String getToken() {
@@ -84,6 +93,10 @@ public class ProcessTaskFlowChartApi extends PrivateApiComponentBase {
             if (CollectionUtils.isNotEmpty(processTaskStepList)) {
                 for (ProcessTaskStepVo processTaskStepVo : processTaskStepList) {
                     processTaskService.setProcessTaskStepUser(processTaskStepVo);
+                    List<ProcessTaskStepUserVo> minorUserList = processTaskStepVo.getMinorUserList();
+                    if (CollectionUtils.isNotEmpty(minorUserList)) {
+                        setTaskConfigName(processTaskStepVo, minorUserList);
+                    }
                     processTaskStepVo.setReplaceableTextList(processTaskService.getReplaceableTextList(processTaskStepVo));
                     processTaskStepVo.setAssignableWorkerStepList(null);
                     processTaskStepVo.setBackwardNextStepList(null);
@@ -152,4 +165,55 @@ public class ProcessTaskFlowChartApi extends PrivateApiComponentBase {
         }
     }
 
+    /**
+     * 设置子任务处理人对应当的子任务名称
+     * @param processTaskStepVo
+     * @param minorUserList
+     */
+    private void setTaskConfigName(ProcessTaskStepVo processTaskStepVo, List<ProcessTaskStepUserVo> minorUserList) {
+        Map<String, Set<String>> userUuidTaskConfigNameMap = new HashMap<>();
+        String stepConfigStr = selectContentByHashMapper.getProcessTaskStepConfigByHash(processTaskStepVo.getConfigHash());
+        JSONObject taskConfig = (JSONObject) JSONPath.read(stepConfigStr, "taskConfig");
+        if (MapUtils.isNotEmpty(taskConfig)) {
+            JSONArray idArray = taskConfig.getJSONArray("idList");
+            if (CollectionUtils.isNotEmpty(idArray)) {
+                List<TaskConfigVo> taskConfigList = taskMapper.getTaskConfigByIdList(idArray);
+                if (CollectionUtils.isNotEmpty(taskConfigList)) {
+                    Map<Long, TaskConfigVo> taskConfigMap = taskConfigList.stream().collect(Collectors.toMap(e -> e.getId(), e -> e));
+                    List<ProcessTaskStepTaskVo> processTaskStepTaskList = processTaskStepTaskMapper.getStepTaskListByProcessTaskStepId(processTaskStepVo.getId());
+                    if (CollectionUtils.isEmpty(processTaskStepTaskList)) {
+                        Map<Long, ProcessTaskStepTaskVo> stepTaskIdMap= processTaskStepTaskList.stream().collect(Collectors.toMap(e -> e.getId(), e -> e));
+                        List<Long> stepTaskIdList= processTaskStepTaskList.stream().map(ProcessTaskStepTaskVo::getId).collect(Collectors.toList());
+                        List<ProcessTaskStepTaskUserVo> stepTaskUserList = processTaskStepTaskMapper.getStepTaskUserByStepTaskIdList(stepTaskIdList);
+                        for (ProcessTaskStepTaskUserVo stepTaskUserVo : stepTaskUserList) {
+                            if (Objects.equals(stepTaskUserVo.getIsDelete(), 1)) {
+                                continue;
+                            }
+                            ProcessTaskStepTaskVo processTaskStepTaskVo = stepTaskIdMap.get(stepTaskUserVo.getProcessTaskStepTaskId());
+                            if (processTaskStepTaskVo == null) {
+                                continue;
+                            }
+                            TaskConfigVo taskConfigVo = taskConfigMap.get(processTaskStepTaskVo.getTaskConfigId());
+                            if (taskConfigVo == null) {
+                                continue;
+                            }
+                            if (StringUtils.isBlank(taskConfigVo.getName())) {
+                                continue;
+                            }
+                            Set<String> taskConfigNameSet = userUuidTaskConfigNameMap.computeIfAbsent(stepTaskUserVo.getUserUuid(), key -> new HashSet<>());
+                            taskConfigNameSet.add(taskConfigVo.getName());
+                        }
+                    }
+                }
+            }
+        }
+        for (ProcessTaskStepUserVo minorUserVo : minorUserList) {
+            Set<String> taskConfigNameSet = userUuidTaskConfigNameMap.get(minorUserVo.getUserUuid());
+            if (CollectionUtils.isNotEmpty(taskConfigNameSet)) {
+                minorUserVo.setTaskType(String.join("、", taskConfigNameSet));
+            } else {
+                minorUserVo.setTaskType("变更步骤");
+            }
+        }
+    }
 }
