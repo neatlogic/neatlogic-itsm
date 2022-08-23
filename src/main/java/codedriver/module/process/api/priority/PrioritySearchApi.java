5b8e4cbfe8be7c6d987c347934bd3bf59d22b587
linbq@techsure.com.cn <Besiter789>
2022-08-23 18:33:45 +0800

    --story=1006562 --user=林邦泉 流程节点通知动作添加审计功能 https://www.tapd.cn/54247054/s/1248544

--- src/main/java/codedriver/module/process/api/priority/PrioritySearchApi.java
+++ src/main/java/codedriver/module/process/api/priority/PrioritySearchApi.java
@@ -8,8 +8,6 @@ import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
