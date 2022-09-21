a7d43981bc5a52e72a2c384fa360e3fe279ab140
linbq@techsure.com.cn <Besiter789>
2022-09-21 15:56:21 +0800

    --story=1006886 --user=林邦泉 用户评分提示功能开发 https://www.tapd.cn/54247054/s/1253346

---
+++ src/main/java/codedriver/module/process/api/processtask/UpdateProcessTaskNeedScoreApi.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.process.api.processtask;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.process.dao.mapper.ProcessTaskMapper;
+import codedriver.framework.process.dao.mapper.SelectContentByHashMapper;
+import codedriver.framework.process.dto.ProcessTaskVo;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@OperationType(type = OperationTypeEnum.UPDATE)
+public class UpdateProcessTaskNeedScoreApi extends PrivateApiComponentBase {
+
+    @Resource
+    private ProcessTaskMapper processTaskMapper;
+    @Resource
+    private SelectContentByHashMapper selectContentByHashMapper;
+
+    @Override
+    public String getToken() {
+        return "processtask/needscore/update";
+    }
+
+    @Override
+    public String getName() {
+        return "批量更新工单的needScore字段值";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "idList", type = ApiParamType.JSONARRAY, desc = "工单ID列表")
+    })
+    @Output({})
+    @Description(desc = "批量更新工单的needScore字段值")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        JSONArray idArray = paramObj.getJSONArray("idList");
+        if (CollectionUtils.isNotEmpty(idArray)) {
+            List<Long> idList = idArray.toJavaList(Long.class);
+            updateNeedScoreField(idList);
+        } else {
+            int rowNum = processTaskMapper.getAllProcessTaskCount();
+            if (rowNum > 0) {
+                ProcessTaskVo searchVo = new ProcessTaskVo();
+                searchVo.setRowNum(rowNum);
+                searchVo.setPageSize(100);
+                int pageCount = searchVo.getPageCount();
+                for (int currentPage = 1; currentPage <= pageCount; currentPage++) {
+                    searchVo.setCurrentPage(currentPage);
+                    List<Long> idList = processTaskMapper.getProcessTaskIdList(searchVo);
+                    if (CollectionUtils.isNotEmpty(idList)) {
+                        updateNeedScoreField(idList);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private void updateNeedScoreField(List<Long> idList) {
+        List<Long> needScoreProcessTaskIdList = new ArrayList<>();
+        List<Long> noNeedScoreProcessTaskIdList = new ArrayList<>();
+        List<ProcessTaskVo> processTaskList = processTaskMapper.getProcessTaskListByIdList(idList);
+        for (ProcessTaskVo processTaskVo : processTaskList) {
+            if (processTaskVo.getNeedScore() == null) {
+                String config = selectContentByHashMapper.getProcessTaskConfigStringByHash(processTaskVo.getConfigHash());
+                Integer isActive = (Integer) JSONPath.read(config, "process.scoreConfig.isActive");
+                if (Objects.equals(isActive, 1)) {
+                    needScoreProcessTaskIdList.add(processTaskVo.getId());
+                } else {
+                    noNeedScoreProcessTaskIdList.add(processTaskVo.getId());
+                }
+            }
+
+        }
+        if (CollectionUtils.isNotEmpty(needScoreProcessTaskIdList)) {
+            processTaskMapper.updateProcessTaskNeedScoreByIdList(needScoreProcessTaskIdList, 1);
+        }
+        if (CollectionUtils.isNotEmpty(noNeedScoreProcessTaskIdList)) {
+            processTaskMapper.updateProcessTaskNeedScoreByIdList(noNeedScoreProcessTaskIdList, 0);
+        }
+    }
+}
