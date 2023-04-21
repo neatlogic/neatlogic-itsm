a78126308b5daab6b6ae177138d2218f5f8043e3
lvzk <897706680@qq.com>
2023-04-21 19:35:16 +0800

    【异常邮件补充邮件相关参数以及异常堆栈信息】 https://www.tapd.cn/54247054/prong/stories/view/1154247054001007511

--- src/main/java/neatlogic/module/process/message/handler/ProcessTaskMessageHandler.java
+++ src/main/java/neatlogic/module/process/message/handler/ProcessTaskMessageHandler.java
@@ -1,7 +1,12 @@
 package neatlogic.module.process.message.handler;
 
 import neatlogic.framework.message.core.MessageHandlerBase;
+import neatlogic.framework.notify.core.INotifyPolicyHandler;
+import neatlogic.framework.notify.core.NotifyPolicyHandlerFactory;
 import neatlogic.framework.notify.dto.NotifyVo;
+import neatlogic.framework.process.dto.ProcessTaskStepVo;
+import neatlogic.framework.util.I18nUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,5 +39,21 @@ public class ProcessTaskMessageHandler extends MessageHandlerBase {
     @Override
     public NotifyVo compress(List<NotifyVo> notifyVoList) {
         return null;
+    }
+
+    /**
+     * 获取发起方信息，目前用于异常邮件
+     *
+     * @return 发起方信息
+     */
+    @Override
+    public String getCallerMessage(NotifyVo notifyVo) {
+        ProcessTaskStepVo taskStepVo = (ProcessTaskStepVo) notifyVo.getCallerData();
+        String policyHandlerName = StringUtils.EMPTY;
+        INotifyPolicyHandler policyHandler = NotifyPolicyHandlerFactory.getHandler(notifyVo.getNotifyPolicyHandler());
+        if (policyHandler != null) {
+            policyHandlerName = policyHandler.getName();
+        }
+        return "<p>" + I18nUtils.getMessage("modulegroup.itsm") + "-" + I18nUtils.getMessage(policyHandlerName) + "-" + notifyVo.getCallerNotifyPolicyVo().getName() + "-" + notifyVo.getTriggerType().getText() + "</p>" + I18nUtils.getMessage("handler.message.itsm.callermessage", taskStepVo.getProcessTaskId().toString(), taskStepVo.getName(), taskStepVo.getId().toString());
     }
 }
