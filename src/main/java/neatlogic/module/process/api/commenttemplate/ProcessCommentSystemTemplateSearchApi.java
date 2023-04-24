9b3dcce973e2273bf8343e4d87321d8a6095964e
longrf <a5739119>
2023-04-24 15:01:38 +0800

    --story=1007485 --user=龙儒锋 ITSM-回复模板修改 https://www.tapd.cn/54247054/s/1271164

--- src/main/java/neatlogic/module/process/api/commenttemplate/ProcessCommentSystemTemplateSearchApi.java
+++ src/main/java/neatlogic/module/process/api/commenttemplate/ProcessCommentSystemTemplateSearchApi.java
@@ -1,24 +1,28 @@
 package neatlogic.module.process.api.commenttemplate;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import neatlogic.framework.asynchronization.threadlocal.UserContext;
 import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.core.AuthActionChecker;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.common.dto.BasePageVo;
 import neatlogic.framework.common.util.PageUtil;
 import neatlogic.framework.process.auth.PROCESS_BASE;
+import neatlogic.framework.process.auth.PROCESS_COMMENT_TEMPLATE_MODIFY;
 import neatlogic.framework.process.dao.mapper.ProcessCommentTemplateMapper;
 import neatlogic.framework.process.dto.ProcessCommentTemplateVo;
-import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.annotation.Input;
 import neatlogic.framework.restful.annotation.OperationType;
 import neatlogic.framework.restful.annotation.Output;
 import neatlogic.framework.restful.annotation.Param;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -50,13 +54,19 @@ public class ProcessCommentSystemTemplateSearchApi extends PrivateApiComponentBa
             @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页")
     })
     @Output({
-            @Param(explode= BasePageVo.class),
+            @Param(explode = BasePageVo.class),
             @Param(name = "tbodyList", explode = ProcessCommentTemplateVo.class, desc = "回复模版集合")
     })
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        ProcessCommentTemplateVo vo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<ProcessCommentTemplateVo>() {});
-        vo.setType(ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue());
+        ProcessCommentTemplateVo vo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<ProcessCommentTemplateVo>() {
+        });
+//        vo.setType(ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue());
+
+        if (!AuthActionChecker.check(PROCESS_COMMENT_TEMPLATE_MODIFY.class.getSimpleName())) {
+            vo.setAuthList(Collections.singletonList(UserContext.get().getUserUuid()));
+            vo.setType(ProcessCommentTemplateVo.TempalteType.CUSTOM.getValue());
+        }
         JSONObject returnObj = new JSONObject();
         if (vo.getNeedPage()) {
             int rowNum = commentTemplateMapper.searchTemplateCount(vo);
