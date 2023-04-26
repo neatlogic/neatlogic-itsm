fb922d499dd6996d81931d6ea43d2f326386ea9c
longrf <a5739119>
2023-04-26 16:10:17 +0800

    --story=1007485 --user=龙儒锋 ITSM-回复模板修改 https://www.tapd.cn/54247054/s/1271164

--- src/main/java/neatlogic/module/process/api/commenttemplate/ProcessCommentSystemTemplateSearchApi.java
+++ src/main/java/neatlogic/module/process/api/commenttemplate/ProcessCommentSystemTemplateSearchApi.java
@@ -5,12 +5,14 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import neatlogic.framework.asynchronization.threadlocal.UserContext;
 import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.core.AuthActionChecker;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.common.constvalue.UserType;
 import neatlogic.framework.common.dto.BasePageVo;
 import neatlogic.framework.common.util.PageUtil;
 import neatlogic.framework.dto.AuthenticationInfoVo;
 import neatlogic.framework.process.auth.PROCESS_BASE;
+import neatlogic.framework.process.auth.PROCESS_COMMENT_TEMPLATE_MODIFY;
 import neatlogic.framework.process.dao.mapper.ProcessCommentTemplateMapper;
 import neatlogic.framework.process.dto.ProcessCommentTemplateVo;
 import neatlogic.framework.restful.annotation.Input;
@@ -20,6 +22,7 @@ import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.service.AuthenticationInfoService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -86,6 +89,18 @@ public class ProcessCommentSystemTemplateSearchApi extends PrivateApiComponentBa
             returnObj.put("pageCount", PageUtil.getPageCount(rowNum, vo.getPageSize()));
         }
         List<ProcessCommentTemplateVo> tbodyList = commentTemplateMapper.searchTemplate(vo);
+        if(CollectionUtils.isNotEmpty(tbodyList)){
+            //有系统模版管理权限才能编辑系统模版
+            tbodyList.forEach(o -> {
+                if((ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue().equals(o.getType())
+                        && AuthActionChecker.check(PROCESS_COMMENT_TEMPLATE_MODIFY.class.getSimpleName()))
+                        || ProcessCommentTemplateVo.TempalteType.CUSTOM.getValue().equals(o.getType())){
+                    o.setIsEditable(1);
+                }else{
+                    o.setIsEditable(0);
+                }
+            });
+        }
         returnObj.put("tbodyList", tbodyList);
         return returnObj;
     }
