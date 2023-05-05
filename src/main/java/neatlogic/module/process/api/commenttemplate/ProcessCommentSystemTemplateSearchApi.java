f510e66631307f6fb7aa3ac9217dce45ec1651c7
linbangquan <1437892690@qq.com>
2023-05-05 19:44:54 +0800

    --bug=1010931 回复模板管理-统计数与列表实际模板数量不一致 https://www.tapd.cn/54247054/s/1272176

--- src/main/java/neatlogic/module/process/api/commenttemplate/ProcessCommentSystemTemplateSearchApi.java
+++ src/main/java/neatlogic/module/process/api/commenttemplate/ProcessCommentSystemTemplateSearchApi.java
@@ -1,18 +1,16 @@
 package neatlogic.module.process.api.commenttemplate;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
 import neatlogic.framework.asynchronization.threadlocal.UserContext;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.core.AuthActionChecker;
 import neatlogic.framework.common.constvalue.ApiParamType;
-import neatlogic.framework.common.constvalue.UserType;
 import neatlogic.framework.common.dto.BasePageVo;
-import neatlogic.framework.common.util.PageUtil;
 import neatlogic.framework.process.auth.PROCESS_BASE;
 import neatlogic.framework.process.auth.PROCESS_COMMENT_TEMPLATE_MODIFY;
 import neatlogic.framework.process.dao.mapper.ProcessCommentTemplateMapper;
+import neatlogic.framework.process.dto.ProcessCommentTemplateAuthVo;
 import neatlogic.framework.process.dto.ProcessCommentTemplateVo;
 import neatlogic.framework.restful.annotation.Input;
 import neatlogic.framework.restful.annotation.OperationType;
@@ -20,11 +18,13 @@ import neatlogic.framework.restful.annotation.Output;
 import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.framework.util.TableResultUtil;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @AuthAction(action = PROCESS_BASE.class)
@@ -61,37 +61,84 @@ public class ProcessCommentSystemTemplateSearchApi extends PrivateApiComponentBa
     })
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        ProcessCommentTemplateVo vo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<ProcessCommentTemplateVo>() {
-        });
-//        vo.setType(ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue());
-
-        //根据当前用户所在组、角色筛选其能看到的模版
-        List<String> uuidList = UserContext.get().getUuidList();
-        uuidList.add(UserType.ALL.getValue());
-        vo.setAuthList(uuidList);
-
-        JSONObject returnObj = new JSONObject();
-        if (vo.getNeedPage()) {
-            int rowNum = commentTemplateMapper.searchTemplateCount(vo);
-            returnObj.put("pageSize", vo.getPageSize());
-            returnObj.put("currentPage", vo.getCurrentPage());
-            returnObj.put("rowNum", rowNum);
-            returnObj.put("pageCount", PageUtil.getPageCount(rowNum, vo.getPageSize()));
+//        ProcessCommentTemplateVo vo = JSON.parseObject(jsonObj.toJSONString(), new TypeReference<ProcessCommentTemplateVo>() {
+//        });
+////        vo.setType(ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue());
+//
+//        //根据当前用户所在组、角色筛选其能看到的模版
+//        List<String> uuidList = UserContext.get().getUuidList();
+//        uuidList.add(UserType.ALL.getValue());
+//        vo.setAuthList(uuidList);
+//
+//        JSONObject returnObj = new JSONObject();
+//        if (vo.getNeedPage()) {
+//            int rowNum = commentTemplateMapper.searchTemplateCount(vo);
+//            returnObj.put("pageSize", vo.getPageSize());
+//            returnObj.put("currentPage", vo.getCurrentPage());
+//            returnObj.put("rowNum", rowNum);
+//            returnObj.put("pageCount", PageUtil.getPageCount(rowNum, vo.getPageSize()));
+//        }
+//        List<ProcessCommentTemplateVo> tbodyList = commentTemplateMapper.searchTemplate(vo);
+//        if(CollectionUtils.isNotEmpty(tbodyList)){
+//            //有系统模版管理权限才能编辑系统模版
+//            tbodyList.forEach(o -> {
+//                if((ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue().equals(o.getType())
+//                        && AuthActionChecker.check(PROCESS_COMMENT_TEMPLATE_MODIFY.class.getSimpleName()))
+//                        || ProcessCommentTemplateVo.TempalteType.CUSTOM.getValue().equals(o.getType())){
+//                    o.setIsEditable(1);
+//                }else{
+//                    o.setIsEditable(0);
+//                }
+//            });
+//        }
+//        returnObj.put("tbodyList", tbodyList);
+//        return returnObj;
+        BasePageVo searchVo = jsonObj.toJavaObject(BasePageVo.class);
+        List<Long> customTemplateIdList = commentTemplateMapper.getCustomTemplateIdListByUserUuid(UserContext.get().getUserUuid());
+        if (CollectionUtils.isNotEmpty(customTemplateIdList)) {
+            JSONArray defaultValue = new JSONArray();
+            for (Long id : customTemplateIdList) {
+                defaultValue.add(id);
+            }
+            searchVo.setDefaultValue(defaultValue);
         }
-        List<ProcessCommentTemplateVo> tbodyList = commentTemplateMapper.searchTemplate(vo);
+        int rowNum = commentTemplateMapper.searchCommentTemplateCount(searchVo);
+        if (rowNum == 0) {
+            return TableResultUtil.getResult(new ArrayList(), searchVo);
+        }
+        searchVo.setRowNum(rowNum);
+        List<ProcessCommentTemplateVo> tbodyList = commentTemplateMapper.searchCommentTemplateList(searchVo);
+        List<Long> systemTemplateIdList = tbodyList.stream().filter(e -> ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue().equals(e.getType())).map(ProcessCommentTemplateVo::getId).collect(Collectors.toList());
+        Map<Long, List<ProcessCommentTemplateAuthVo>> authMap = new HashMap<>();
+        if (CollectionUtils.isNotEmpty(systemTemplateIdList)) {
+            List<ProcessCommentTemplateAuthVo> authVoList = commentTemplateMapper.getProcessCommentTemplateAuthListByCommentTemplateIdList(systemTemplateIdList);
+            for (ProcessCommentTemplateAuthVo authVo : authVoList) {
+                Long commentTemplateId = authVo.getCommentTemplateId();
+                authMap.computeIfAbsent(commentTemplateId, key -> new ArrayList<>()).add(authVo);
+            }
+        }
         if(CollectionUtils.isNotEmpty(tbodyList)){
             //有系统模版管理权限才能编辑系统模版
-            tbodyList.forEach(o -> {
-                if((ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue().equals(o.getType())
-                        && AuthActionChecker.check(PROCESS_COMMENT_TEMPLATE_MODIFY.class.getSimpleName()))
-                        || ProcessCommentTemplateVo.TempalteType.CUSTOM.getValue().equals(o.getType())){
-                    o.setIsEditable(1);
-                }else{
-                    o.setIsEditable(0);
+            for (ProcessCommentTemplateVo processCommentTemplateVo : tbodyList) {
+                if(ProcessCommentTemplateVo.TempalteType.SYSTEM.getValue().equals(processCommentTemplateVo.getType())){
+                    List<ProcessCommentTemplateAuthVo> authVoList = authMap.get(processCommentTemplateVo.getId());
+                    if (CollectionUtils.isNotEmpty(authVoList)) {
+                        List<String> authList = new ArrayList<>(authVoList.size());
+                        for (ProcessCommentTemplateAuthVo authVo : authVoList) {
+                            authList.add(authVo.getType() + "#" + authVo.getUuid());
+                        }
+                        processCommentTemplateVo.setAuthList(authList);
+                    }
+                    if (AuthActionChecker.check(PROCESS_COMMENT_TEMPLATE_MODIFY.class.getSimpleName())) {
+                        processCommentTemplateVo.setIsEditable(1);
+                    } else{
+                        processCommentTemplateVo.setIsEditable(0);
+                    }
+                } else if (ProcessCommentTemplateVo.TempalteType.CUSTOM.getValue().equals(processCommentTemplateVo.getType())) {
+                    processCommentTemplateVo.setIsEditable(1);
                 }
-            });
+            }
         }
-        returnObj.put("tbodyList", tbodyList);
-        return returnObj;
+        return TableResultUtil.getResult(tbodyList, searchVo);
     }
 }
