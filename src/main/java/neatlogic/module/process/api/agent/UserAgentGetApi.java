61bcf7fe0c32f46a13c415ffff9b48eb202b8eee
chenqw <zenkiray@live.com>
2023-06-19 18:48:13 +0800

    优化代码
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/module/process/api/agent/UserAgentGetApi.java
+++ src/main/java/neatlogic/module/process/api/agent/UserAgentGetApi.java
@@ -1,27 +1,38 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package neatlogic.module.process.api.agent;
 
-import neatlogic.framework.asynchronization.threadlocal.UserContext;
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.dao.mapper.UserMapper;
-import neatlogic.framework.dto.UserAuthVo;
-import neatlogic.framework.dto.UserVo;
 import neatlogic.framework.process.auth.PROCESS_BASE;
-import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import com.alibaba.fastjson.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import javax.annotation.Resource;
 @Deprecated
 //@Service
 @AuthAction(action = PROCESS_BASE.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class UserAgentGetApi extends PrivateApiComponentBase {
 
-	@Autowired
+	@Resource
 	private UserMapper userMapper;
 	
 	@Override
@@ -39,7 +50,6 @@ public class UserAgentGetApi extends PrivateApiComponentBase {
 		return null;
 	}
 
-	@Input({})
 	@Output({
 			@Param(name = "uuid", type = ApiParamType.STRING, desc = "用户uuid"),
 			@Param(name = "userId", type = ApiParamType.STRING, desc = "用户userId"),
--- src/main/java/neatlogic/module/process/api/agent/UserAgentSaveApi.java
+++ src/main/java/neatlogic/module/process/api/agent/UserAgentSaveApi.java
@@ -1,30 +1,39 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package neatlogic.module.process.api.agent;
 
-import neatlogic.framework.asynchronization.threadlocal.UserContext;
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.dao.mapper.UserMapper;
-import neatlogic.framework.dto.UserVo;
-import neatlogic.framework.exception.user.AgentIsUserSelfException;
-import neatlogic.framework.exception.user.UserAgentLoopException;
-import neatlogic.framework.exception.user.UserAgentRepeatException;
-import neatlogic.framework.exception.user.UserHasAgentException;
 import neatlogic.framework.process.auth.PROCESS_BASE;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
 @Deprecated
 //@Service
 @OperationType(type = OperationTypeEnum.CREATE)
 @AuthAction(action = PROCESS_BASE.class)
 public class UserAgentSaveApi extends PrivateApiComponentBase {
 
-	@Autowired
+	@Resource
 	private UserMapper userMapper;
 	
 	@Override
