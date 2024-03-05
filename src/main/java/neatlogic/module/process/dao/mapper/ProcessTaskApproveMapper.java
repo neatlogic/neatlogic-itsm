0b2bdfbc068cb954cfd6add7b8a8c13572e27b74
linbangquan <1437892690@qq.com>
2024-03-05 11:06:17 +0800

    [功能] 后端-审批对接其他模块
    关联 #[1102743677140992]后端-审批对接其他模块 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1102743677140992

--- src/main/java/neatlogic/module/process/dao/mapper/ProcessTaskApproveMapper.java
+++ src/main/java/neatlogic/module/process/dao/mapper/ProcessTaskApproveMapper.java
@@ -3,6 +3,4 @@ package neatlogic.module.process.dao.mapper;
 import neatlogic.framework.process.crossover.IProcessTaskApproveCrossoverMapper;
 
 public interface ProcessTaskApproveMapper extends IProcessTaskApproveCrossoverMapper {
-
-    String getProcessTaskApproveEntityConfigByProcessTaskId(Long processTaskId);
 }
--- src/main/resources/neatlogic/resources/process/changelog/2024-03-01/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/process/changelog/2024-03-01/neatlogic_tenant.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS `processtask_approve_entity_config` (
   PRIMARY KEY (`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='工单审批实体信息hash表';
 
-CREATE TABLE IF NOT EXISTS `processtask_approve_status` (
+CREATE TABLE IF NOT EXISTS `processtask_final_status` (
   `processtask_id` bigint NOT NULL COMMENT '工单id',
-  `approve_status` enum('accept','neutral','deny') COLLATE utf8mb4_general_ci NOT NULL COMMENT '状态',
+  `status` enum('succeed','failed') COLLATE utf8mb4_general_ci NOT NULL COMMENT '状态',
   PRIMARY KEY (`processtask_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='工单审批状态表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='工单最终状态表';
