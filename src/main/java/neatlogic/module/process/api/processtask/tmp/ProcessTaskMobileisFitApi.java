1377f149ec7e8408658bf7256ba2e79a5d5bb95e
lvzk <897706680@qq.com>
2024-03-27 11:47:05 +0800

    [功能]完善移动端工单步骤黑名单

--- src/main/java/neatlogic/module/process/api/processtask/tmp/ProcessTaskMobileisFitApi.java
+++ src/main/java/neatlogic/module/process/api/processtask/tmp/ProcessTaskMobileisFitApi.java
@@ -1,78 +1,68 @@
 package neatlogic.module.process.api.processtask.tmp;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.process.auth.PROCESS_BASE;
+import neatlogic.framework.process.dao.mapper.ProcessTaskMapper;
+import neatlogic.framework.process.dto.ProcessStepHandlerVo;
+import neatlogic.framework.process.dto.ProcessTaskStepVo;
+import neatlogic.framework.process.stephandler.core.ProcessStepHandlerFactory;
 import neatlogic.framework.process.stephandler.core.ProcessStepHandlerTypeFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSONObject;
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
 
-import neatlogic.framework.common.constvalue.ApiParamType;
-import neatlogic.framework.process.constvalue.ProcessStepHandlerType;
-import neatlogic.framework.process.dao.mapper.ProcessTaskMapper;
-import neatlogic.framework.process.dto.ProcessTaskStepVo;
-import neatlogic.framework.restful.constvalue.OperationTypeEnum;
-import neatlogic.framework.restful.annotation.Description;
-import neatlogic.framework.restful.annotation.Input;
-import neatlogic.framework.restful.annotation.OperationType;
-import neatlogic.framework.restful.annotation.Output;
-import neatlogic.framework.restful.annotation.Param;
-import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 @Service
 @AuthAction(action = PROCESS_BASE.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class ProcessTaskMobileisFitApi extends PrivateApiComponentBase {
 
-	@Autowired
-	private ProcessTaskMapper processTaskMapper;
-	
-	@Override
-	public String getToken() {
-		return "processtask/mobile/isfit";
-	}
+    @Resource
+    private ProcessTaskMapper processTaskMapper;
 
-	@Override
-	public String getName() {
-		return "查看工单是否支持移动端";
-	}
+    @Override
+    public String getToken() {
+        return "processtask/mobile/isfit";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getName() {
+        return "查看工单是否支持移动端";
+    }
 
-	@Input({
-		@Param(name = "processTaskId", type = ApiParamType.LONG, isRequired = true, desc = "工单id")
-	})
-	@Output({
-		@Param(name = "Return", type = ApiParamType.BOOLEAN, desc = "工单是否支持移动端，1：支持，0：不支持;不支持则移动端提示不支持")
-	})
-	@Description(desc = "临时屏蔽移动端工单查看处理接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		//TODO 临时屏蔽移动端工单
-		JSONObject result = new JSONObject();
-		result.put("isfit", true);
-		//屏蔽节点
-		List<String> blackList = new ArrayList<>();
-		blackList.add("changecreate");
-		blackList.add("changehandle");
-		//blackList.add(ProcessStepHandler.EVENT.getHandler());
-		blackList.add(ProcessStepHandlerType.AUTOMATIC.getHandler());
-		List<ProcessTaskStepVo> processTaskStepList = processTaskMapper.getProcessTaskStepListByProcessTaskId(jsonObj.getLong("processTaskId"));
-		for(ProcessTaskStepVo processTaskStepVo : processTaskStepList) {
-			String handler = processTaskStepVo.getHandler().toLowerCase();
-			if(blackList.contains(handler)) {
-				result.put("isfit", false);
-				result.put("msg", String.format("抱歉！移动端暂时不支持查看/处理含有‘%s’步骤节点的工单", ProcessStepHandlerTypeFactory.getName(handler)));
-				break;
-			}
-		}
-		return result;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "processTaskId", type = ApiParamType.LONG, isRequired = true, desc = "工单id")
+    })
+    @Output({
+            @Param(name = "Return", type = ApiParamType.BOOLEAN, desc = "工单是否支持移动端，1：支持，0：不支持;不支持则移动端提示不支持")
+    })
+    @Description(desc = "临时屏蔽移动端工单查看处理接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        JSONObject result = new JSONObject();
+        result.put("isfit", true);
+        List<ProcessTaskStepVo> processTaskStepList = processTaskMapper.getProcessTaskStepListByProcessTaskId(jsonObj.getLong("processTaskId"));
+        for (ProcessTaskStepVo processTaskStepVo : processTaskStepList) {
+            String handler = processTaskStepVo.getHandler().toLowerCase();
+            List<ProcessStepHandlerVo> processStepHandlerVos = ProcessStepHandlerFactory.getActiveProcessStepHandler();
+            if (processStepHandlerVos.stream().noneMatch(o -> Objects.equals(o.getHandler(), handler) && o.getFitMobile())) {
+                result.put("isfit", false);
+                result.put("msg", String.format("抱歉！移动端暂时不支持查看/处理含有‘%s’步骤节点的工单", ProcessStepHandlerTypeFactory.getName(handler)));
+                break;
+            }
+        }
+        return result;
+    }
 
 }
