2582d85c565121035ece32255139f9f30bd63d71
1437892690@qq.com <1437892690@qq.com>
2024-04-24 10:19:33 +0800

    [功能] 后端-自定义表单标准规范定义表结构
    关联 #[1141831629242368]后端-自定义表单标准规范定义表结构 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1141831629242368

--- src/main/resources/neatlogic/resources/process/sqlscript/ddl.sql
+++ src/main/resources/neatlogic/resources/process/sqlscript/ddl.sql
@@ -1415,3 +1415,24 @@ CREATE TABLE IF NOT EXISTS `processtask_urge` (
   `lcd` timestamp(3) NOT NULL COMMENT '催办时间',
   PRIMARY KEY (`processtask_id`,`lcu`,`lcd`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `processtask_extend_formattribute` (
+    `processtask_id` bigint NOT NULL COMMENT '工单id',
+    `form_attribute_data_id` bigint NOT NULL COMMENT '表单属性值id',
+    `tag` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标签',
+    PRIMARY KEY (`processtask_id`,`form_attribute_data_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='工单与表单扩展属性值关系表';
+
+CREATE TABLE IF NOT EXISTS `processtask_form_extend_attribute` (
+    `processtask_id` bigint NOT NULL COMMENT '工单id',
+    `form_uuid` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '表单uuid',
+    `parent_uuid` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '属性父级uuid',
+    `tag` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标签',
+    `key` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '属性key',
+    `uuid` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '属性uuid',
+    `label` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '属性名',
+    `type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '属性类型，系统属性不允许修改',
+    `handler` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '属性处理器',
+    `config_hash` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '属性配置Hash值',
+    PRIMARY KEY (`processtask_id`,`form_uuid`,`tag`,`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='工单表单版本扩展属性';
