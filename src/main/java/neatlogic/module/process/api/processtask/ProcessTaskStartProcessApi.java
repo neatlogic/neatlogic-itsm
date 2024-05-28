4f1bce66c2e37fb4fb003be24fe628ed413bc5b2
1437892690@qq.com <1437892690@qq.com>
2024-05-28 18:59:58 +0800

    [功能] IT服务-工单提交后进入工单详情页时提示“工单未提交”
    关联 #[1166734671708160]IT服务-工单提交后进入工单详情页时提示“工单未提交” http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1166734671708160

--- src/main/java/neatlogic/module/process/api/processtask/ProcessTaskStartProcessApi.java
+++ src/main/java/neatlogic/module/process/api/processtask/ProcessTaskStartProcessApi.java
@@ -1,18 +1,24 @@
 package neatlogic.module.process.api.processtask;
 
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.process.auth.PROCESS_BASE;
-import neatlogic.module.process.service.ProcessTaskService;
+import neatlogic.framework.process.dto.ProcessTaskVo;
 import neatlogic.framework.restful.annotation.Description;
 import neatlogic.framework.restful.annotation.Input;
 import neatlogic.framework.restful.annotation.OperationType;
 import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import com.alibaba.fastjson.JSONObject;
+import neatlogic.module.process.dao.mapper.processtask.ProcessTaskMapper;
+import neatlogic.module.process.service.ProcessTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @OperationType(type = OperationTypeEnum.UPDATE)
@@ -21,6 +27,9 @@ public class ProcessTaskStartProcessApi extends PrivateApiComponentBase {
     
     @Autowired
     private ProcessTaskService processTaskService;
+
+	@Resource
+	private ProcessTaskMapper processTaskMapper;
 
 	@Override
 	public String getToken() {
@@ -76,7 +85,9 @@ public class ProcessTaskStartProcessApi extends PrivateApiComponentBase {
 //            throw new PermissionDeniedException();
 //        }
 		processTaskService.startProcessProcessTask(jsonObj);
-		return null;
+		Long processTaskId = jsonObj.getLong("processTaskId");
+		List<ProcessTaskVo> processTaskList = processTaskMapper.getProcessTaskListByIdList(Collections.singletonList(processTaskId));
+		return processTaskList.get(0);
 	}
 
 }
