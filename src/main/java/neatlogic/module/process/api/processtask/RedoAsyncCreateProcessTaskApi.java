361d8b0b2268c18bdbe0efd1d43c94c9de20f901
1437892690@qq.com <1437892690@qq.com>
2024-08-02 21:50:42 +0800

    [功能] IT服务-增加基于队列的异步上报工单方式
    关联 #[1212437796192256]IT服务-增加基于队列的异步上报工单方式 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1212437796192256

---
+++ src/main/java/neatlogic/module/process/api/processtask/RedoAsyncCreateProcessTaskApi.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.module.process.api.processtask;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.process.auth.PROCESSTASK_MODIFY;
+import neatlogic.framework.process.dto.ProcessTaskAsyncCreateVo;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.module.process.dao.mapper.processtask.ProcessTaskAsyncCreateMapper;
+import neatlogic.module.process.dao.mapper.processtask.ProcessTaskMapper;
+import neatlogic.module.process.service.ProcessTaskAsyncCreateService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@AuthAction(action = PROCESSTASK_MODIFY.class)
+@OperationType(type = OperationTypeEnum.CREATE)
+public class RedoAsyncCreateProcessTaskApi extends PrivateApiComponentBase {
+
+    @Resource
+    private ProcessTaskAsyncCreateMapper processTaskAsyncCreateMapper;
+
+    @Resource
+    private ProcessTaskMapper processTaskMapper;
+
+    @Resource
+    private ProcessTaskAsyncCreateService processTaskAsyncCreateService;
+
+    @Override
+    public String getName() {
+        return "nmpap.redoasynccreateprocesstaskapi.getname";
+    }
+
+    @Input({
+            @Param(name = "id", type = ApiParamType.LONG, desc = "id"),
+            @Param(name = "serverId", type = ApiParamType.INTEGER, desc = "term.framework.serverid"),
+    })
+    @Output({
+            @Param(name = "processTaskIdList", type = ApiParamType.LONG, desc = "term.itsm.processtaskidlist")
+    })
+    @Description(desc = "nmpap.redoasynccreateprocesstaskapi.getname")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        List<Long> processTaskIdArray = new ArrayList<>();
+        Long id = paramObj.getLong("id");
+        Integer serverId = paramObj.getInteger("serverId");
+        if (id != null) {
+            ProcessTaskAsyncCreateVo processTaskAsyncCreateVo = processTaskAsyncCreateMapper.getProcessTaskAsyncCreateById(id);
+            if (processTaskAsyncCreateVo != null) {
+                processTaskIdArray.add(processTaskAsyncCreateVo.getProcessTaskId());
+                processTaskAsyncCreateService.addRedoProcessTaskAsyncCreate(processTaskAsyncCreateVo);
+            }
+
+        } else if (serverId != null) {
+            ProcessTaskAsyncCreateVo searchVo = new ProcessTaskAsyncCreateVo();
+            searchVo.setStatus("redo");
+            searchVo.setServerId(serverId);
+            int rowNum = processTaskAsyncCreateMapper.getProcessTaskAsyncCreateCount(searchVo);
+            if (rowNum > 0) {
+                searchVo.setRowNum(rowNum);
+                searchVo.setPageSize(100);
+                Integer pageCount = searchVo.getPageCount();
+                List<ProcessTaskAsyncCreateVo> doneList = new ArrayList<>();
+                for (int currentPage = 1; currentPage <= pageCount; currentPage++) {
+                    searchVo.setCurrentPage(currentPage);
+                    List<ProcessTaskAsyncCreateVo> list = processTaskAsyncCreateMapper.getProcessTaskAsyncCreateList(searchVo);
+                    List<Long> processTaskIdList = list.stream().map(ProcessTaskAsyncCreateVo::getProcessTaskId).filter(Objects::nonNull).collect(Collectors.toList());
+                    if (CollectionUtils.isNotEmpty(processTaskIdList)) {
+                        processTaskIdList = processTaskMapper.checkProcessTaskIdListIsExists(processTaskIdList);
+                    }
+                    for (ProcessTaskAsyncCreateVo processTaskAsyncCreateVo : list) {
+                        if (processTaskIdList.contains(processTaskAsyncCreateVo.getProcessTaskId())) {
+                            processTaskAsyncCreateVo.setStatus("done");
+                            doneList.add(processTaskAsyncCreateVo);
+                        } else {
+                            processTaskIdList.add(processTaskAsyncCreateVo.getProcessTaskId());
+                            processTaskAsyncCreateService.addRedoProcessTaskAsyncCreate(processTaskAsyncCreateVo);
+                        }
+
+                    }
+                }
+                for (ProcessTaskAsyncCreateVo processTaskAsyncCreateVo : doneList) {
+                    processTaskAsyncCreateMapper.updateProcessTaskAsyncCreate(processTaskAsyncCreateVo);
+                }
+            }
+        }
+        JSONObject resultObj = new JSONObject();
+        resultObj.put("processTaskIdList", processTaskIdArray);
+        return resultObj;
+    }
+
+    @Override
+    public String getToken() {
+        return "processtask/asynccreate/redo";
+    }
+}
---
+++ src/main/java/neatlogic/module/process/api/processtask/test/TestAsyncCreateProcessTaskApi.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.module.process.api.processtask.test;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.process.auth.PROCESSTASK_MODIFY;
+import neatlogic.framework.process.dto.ProcessTaskAsyncCreateVo;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.framework.util.SnowflakeUtil;
+import neatlogic.module.process.service.ProcessTaskAsyncCreateService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AuthAction(action = PROCESSTASK_MODIFY.class)
+@OperationType(type = OperationTypeEnum.CREATE)
+public class TestAsyncCreateProcessTaskApi extends PrivateApiComponentBase {
+
+    @Resource
+    private ProcessTaskAsyncCreateService processTaskAsyncCreateService;
+
+    @Override
+    public String getName() {
+        return "nmpapt.testasynccreateprocesstaskapi.getname";
+    }
+
+    @Input({
+            @Param(name = "channel", type = ApiParamType.STRING, isRequired = true, desc = "term.itsm.channel", help = "支持channelUuid和channelName入参"),
+            @Param(name = "title", type = ApiParamType.STRING, isRequired = true, maxLength = 80, desc = "common.title"),
+            @Param(name = "owner", type = ApiParamType.STRING, isRequired = true, desc = "term.itsm.owner", help = "上报人uuid和上报人id入参"),
+            @Param(name = "reporter", type = ApiParamType.STRING, desc = "term.itsm.reporter"),
+            @Param(name = "priority", type = ApiParamType.STRING, isRequired = true, desc = "common.priority"),
+            @Param(name = "formAttributeDataList", type = ApiParamType.JSONARRAY, desc = "term.itsm.formattributedatalist"),
+            @Param(name = "hidecomponentList", type = ApiParamType.JSONARRAY, desc = "term.itsm.hidecomponentlist"),
+            @Param(name = "readcomponentList", type = ApiParamType.JSONARRAY, desc = "term.itsm.readcomponentlist"),
+            @Param(name = "content", type = ApiParamType.STRING, desc = "common.content"),
+            @Param(name = "filePathPrefix", type = ApiParamType.STRING, defaultValue = "file:", desc = "common.filepathprefix"),
+            @Param(name = "filePathList", type = ApiParamType.JSONARRAY, desc = "common.filepathlist"),
+            @Param(name = "fileIdList", type = ApiParamType.JSONARRAY, desc = "common.fileidlist"),
+            @Param(name = "handlerStepInfo", type = ApiParamType.JSONOBJECT, desc = "term.itsm.handlerstepinfo"),
+            @Param(name = "source", type = ApiParamType.STRING, desc = "common.source"),
+//            @Param(name = "newProcessTaskId", type = ApiParamType.LONG, desc = "指定工单id，则会使用该id作为工单id"),
+            @Param(name = "region", type = ApiParamType.STRING, desc = "common.region", help = "全路径or地域id"),
+            @Param(name = "count", type = ApiParamType.INTEGER, isRequired = true, desc = "common.count")
+    })
+    @Output({
+            @Param(name = "processTaskIdList", type = ApiParamType.LONG, desc = "term.itsm.processtaskidlist")
+    })
+    @Description(desc = "nmpapt.testasynccreateprocesstaskapi.getname")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        System.out.println("start = ");
+        Integer count = paramObj.getInteger("count");
+        String title = paramObj.getString("title");
+        String jsonString = paramObj.toJSONString();
+        List<Long> processTaskIdList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            JSONObject config = JSONObject.parseObject(jsonString);
+            Long newProcessTaskId = SnowflakeUtil.uniqueLong();
+            config.put("newProcessTaskId", newProcessTaskId);
+            config.put("title", title + "-" + i);
+            processTaskAsyncCreateService.addNewProcessTaskAsyncCreate(new ProcessTaskAsyncCreateVo(config));
+            processTaskIdList.add(newProcessTaskId);
+        }
+        System.out.println("end = ");
+        JSONObject resultObj = new JSONObject();
+        resultObj.put("processTaskIdList", processTaskIdList);
+        return resultObj;
+    }
+
+    @Override
+    public String getToken() {
+        return "processtask/asynccreate/test";
+    }
+}
