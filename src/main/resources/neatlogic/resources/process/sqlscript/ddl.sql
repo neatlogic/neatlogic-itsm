d87fbccf7166d5bb4600faaa47fba58cf7632e01
1437892690@qq.com <1437892690@qq.com>
2024-08-02 21:38:30 +0800

    [功能] IT服务-增加基于队列的异步上报工单方式
    关联 #[1212437796192256]IT服务-增加基于队列的异步上报工单方式 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1212437796192256

--- src/main/resources/neatlogic/resources/process/sqlscript/ddl.sql
+++ src/main/resources/neatlogic/resources/process/sqlscript/ddl.sql
@@ -654,6 +654,24 @@ CREATE TABLE IF NOT EXISTS `processtask_auto_score`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '工单自动评分表';
 
 -- ----------------------------
+-- Table structure for processtask_async_create
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `processtask_async_create` (
+  `id` bigint NOT NULL COMMENT '主键ID',
+  `processtask_id` bigint DEFAULT NULL COMMENT '工单ID',
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
+  `status` enum('doing','done','failed','aborted','redo') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '状态',
+  `config` longtext COLLATE utf8mb4_general_ci NOT NULL COMMENT '配置信息',
+  `error` longtext COLLATE utf8mb4_general_ci COMMENT '异常信息',
+  `try_count` int NOT NULL DEFAULT '0' COMMENT '尝试次数',
+  `server_id` int NOT NULL COMMENT '服务器ID',
+  `fcu` char(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT '创建人',
+  `fcd` timestamp(3) NOT NULL COMMENT '创建时间',
+  `lcd` timestamp(3) NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT = '异步创建工单表';
+
+-- ----------------------------
 -- Table structure for processtask_config
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `processtask_config`  (
