dfd4a4020a242a91bfb3f7b4375ac609cf66cdc8
1437892690@qq.com <1437892690@qq.com>
2024-08-16 17:17:08 +0800

    [功能] 后端-指派前置步骤不能选没有处理人的步骤指定
    关联 #[1211578056146944]通知策略梳理每个触发点的收件人，没用的收件人去掉，如激活的处理人 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1211578056146944

--- src/main/java/neatlogic/module/process/notify/handler/AutomaticNotifyPolicyHandler.java
+++ src/main/java/neatlogic/module/process/notify/handler/AutomaticNotifyPolicyHandler.java
@@ -1,22 +1,15 @@
 package neatlogic.module.process.notify.handler;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import neatlogic.framework.common.constvalue.Expression;
-import neatlogic.framework.common.constvalue.ParamType;
-import neatlogic.framework.condition.core.IConditionHandler;
 import neatlogic.framework.dto.ConditionParamVo;
-import neatlogic.framework.dto.ExpressionVo;
-import neatlogic.framework.form.constvalue.FormConditionModel;
-import neatlogic.framework.notify.core.NotifyPolicyHandlerBase;
 import neatlogic.framework.notify.dto.NotifyTriggerVo;
 import neatlogic.framework.process.auth.PROCESS_MODIFY;
-import neatlogic.framework.process.condition.core.ProcessTaskConditionFactory;
-import neatlogic.framework.process.constvalue.ConditionProcessTaskOptions;
 import neatlogic.framework.process.constvalue.ProcessStepHandlerType;
 import neatlogic.framework.process.constvalue.ProcessTaskGroupSearch;
 import neatlogic.framework.process.constvalue.ProcessUserType;
-import neatlogic.framework.process.notify.constvalue.*;
+import neatlogic.framework.process.notify.constvalue.ProcessTaskStepAutomaticNotifyParam;
+import neatlogic.framework.process.notify.constvalue.ProcessTaskStepAutomaticNotifyTriggerType;
+import neatlogic.framework.process.notify.core.ProcessTaskNotifyHandlerBase;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -31,7 +24,7 @@ import java.util.List;
 
  **/
 @Component
-public class AutomaticNotifyPolicyHandler extends NotifyPolicyHandlerBase {
+public class AutomaticNotifyPolicyHandler extends ProcessTaskNotifyHandlerBase {
     @Override
     public String getName() {
         return ProcessStepHandlerType.AUTOMATIC.getName();
@@ -46,11 +39,8 @@ public class AutomaticNotifyPolicyHandler extends NotifyPolicyHandlerBase {
     }
 
     @Override
-    protected List<NotifyTriggerVo> myNotifyTriggerList() {
+    protected List<NotifyTriggerVo> myCustomNotifyTriggerList() {
         List<NotifyTriggerVo> returnList = new ArrayList<>();
-        for (ProcessTaskStepNotifyTriggerType notifyTriggerType : ProcessTaskStepNotifyTriggerType.values()) {
-            returnList.add(new NotifyTriggerVo(notifyTriggerType));
-        }
         for (ProcessTaskStepAutomaticNotifyTriggerType notifyTriggerType : ProcessTaskStepAutomaticNotifyTriggerType.values()) {
             returnList.add(new NotifyTriggerVo(notifyTriggerType));
         }
@@ -58,14 +48,8 @@ public class AutomaticNotifyPolicyHandler extends NotifyPolicyHandlerBase {
     }
 
     @Override
-    protected List<ConditionParamVo> mySystemParamList() {
+    protected List<ConditionParamVo> myCustomSystemParamList() {
         List<ConditionParamVo> notifyPolicyParamList = new ArrayList<>();
-        for(ProcessTaskNotifyParam param : ProcessTaskNotifyParam.values()) {
-            notifyPolicyParamList.add(createConditionParam(param));
-        }
-        for(ProcessTaskStepNotifyParam param : ProcessTaskStepNotifyParam.values()) {
-            notifyPolicyParamList.add(createConditionParam(param));
-        }
         for (ProcessTaskStepAutomaticNotifyParam param : ProcessTaskStepAutomaticNotifyParam.values()) {
             notifyPolicyParamList.add(createConditionParam(param));
         }
@@ -73,44 +57,9 @@ public class AutomaticNotifyPolicyHandler extends NotifyPolicyHandlerBase {
     }
 
     @Override
-    protected List<ConditionParamVo> mySystemConditionOptionList() {
-        List<ConditionParamVo> notifyPolicyParamList = new ArrayList<>();
-        for(ConditionProcessTaskOptions option : ConditionProcessTaskOptions.values()) {
-            IConditionHandler condition = ProcessTaskConditionFactory.getHandler(option.getValue());
-            if(condition != null) {
-                ConditionParamVo param = new ConditionParamVo();
-                param.setName(condition.getName());
-                param.setLabel(condition.getDisplayName());
-                param.setController(condition.getHandler(FormConditionModel.CUSTOM));
-                if(condition.getConfig() != null) {
-                    param.setConfig(condition.getConfig().toJSONString());
-                }
-                param.setType(condition.getType());
-                ParamType paramType = condition.getParamType();
-                if(paramType != null) {
-                    param.setParamType(paramType.getName());
-                    param.setParamTypeName(paramType.getText());
-                    param.setDefaultExpression(paramType.getDefaultExpression().getExpression());
-                    for(Expression expression : paramType.getExpressionList()) {
-                        param.getExpressionList().add(new ExpressionVo(expression.getExpression(), expression.getExpressionName(),expression.getIsShowConditionValue()));
-                    }
-                }
-
-                param.setIsEditable(0);
-                notifyPolicyParamList.add(param);
-            }
-        }
-        return notifyPolicyParamList;
-    }
-
-    @Override
-    protected void myAuthorityConfig(JSONObject config) {
-        List<String> groupList = JSON.parseArray(config.getJSONArray("groupList").toJSONString(), String.class);
-        groupList.add(ProcessTaskGroupSearch.PROCESSUSERTYPE.getValue());
-        config.put("groupList", groupList);
-        List<String> includeList = JSON.parseArray(config.getJSONArray("includeList").toJSONString(), String.class);
-        includeList.add(ProcessTaskGroupSearch.PROCESSUSERTYPE.getValue() + "#" + ProcessUserType.DEFAULT_WORKER.getValue());
-        includeList.add(ProcessTaskGroupSearch.PROCESSUSERTYPE.getValue() + "#" + ProcessUserType.FOCUS_USER.getValue());
-        config.put("includeList", includeList);
+    protected void myCustomAuthorityConfig(JSONObject config) {
+        List<String> excludeList = config.getJSONArray("excludeList").toJavaList(String.class);
+        excludeList.add(ProcessTaskGroupSearch.PROCESSUSERTYPE.getValue() + "#" + ProcessUserType.MINOR.getValue());
+        config.put("excludeList", excludeList);
     }
 }
