bd217b95d43c0c4a325a8db7a6add35b85cf86c5
1437892690@qq.com <1437892690@qq.com>
2024-12-01 17:36:17 +0800

    [功能] 后端-IT服务-流程编辑页支持查看工单备份的流程图信息
    关联 #[1302178805809152]后端-IT服务-流程编辑页支持查看工单备份的流程图信息 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1302178805809152

---
+++ src/main/java/neatlogic/module/process/api/process/ListProcessFormTagApi.java
@@ -0,0 +1,109 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.module.process.api.process;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.form.dao.mapper.FormMapper;
+import neatlogic.framework.form.dto.FormAttributeVo;
+import neatlogic.framework.form.dto.FormVersionVo;
+import neatlogic.framework.form.dto.FormVo;
+import neatlogic.framework.form.exception.FormActiveVersionNotFoundExcepiton;
+import neatlogic.framework.form.exception.FormNotFoundException;
+import neatlogic.framework.process.dto.ProcessTaskFormVo;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.framework.util.TableResultUtil;
+import neatlogic.module.process.dao.mapper.processtask.ProcessTaskMapper;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class ListProcessFormTagApi extends PrivateApiComponentBase {
+
+    @Resource
+    private FormMapper formMapper;
+
+    @Resource
+    private ProcessTaskMapper processTaskMapper;
+
+    @Override
+    public String getToken() {
+        return "process/form/tag/list";
+    }
+
+    @Override
+    public String getName() {
+        return "nmtaf.listformtagapi.getname";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "processTaskId", type = ApiParamType.STRING, desc = "term.itsm.processtaskid"),
+            @Param(name = "formUuid", type = ApiParamType.STRING, desc = "term.framework.formuuid")
+    })
+    @Output({
+            @Param(name = "tbodyList", explode = String[].class, desc = "common.tbodylist")
+    })
+    @Description(desc = "nmtaf.listformtagapi.getname")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        FormVersionVo formVersionVo = null;
+        Long processTaskId = jsonObj.getLong("processTaskId");
+        if (processTaskId != null) {
+            ProcessTaskFormVo processTaskForm = processTaskMapper.getProcessTaskFormByProcessTaskId(processTaskId);
+            if (processTaskForm != null) {
+                formVersionVo = new FormVersionVo();
+                formVersionVo.setFormUuid(processTaskForm.getFormUuid());
+                formVersionVo.setFormName(processTaskForm.getFormName());
+                formVersionVo.setFormConfigStr(processTaskForm.getFormContent());
+            }
+        } else {
+            String formUuid = jsonObj.getString("formUuid");
+            FormVo form = formMapper.getFormByUuid(formUuid);
+            if (form == null) {
+                throw new FormNotFoundException(formUuid);
+            }
+            formVersionVo = formMapper.getActionFormVersionByFormUuid(formUuid);
+            if (formVersionVo == null) {
+                throw new FormActiveVersionNotFoundExcepiton(form.getName());
+            }
+        }
+        Set<String> tagSet = new HashSet<>();
+        if (formVersionVo != null) {
+            List<FormAttributeVo> formCustomExtendAttributeList = formVersionVo.getFormCustomExtendAttributeList();
+            if (CollectionUtils.isNotEmpty(formCustomExtendAttributeList)) {
+                for (FormAttributeVo formAttributeVo : formCustomExtendAttributeList) {
+                    tagSet.add(formAttributeVo.getTag());
+                }
+            }
+        }
+        return TableResultUtil.getResult(new ArrayList<>(tagSet));
+    }
+
+}
--- src/main/java/neatlogic/module/process/api/process/ProcessGetApi.java
+++ src/main/java/neatlogic/module/process/api/process/ProcessGetApi.java
@@ -1,21 +1,22 @@
 package neatlogic.module.process.api.process;
 
-import neatlogic.framework.process.stephandler.core.ProcessMessageManager;
-import neatlogic.framework.process.util.ProcessConfigUtil;
-import neatlogic.framework.restful.constvalue.OperationTypeEnum;
-import neatlogic.framework.restful.annotation.*;
-import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import neatlogic.framework.process.auth.PROCESS_MODIFY;
-
-import neatlogic.module.process.dao.mapper.process.ProcessMapper;
-import org.springframework.stereotype.Service;
-
 import com.alibaba.fastjson.JSONObject;
-
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.process.auth.PROCESS_MODIFY;
+import neatlogic.framework.process.dto.ProcessTaskVo;
 import neatlogic.framework.process.dto.ProcessVo;
 import neatlogic.framework.process.exception.process.ProcessNotFoundEditTargetException;
+import neatlogic.framework.process.stephandler.core.ProcessMessageManager;
+import neatlogic.framework.process.util.ProcessConfigUtil;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.module.process.dao.mapper.SelectContentByHashMapper;
+import neatlogic.module.process.dao.mapper.process.ProcessMapper;
+import neatlogic.module.process.dao.mapper.processtask.ProcessTaskMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
@@ -26,6 +27,12 @@ public class ProcessGetApi extends PrivateApiComponentBase {
 
     @Resource
     private ProcessMapper processMapper;
+
+    @Resource
+    private ProcessTaskMapper processTaskMapper;
+
+    @Resource
+    private SelectContentByHashMapper selectContentByHashMapper;
 
     @Override
     public String getToken() {
@@ -43,7 +50,8 @@ public class ProcessGetApi extends PrivateApiComponentBase {
     }
 
     @Input({
-			@Param(name = "uuid", type = ApiParamType.STRING, isRequired = true, desc = "term.itsm.processuuid")
+			@Param(name = "uuid", type = ApiParamType.STRING, desc = "term.itsm.processuuid"),
+            @Param(name = "processTaskId", type = ApiParamType.LONG, desc = "term.itsm.processtaskid")
     })
     @Output({
 			@Param(explode = ProcessVo.class)
@@ -51,15 +59,39 @@ public class ProcessGetApi extends PrivateApiComponentBase {
     @Description(desc = "nmpap.processgetapi.getname")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        String uuid = jsonObj.getString("uuid");
-        ProcessVo processVo = processMapper.getProcessByUuid(uuid);
-        if (processVo == null) {
-            throw new ProcessNotFoundEditTargetException(uuid);
+        Long processTaskId = jsonObj.getLong("processTaskId");
+        if (processTaskId != null) {
+            ProcessTaskVo processTaskVo = processTaskMapper.getProcessTaskBaseInfoByIdIncludeIsDeleted(processTaskId);
+            if (processTaskVo != null) {
+                String configStr = selectContentByHashMapper.getProcessTaskConfigStringByHash(processTaskVo.getConfigHash());
+                ProcessVo processVo = processMapper.getProcessByUuid(processTaskVo.getProcessUuid());
+                if (processVo == null) {
+                    processVo = new ProcessVo();
+                    processVo.setUuid(processTaskVo.getProcessUuid());
+                    processVo.setName("");
+                    processVo.setIsActive(1);
+                }
+                processVo.setName(processVo.getName() + "【工单：" + processTaskVo.getTitle()+ "】");
+                processVo.setConfig(configStr);
+                ProcessMessageManager.setOperationType(OperationTypeEnum.SEARCH);
+                processVo.setConfig(ProcessConfigUtil.regulateProcessConfig(processVo.getConfig()));
+                processVo.setReferenceCount(1);
+                return processVo;
+            }
+        } else {
+            String uuid = jsonObj.getString("uuid");
+            if (StringUtils.isNotBlank(uuid)) {
+                ProcessVo processVo = processMapper.getProcessByUuid(uuid);
+                if (processVo == null) {
+                    throw new ProcessNotFoundEditTargetException(uuid);
+                }
+                ProcessMessageManager.setOperationType(OperationTypeEnum.SEARCH);
+                processVo.setConfig(ProcessConfigUtil.regulateProcessConfig(processVo.getConfig()));
+                int count = processMapper.getProcessReferenceCount(uuid);
+                processVo.setReferenceCount(count);
+                return processVo;
+            }
         }
-        ProcessMessageManager.setOperationType(OperationTypeEnum.SEARCH);
-        processVo.setConfig(ProcessConfigUtil.regulateProcessConfig(processVo.getConfig()));
-        int count = processMapper.getProcessReferenceCount(uuid);
-        processVo.setReferenceCount(count);
-        return processVo;
+        return null;
     }
 }
