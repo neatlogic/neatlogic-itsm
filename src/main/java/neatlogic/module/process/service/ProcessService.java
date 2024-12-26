ed36e8bd0c634c90cc8d93825d1d03d255d0eaad
1437892690@qq.com <1437892690@qq.com>
2024-12-26 10:29:00 +0800

    [功能] IT服务-流程图保存逻辑优化
    关联 #[1320124261302272]IT服务-流程图保存逻辑优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1320124261302272

--- src/main/java/neatlogic/module/process/service/ProcessService.java
+++ src/main/java/neatlogic/module/process/service/ProcessService.java
@@ -1,5 +1,6 @@
 package neatlogic.module.process.service;
 
+import neatlogic.framework.process.dto.ProcessStepVo;
 import neatlogic.framework.process.dto.ProcessVo;
 import neatlogic.framework.process.exception.process.ProcessNameRepeatException;
 
@@ -12,4 +13,18 @@ public interface ProcessService {
 	 * @param uuid 流程uuid
 	 */
 	void deleteProcessRelevantData(String uuid);
+
+	/**
+	 * 保存或删除流程的依赖关系数据
+	 * @param processVo 流程信息
+	 * @param action 保存或删除
+	 */
+	void saveOrDeleteProcessDependency(ProcessVo processVo, String action);
+
+	/**
+	 * 保存或删除流程步骤的依赖关系数据
+	 * @param processStepVo 流程步骤信息
+	 * @param action 保存或删除
+	 */
+	void saveOrDeleteProcessStepDependency(ProcessStepVo processStepVo, String action);
 }
