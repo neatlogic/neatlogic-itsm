ac183b14d332b089514ef2d49c5ba03f0cd01948
chenqw <gdnpc@163.com>
2025-02-15 19:58:15 +0800

    adjust
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/process/api/processtask/ProcessTaskStartProcessApi.java
+++ src/main/java/neatlogic/module/process/api/processtask/ProcessTaskStartProcessApi.java
@@ -10,9 +10,7 @@ import neatlogic.framework.restful.annotation.OperationType;
 import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import neatlogic.module.process.dao.mapper.processtask.ProcessTaskMapper;
 import neatlogic.module.process.service.ProcessTaskService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -21,37 +19,35 @@ import javax.annotation.Resource;
 @OperationType(type = OperationTypeEnum.UPDATE)
 @AuthAction(action = PROCESS_BASE.class)
 public class ProcessTaskStartProcessApi extends PrivateApiComponentBase {
-    
-    @Autowired
+
+    @Resource
     private ProcessTaskService processTaskService;
 
-	@Resource
-	private ProcessTaskMapper processTaskMapper;
 
-	@Override
-	public String getToken() {
-		return "processtask/startprocess";
-	}
+    @Override
+    public String getToken() {
+        return "processtask/startprocess";
+    }
 
-	@Override
-	public String getName() {
-		return "工单上报提交接口";
-	}
+    @Override
+    public String getName() {
+        return "工单上报提交接口";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	@Input({
-		@Param(name = "processTaskId", type = ApiParamType.LONG, isRequired = true, desc = "工单Id"),
-		@Param(name = "nextStepId", type = ApiParamType.LONG, desc = "激活下一步骤Id（如果有且仅有一个下一节点，则可以不传这个参数）"),
-		@Param(name = "source", type = ApiParamType.STRING, defaultValue = "pc", desc = "来源"),
-		@Param(name = "assignWorkerList", type = ApiParamType.JSONARRAY, desc = "分配步骤处理人信息列表，格式[{\"processTaskStepId\":1, \"processStepUuid\":\"abc\", \"workerList\":[\"user#xxx\",\"team#xxx\",\"role#xxx\"]}]")
-	})
-	@Description(desc = "工单上报提交接口")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
+    @Input({
+            @Param(name = "processTaskId", type = ApiParamType.LONG, isRequired = true, desc = "工单Id"),
+            @Param(name = "nextStepId", type = ApiParamType.LONG, desc = "激活下一步骤Id（如果有且仅有一个下一节点，则可以不传这个参数）"),
+            @Param(name = "source", type = ApiParamType.STRING, defaultValue = "pc", desc = "来源"),
+            @Param(name = "assignWorkerList", type = ApiParamType.JSONARRAY, desc = "分配步骤处理人信息列表，格式[{\"processTaskStepId\":1, \"processStepUuid\":\"abc\", \"workerList\":[\"user#xxx\",\"team#xxx\",\"role#xxx\"]}]")
+    })
+    @Description(desc = "工单上报提交接口")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
 //		Long processTaskId = jsonObj.getLong("processTaskId");
 //        Long nextStepId = jsonObj.getLong("nextStepId");
 //        processTaskService.checkProcessTaskParamsIsLegal(processTaskId, null, nextStepId);
@@ -81,8 +77,8 @@ public class ProcessTaskStartProcessApi extends PrivateApiComponentBase {
 //        }catch(ProcessTaskNoPermissionException e) {
 //            throw new PermissionDeniedException();
 //        }
-		processTaskService.startProcessProcessTask(jsonObj);
-		return null;
-	}
+        processTaskService.startProcessProcessTask(jsonObj);
+        return null;
+    }
 
 }
