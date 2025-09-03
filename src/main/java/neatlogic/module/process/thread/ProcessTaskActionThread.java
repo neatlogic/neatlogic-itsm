de50eff8d06ff301b5471529dda93d7cf4c1e181
1437892690@qq.com <1437892690@qq.com>
2025-09-03 12:14:14 +0800

    [修复] 流程图某个步骤配置多个相同触发点的动作时，只有第一个动作有效，后面动作不生效
    关联 #[1502103384588288]流程图某个步骤配置多个相同触发点的动作时，只有第一个动作有效，后面动作不生效 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1502103384588288

--- src/main/java/neatlogic/module/process/thread/ProcessTaskActionThread.java
+++ src/main/java/neatlogic/module/process/thread/ProcessTaskActionThread.java
@@ -247,7 +247,7 @@ public class ProcessTaskActionThread extends NeatLogicThread {
                         processTaskActionMapper.insertProcessTaskAction(actionVo);
                         currentProcessTaskStepVo.getParamObj().put(ProcessTaskAuditDetailType.RESTFULACTION.getParamName(), JSON.toJSONString(actionVo));
                         ProcessTaskAuditThread.audit(currentProcessTaskStepVo, ProcessTaskAuditType.RESTFULACTION);
-                        break;
+//                        break;
                     }
                 }
             }
