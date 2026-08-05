6de261c84347f0e263f6a4b7e1f09168f9fd2aa4
1437892690@qq.com <1437892690@qq.com>
2026-08-05 18:06:34 +0800

    [功能] 后端-门户管理接口
    关联 #[1734939029274631]后端-门户管理接口 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1734939029274631
    .html#/bug-detail/939050947543040/939050947543057/1744349386506246

--- src/main/java/neatlogic/module/process/portal/widget/ProcessPortalWidget.java
+++ src/main/java/neatlogic/module/process/portal/widget/ProcessPortalWidget.java
@@ -11,23 +11,29 @@
 package neatlogic.module.process.portal.widget;
 
 import neatlogic.framework.portal.widget.core.IPortalWidget;
+import neatlogic.framework.portal.widgetdata.core.IPortalWidgetDataHandler;
+import neatlogic.module.process.portal.widget.handler.*;
+
+import java.util.List;
 
 public enum ProcessPortalWidget implements IPortalWidget {
-    processingOfMineProcessTask("processingOfMineProcessTask", "我的待办", 1),
-    draftProcessTask("draftProcessTask", "我的草稿", 2),
-    processTaskSearch("processTaskSearch", "工单列表", 3),
-    processFavoriteService("processFavoriteService", "收藏服务", 4),
-    personalProcessTaskOverview("personalProcessTaskOverview", "个人工单状态概览", 5),
+    processingOfMineProcessTask("processingOfMineProcessTask", "我的待办", 1, List.of(ProcessingOfMineProcessTaskPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
+    draftProcessTask("draftProcessTask", "我的草稿", 2, List.of(DraftProcessTaskPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
+    processTaskSearch("processTaskSearch", "工单列表", 3, List.of(ProcessTaskSearchPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
+    processFavoriteService("processFavoriteService", "收藏服务", 4, List.of(FavoritedServiceListPortalWidgetDataHandler.class)),
+    personalProcessTaskOverview("personalProcessTaskOverview", "个人工单状态概览", 5, List.of(PersonalProcessTaskOverviewPortalWidgetDataHandler.class)),
     ;
 
     private final String value;
     private final String text;
     private final Integer sort;
+    private final List<Class<? extends IPortalWidgetDataHandler>> portalWidgetDataHandlerClassList;
 
-    ProcessPortalWidget(String value, String text, Integer sort) {
+    ProcessPortalWidget(String value, String text, Integer sort, List<Class<? extends IPortalWidgetDataHandler>> portalWidgetDataHandlerClassList) {
         this.value = value;
         this.text = text;
         this.sort = sort;
+        this.portalWidgetDataHandlerClassList = portalWidgetDataHandlerClassList;
     }
 
     @Override
@@ -43,5 +49,10 @@ public enum ProcessPortalWidget implements IPortalWidget {
     @Override
     public Integer getSort() {
         return this.sort;
+    }
+
+    @Override
+    public List<Class<? extends IPortalWidgetDataHandler>> getPortalWidgetDataHandlerClassList() {
+        return this.portalWidgetDataHandlerClassList;
     }
 }
