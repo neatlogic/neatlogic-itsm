053c87b36998aad7f408803e45f507a6a6ceefd5
1437892690@qq.com <1437892690@qq.com>
2026-08-05 12:10:34 +0800

    [功能] 后端-门户管理接口
    关联 #[1734939029274631]后端-门户管理接口 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1734939029274631
    .html#/bug-detail/939050947543040/939050947543057/1744349386506246

--- src/main/java/neatlogic/module/process/portal/widget/handler/PersonalProcessTaskOverviewPortalWidgetDataHandler.java
+++ src/main/java/neatlogic/module/process/portal/widget/handler/PersonalProcessTaskOverviewPortalWidgetDataHandler.java
@@ -11,11 +11,33 @@
 package neatlogic.module.process.portal.widget.handler;
 
 import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.asynchronization.threadlocal.UserContext;
+import neatlogic.framework.dto.AuthenticationInfoVo;
 import neatlogic.framework.portal.widgetdata.core.PortalWidgetDataHandlerBase;
+import neatlogic.framework.process.constvalue.ProcessTaskStatus;
+import neatlogic.framework.process.constvalue.ProcessTaskStepStatus;
+import neatlogic.framework.process.dto.ProcessTaskSlaTimeVo;
+import neatlogic.framework.process.dto.ProcessTaskStepVo;
+import neatlogic.framework.process.dto.ProcessTaskVo;
+import neatlogic.framework.util.TimeUtil;
+import neatlogic.module.process.dao.mapper.processtask.ProcessTaskMapper;
+import neatlogic.module.process.service.ProcessTaskService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class PersonalProcessTaskOverviewPortalWidgetDataHandler extends PortalWidgetDataHandlerBase {
+
+    @Resource
+    private ProcessTaskMapper processTaskMapper;
+
+    @Resource
+    private ProcessTaskService processTaskService;
 
     @Override
     public String getHandler() {
@@ -24,19 +46,113 @@ public class PersonalProcessTaskOverviewPortalWidgetDataHandler extends PortalWi
 
     @Override
     protected JSONObject getMyData(JSONObject paramObj) {
-        Integer timeRange = paramObj.getInteger("timeRange");
-        String timeUnit = paramObj.getString("timeUnit");
+        Date startTime = paramObj.getDate("startTime");
+        Date endTime = paramObj.getDate("endTime");
+        if (startTime == null && endTime == null) {
+            Integer timeRange = paramObj.getInteger("timeRange");
+            String timeUnit = paramObj.getString("timeUnit");
+            if (timeRange != null && StringUtils.isNotBlank(timeUnit)) {
+                startTime = TimeUtil.recentTimeTransfer(timeRange, timeUnit);
+                endTime = new Date();
+            }
+        }
+        int myTaskCount = 0;
+        int todoCount = 0;
+        int doingCount = 0;
+        int riskCount = 0;
+        int doneCount = 0;
+        {
+            AuthenticationInfoVo authenticationInfoVo = UserContext.get().getAuthenticationInfoVo();
+//            long startTimeA = System.currentTimeMillis();
+            List<Long> processTaskStepIdList = processTaskMapper.getProcessTaskWorkerProcessTaskStepIdListByAuthenticationInfoVo(authenticationInfoVo);
+//            System.out.println("costTimeA = " + (System.currentTimeMillis() - startTimeA));
+//            System.out.println("processTaskStepIdList.size() = " + processTaskStepIdList.size());
+            if (CollectionUtils.isNotEmpty(processTaskStepIdList)) {
+                int count = 0;
+                long startTimeB = System.currentTimeMillis();
+                List<ProcessTaskStepVo> processTaskStepList = processTaskMapper.getProcessTaskStepListByIdList(processTaskStepIdList);
+//                System.out.println("costTimeB = " + (System.currentTimeMillis() - startTimeB));
+//                System.out.println("processTaskStepList.size() = " + processTaskStepList.size());
+                Set<Long> processTaskIdSet = processTaskStepList.stream().map(ProcessTaskStepVo::getProcessTaskId).collect(Collectors.toSet());
+//                long startTimeC = System.currentTimeMillis();
+                List<ProcessTaskVo> processTaskList = processTaskMapper.getProcessTaskListByIdList(new ArrayList<>(processTaskIdSet));
+//                System.out.println("costTimeC = " + (System.currentTimeMillis() - startTimeC));
+                Map<Long, ProcessTaskVo> processTaskMap = processTaskList.stream().collect(Collectors.toMap(ProcessTaskVo::getId, e -> e));
+                for (ProcessTaskStepVo processTaskStepVo : processTaskStepList) {
+                    ProcessTaskVo processTaskVo = processTaskMap.get(processTaskStepVo.getProcessTaskId());
+                    if (processTaskVo != null && Objects.equals(processTaskVo.getIsDeleted(), 0) && Objects.equals(processTaskVo.getStatus(), ProcessTaskStatus.RUNNING.getValue())) {
+                        if (Objects.equals(processTaskStepVo.getIsActive(), 1)) {
+                            myTaskCount++;
+                            if (Objects.equals(processTaskStepVo.getStatus(), ProcessTaskStepStatus.PENDING.getValue())) {
+                                todoCount++;
+                            } else if (Objects.equals(processTaskStepVo.getStatus(), ProcessTaskStepStatus.RUNNING.getValue())) {
+                                doingCount++;
+                            }
+                            if (count > 100) {
+                                continue;
+                            }
+                            count++;
+//                            long startTimeD = System.currentTimeMillis();
+                            List<ProcessTaskSlaTimeVo> slaTimeList = processTaskService.getSlaTimeListByProcessTaskStepId(processTaskStepVo.getId());
+//                            System.out.println("costTimeD = " + (System.currentTimeMillis() - startTimeD));
+                            if (CollectionUtils.isNotEmpty(slaTimeList)) {
+                                for (ProcessTaskSlaTimeVo processTaskSlaTimeVo : slaTimeList) {
+                                    if (Objects.equals(processTaskSlaTimeVo.getSlaTimeDisplayMode(), "naturalTime")) {
+                                        if (processTaskSlaTimeVo.getRealTimeLeft() < 0) {
+                                            riskCount++;
+                                        }
+                                    } else if (Objects.equals(processTaskSlaTimeVo.getSlaTimeDisplayMode(), "naturalTime")) {
+                                        if (processTaskSlaTimeVo.getTimeLeft() < 0) {
+                                            riskCount++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        {
+//            long startTimeE = System.currentTimeMillis();
+            List<Long> processTaskStepIdList = processTaskMapper.getProcessTaskStepUserProcessTaskStepIdListByUserUuidAndStatusAndStartTimeAndEndTime(
+                    UserContext.get().getUserUuid(),
+                    List.of("major", "minor"),
+                    List.of("done"),
+                    startTime,
+                    endTime
+            );
+//            System.out.println("costTimeE = " + (System.currentTimeMillis() - startTimeE));
+            if (CollectionUtils.isNotEmpty(processTaskStepIdList)) {
+//                long startTimeF = System.currentTimeMillis();
+                List<ProcessTaskStepVo> processTaskStepList = processTaskMapper.getProcessTaskStepListByIdList(processTaskStepIdList);
+//                System.out.println("costTimeF = " + (System.currentTimeMillis() - startTimeF));
+                Set<Long> processTaskIdSet = processTaskStepList.stream().map(ProcessTaskStepVo::getProcessTaskId).collect(Collectors.toSet());
+//                long startTimeG = System.currentTimeMillis();
+                List<ProcessTaskVo> processTaskList = processTaskMapper.getProcessTaskListByIdList(new ArrayList<>(processTaskIdSet));
+//                System.out.println("costTimeG = " + (System.currentTimeMillis() - startTimeG));
+                Map<Long, ProcessTaskVo> processTaskMap = processTaskList.stream().collect(Collectors.toMap(ProcessTaskVo::getId, e -> e));
+                for (ProcessTaskStepVo processTaskStepVo : processTaskStepList) {
+                    ProcessTaskVo processTaskVo = processTaskMap.get(processTaskStepVo.getProcessTaskId());
+                    if (processTaskVo != null && Objects.equals(processTaskVo.getIsDeleted(), 0)) {
+                        if (Objects.equals(processTaskStepVo.getIsActive(), 2)) {
+                            doneCount++;
+                        }
+                    }
+                }
+            }
+        }
         JSONObject resultObj = new JSONObject();
         // 我的待办
-        resultObj.put("myTask", 6);
+        resultObj.put("myTask", myTaskCount);
         // 可抢单
-        resultObj.put("todo", 1);
+        resultObj.put("todo", todoCount);
         // 处理中
-        resultObj.put("doing", 2);
+        resultObj.put("doing", doingCount);
         // 已超时
-        resultObj.put("risk", 3);
+        resultObj.put("risk", riskCount);
         // 已完成
-        resultObj.put("done", 4);
+        resultObj.put("done", doneCount);
         return resultObj;
     }
 }
