23bea58ff99b740fe981fd2280b0fb7da86c9bfd
1437892690@qq.com <1437892690@qq.com>
2026-08-06 16:51:39 +0800

    [功能] 后端-门户管理接口
    关联 #[1734939029274631]后端-门户管理接口 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1734939029274631

--- src/main/java/neatlogic/module/process/portal/widget/ProcessPortalWidget.java
+++ src/main/java/neatlogic/module/process/portal/widget/ProcessPortalWidget.java
@@ -12,16 +12,17 @@ package neatlogic.module.process.portal.widget;
 
 import neatlogic.framework.portal.widget.core.IPortalWidget;
 import neatlogic.framework.portal.widgetdata.core.IPortalWidgetDataHandler;
+import neatlogic.framework.util.$;
 import neatlogic.module.process.portal.widget.handler.*;
 
 import java.util.List;
 
 public enum ProcessPortalWidget implements IPortalWidget {
-    processingOfMineProcessTask("processingOfMineProcessTask", "我的待办", 1, List.of(ProcessingOfMineProcessTaskPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
-    draftProcessTask("draftProcessTask", "我的草稿", 2, List.of(DraftProcessTaskPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
-    processTaskSearch("processTaskSearch", "工单列表", 3, List.of(ProcessTaskSearchPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
-    processFavoriteService("processFavoriteService", "收藏服务", 4, List.of(FavoritedServiceListPortalWidgetDataHandler.class)),
-    personalProcessTaskOverview("personalProcessTaskOverview", "个人工单状态概览", 5, List.of(PersonalProcessTaskOverviewPortalWidgetDataHandler.class)),
+    PROCESSING_OF_MINE_PROCESSTASK("processingOfMineProcessTask", "我的待办", 1, List.of(ProcessingOfMineProcessTaskPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
+    DRAFT_PROCESS_TASK("draftProcessTask", "我的草稿", 2, List.of(DraftProcessTaskPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
+    PROCESS_TASK_SEARCH("processTaskSearch", "工单列表", 3, List.of(ProcessTaskSearchPortalWidgetDataHandler.class, ProcessTaskTheadListWidgetDataHandler.class)),
+    PROCESS_FAVORITE_SERVICE("processFavoriteService", "收藏服务", 4, List.of(FavoritedServiceListPortalWidgetDataHandler.class)),
+    PERSONAL_PROCESS_TASK_OVERVIEW("personalProcessTaskOverview", "个人工单状态概览", 5, List.of(PersonalProcessTaskOverviewPortalWidgetDataHandler.class)),
     ;
 
     private final String value;
@@ -43,7 +44,7 @@ public enum ProcessPortalWidget implements IPortalWidget {
 
     @Override
     public String getText() {
-        return this.text;
+        return $.t(this.text);
     }
 
     @Override
