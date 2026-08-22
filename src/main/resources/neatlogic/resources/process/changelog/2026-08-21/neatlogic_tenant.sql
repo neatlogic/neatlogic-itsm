d393daa3f31120b275366df31502bd2465971745
1437892690@qq.com <1437892690@qq.com>
2026-08-22 16:05:09 +0800

    [功能] 服务目录管理增加代报授权
    关联 #[1757354958520323]服务目录管理增加代报授权 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1757354958520323

--- src/main/resources/neatlogic/resources/process/changelog/2026-08-21/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/process/changelog/2026-08-21/neatlogic_tenant.sql
@@ -1,15 +1,14 @@
--- 扩展服务目录和服务通道授权类型，使 action=delegate 可以参与联合主键并正常保存。
 ALTER TABLE `catalog_authority`
-    MODIFY COLUMN `action` ENUM('report', 'view', 'delegate') NOT NULL COMMENT '授权类型：report-上报，view-查看，delegate-代报';
-
+    CHANGE `action` `action` ENUM ('report', 'view', 'delegate') CHARSET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '授权类型：report-上报，view-查看，delegate-代报';
 ALTER TABLE `channel_authority`
-    MODIFY COLUMN `action` ENUM('report', 'view', 'delegate') NOT NULL COMMENT '授权类型：report-上报，view-查看，delegate-代报';
+    CHANGE `action` `action` ENUM ('report', 'view', 'delegate') CHARSET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '授权类型：report-上报，view-查看，delegate-代报';
 
--- 为存量目录和服务补充“所有人”代报授权，保持升级前上报人可修改的兼容行为。
 INSERT IGNORE INTO `catalog_authority` (`catalog_uuid`, `type`, `uuid`, `action`)
 SELECT `uuid`, 'common', 'alluser', 'delegate'
-FROM `catalog`;
+FROM `catalog`
+WHERE `uuid` NOT IN (SELECT `catalog_uuid` FROM `catalog_authority` WHERE `action` = 'delegate');
 
 INSERT IGNORE INTO `channel_authority` (`channel_uuid`, `type`, `uuid`, `action`)
 SELECT `uuid`, 'common', 'alluser', 'delegate'
-FROM `channel`;
+FROM `channel`
+WHERE `uuid` NOT IN (SELECT `channel_uuid` FROM `channel_authority` WHERE `action` = 'delegate');
